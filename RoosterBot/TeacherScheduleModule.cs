d01edbbcd8a0028a756f780ad733deac654b36db
DirkPC <d.kok.2000@gmail.com>
2018-10-01 15:27:01 +0200

    replace text output with table output in TeacherScheduleModule

--- RoosterBot/TeacherScheduleModule.cs
+++ RoosterBot/TeacherScheduleModule.cs
@@ -21,7 +21,7 @@ namespace RoosterBot {
 				await ReplyAsync("Is dat wel een leraar? :thinking: Als hij of zij nieuw is, moet hij worden toegevoegd door de bot eigenaar.");
 			} else {
 				if (teacher.Contains(", ")) { // There are multiple
-					await RespondTeacherMultiple(false, leraar, teacher.Split(new[] { ", " }, StringSplitOptions.None));
+					await ReplyAsync(await RespondTeacherMultiple(false, leraar, teacher.Split(new[] { ", " }, StringSplitOptions.None)));
 				} else {
 					ReturnValue<ScheduleRecord> result = await GetRecord(false, "StaffMember", teacher);
 					if (result.Success) {
@@ -43,7 +43,7 @@ namespace RoosterBot {
 				await ReplyAsync("Is dat wel een leraar? :thinking: Als hij of zij nieuw is, moet hij worden toegevoegd door de bot eigenaar.");
 			} else {
 				if (teacher.Contains(", ")) { // There are multiple
-					await RespondTeacherMultiple(true, leraar, teacher.Split(new[] { ", " }, StringSplitOptions.None));
+					await ReplyAsync(await RespondTeacherMultiple(true, leraar, teacher.Split(new[] { ", " }, StringSplitOptions.None)));
 				} else {
 					ReturnValue<ScheduleRecord> result = await GetRecord(true, "StaffMember", teacher);
 					if (result.Success) {
@@ -76,7 +76,7 @@ namespace RoosterBot {
 					await ReplyAsync("Is dat wel een leraar? :thinking: Als hij of zij nieuw is, moet hij worden toegevoegd door de bot eigenaar.");
 				} else {
 					if (teacher.Contains(", ")) {
-						await RespondTeacherWeekdayMultiple(day, teacherGiven, teacher.Split(new[] { ", " }, StringSplitOptions.None));
+						await ReplyAsync(await RespondTeacherWeekdayMultiple(day, teacherGiven, teacher.Split(new[] { ", " }, StringSplitOptions.None)));
 					} else {
 						ReturnValue<ScheduleRecord> result = await GetFirstRecord(day, "StaffMember", teacher);
 						if (result.Success) {
@@ -87,127 +87,64 @@ namespace RoosterBot {
 				}
 			}
 		}
-
-		private string RespondTeacherWeekday(string teacher, DayOfWeek day, ScheduleRecord record) {
-			if (record == null) {
-				string response = $"Het lijkt er op dat {teacher} op {DateTimeFormatInfo.CurrentInfo.GetDayName(day)} niets heeft.";
-				if (day == DayOfWeek.Saturday || day == DayOfWeek.Sunday) {
-					response += "\nDat is dan ook in het weekend.";
-				}
-				return response;
-			} else {
-				string response = $"{teacher} heeft op {(DateTime.Today.DayOfWeek == day ? "volgende week " : "")}{DateTimeFormatInfo.CurrentInfo.GetDayName(record.Start.DayOfWeek)} als eerste";
-				if (record.Activity == "pauze") {
-					response += $" pauze van {record.Start.ToShortTimeString()} tot {record.End.ToShortTimeString()}.";
-				} else {
-					response += $" {record.Activity}";
-
-					if (!string.IsNullOrEmpty(record.StudentSets)) {
-						response += $" aan {record.StudentSets}";
-					}
-					if (!string.IsNullOrEmpty(record.Room)) {
-						response += $" in {record.Room}";
-					} else {
-						response += ", maar ik weet niet waar";
-					}
-					response += $".\n{GetTimeSpanResponse(record)}";
-				}
-				return response;
-			}
-		}
-
-		private async Task RespondTeacherWeekdayMultiple(DayOfWeek day, string ambiguousPart, params string[] teacherAbbrs) {
-			string response = "";
-
-			string[] teachers = new string[teacherAbbrs.Length];
-			for (int i = 0; i < teacherAbbrs.Length; i++) {
-				teachers[i] = GetTeacherNameFromAbbr(teacherAbbrs[i]);
-			}
-
-			if (teachers.Contains(null)) {
-				await FatalError($"RespondTeacherWeekdayMultiple: GetTeacherName({string.Join(",", teacherAbbrs)}) == null");
-			}
-
-			response += $"\"{ambiguousPart}\" kan {Util.FormatStringArray(teachers, ", of ")} zijn.\n\n";
-			ReturnValue<ScheduleRecord>[] results = new ReturnValue<ScheduleRecord>[teachers.Length];
-			for (int i = 0; i < results.Length; i++) {
-				results[i] = await GetFirstRecord(day, "StaffMember", teacherAbbrs[i]);
-			}
-
-			bool allSuccessful = true;
-			string resultResponse = "";
-			for (int i = 0; i < results.Length; i++) {
-				if (results[i].Success) {
-					resultResponse += RespondTeacherWeekday(teachers[i], day, results[i].Value) + "\n";
-				} else {
-					allSuccessful = false;
-				}
-			}
-
-			if (allSuccessful) {
-				response += resultResponse;
-				response += "Hou er rekening mee dat ik niet weet of een leraar ziek is, of om een andere reden weg is.";
-			} else if (string.IsNullOrEmpty(resultResponse)) {
-				response += "Echter heb ik info over geen enkele leraar kunnen vinden.\n";
-			} else {
-				response += "Echter heb ik alleen over sommige leraren info kunnen vinden.\n";
-				response += resultResponse;
-				response += "Hou er rekening mee dat ik niet weet of een leraar ziek is, of om een andere reden weg is.";
-			}
-
-			await ReplyAsync(response);
-		}
-
+		
 		// This is a seperate function because two teachers have the same name. We would have to write this function three times in TeacherCurrentCommand().
 		private string RespondTeacherCurrent(string teacher, ScheduleRecord record) {
+			string response;
 			if (record == null) {
-				string response = $"Het ziet ernaar uit dat {teacher} nu niets heeft.";
+				response = $"Het lijkt erop dat {teacher} nu niets heeft.";
 				if (DateTime.Today.DayOfWeek == DayOfWeek.Saturday || DateTime.Today.DayOfWeek == DayOfWeek.Sunday) {
 					response += " Het is dan ook weekend.";
 				}
-				return response;
 			} else {
-				string response = "";
-				if (record.Activity == "pauze") {
-					response += $"{teacher} heeft nu pauze van {record.Start.ToShortTimeString()} tot {record.End.ToShortTimeString()}.";
-				} else {
-					response += $"{teacher} geeft nu {record.Activity}";
+				response = $"{teacher}: Nu\n";
+				response += $":notepad_spiral: {record.Activity}\n";
 
-					if (!string.IsNullOrEmpty(record.StudentSets)) {
-						response += $" aan {record.StudentSets}";
-					}
-					if (!string.IsNullOrEmpty(record.Room)) {
-						response += $" in {record.Room}";
-					} else {
-						response += ", en ik weet niet waar";
-					}
-					response += $".\n{GetTimeSpanResponse(record)}";
+				if (!string.IsNullOrWhiteSpace(record.StudentSets)) {
+					response += $":busts_in_silhouette: {record.StudentSets}\n";
 				}
-				return response;
-			}
-		}
-
-		private string RespondTeacherNext(string teacher, ScheduleRecord record) {
-			string response = "";
-			if (record.Activity == "pauze") {
-				response += $"{teacher} heeft {GetNextTimeString(record)} pauze van {record.Start.ToShortTimeString()} tot {record.End.ToShortTimeString()}.";
-			} else {
-				response += $"{teacher} geeft {GetNextTimeString(record)} {record.Activity}";
-
-				if (!string.IsNullOrEmpty(record.StudentSets)) {
-					response += $" aan {record.StudentSets}";
-				}
-				if (!string.IsNullOrEmpty(record.Room)) {
-					response += $" in {record.Room}";
-				} else {
-					response += ", en ik weet niet waar";
-				}
-				response += $".\n{GetTimeSpanResponse(record)}";
+				response += $":calendar_spiral: {DateTimeFormatInfo.CurrentInfo.GetDayName(record.Start.DayOfWeek)} {record.Start.ToShortDateString()}\n";
+				response += $":clock5: {record.Start.ToShortTimeString()} - {record.End.ToShortTimeString()}\n";
+				response += $":stopwatch: {record.Duration}\n";
 			}
 			return response;
 		}
 
-		private async Task RespondTeacherMultiple(bool next, string ambiguousPart, params string[] teacherAbbrs) {
+		private string RespondTeacherNext(string teacher, ScheduleRecord record) {
+			string response = $"{teacher}: Hierna\n";
+			response += $":notepad_spiral: {record.Activity}\n";
+
+			if (!string.IsNullOrWhiteSpace(record.StudentSets)) {
+				response += $":busts_in_silhouette: {record.StudentSets}\n";
+			}
+			response += $":calendar_spiral: {DateTimeFormatInfo.CurrentInfo.GetDayName(record.Start.DayOfWeek)} {record.Start.ToShortDateString()}\n";
+			response += $":clock5: {record.Start.ToShortTimeString()} - {record.End.ToShortTimeString()}\n";
+			response += $":stopwatch: {record.Duration}\n";
+			return response;
+		}
+
+		private string RespondTeacherWeekday(string teacher, DayOfWeek day, ScheduleRecord record) {
+			string response;
+			if (record == null) {
+				response = $"Het lijkt er op dat {teacher} op {DateTimeFormatInfo.CurrentInfo.GetDayName(day)} niets heeft.";
+				if (day == DayOfWeek.Saturday || day == DayOfWeek.Sunday) {
+					response += "\nDat is dan ook in het weekend.";
+				}
+			} else {
+				response = $"{teacher}: Als eerste op {DateTimeFormatInfo.CurrentInfo.GetDayName(day)}\n";
+				response += $":notepad_spiral: {record.Activity}\n";
+
+				if (!string.IsNullOrWhiteSpace(record.StudentSets)) {
+					response += $":busts_in_silhouette: {record.StudentSets}\n";
+				}
+				response += $":calendar_spiral: {DateTimeFormatInfo.CurrentInfo.GetDayName(record.Start.DayOfWeek)} {record.Start.ToShortDateString()}\n";
+				response += $":clock5: {record.Start.ToShortTimeString()} - {record.End.ToShortTimeString()}\n";
+				response += $":stopwatch: {record.Duration}";
+			}
+			return response;
+		}
+
+		private async Task<string> RespondTeacherMultiple(bool next, string ambiguousPart, params string[] teacherAbbrs) {
 			string response = "";
 
 			string[] teachers = new string[teacherAbbrs.Length];
@@ -218,37 +155,51 @@ namespace RoosterBot {
 			if (teachers.Contains(null)) {
 				await FatalError($"RespondTeacherMultiple: GetTeacherName({string.Join(",", teacherAbbrs)}) == null");
 			}
-
-			response += $"\"{ambiguousPart}\" kan {Util.FormatStringArray(teachers, ", of ")} zijn.\n\n";
+			
 			ReturnValue<ScheduleRecord>[] results = new ReturnValue<ScheduleRecord>[teachers.Length];
 			for (int i = 0; i < results.Length; i++) {
 				results[i] = await GetRecord(next, "StaffMember", teacherAbbrs[i]);
 			}
 
 			Func<string, ScheduleRecord, string> respondFunction = next ? (Func<string, ScheduleRecord, string>) RespondTeacherNext : RespondTeacherCurrent;
-
-			bool allSuccessful = true;
-			string resultResponse = "";
+			
 			for (int i = 0; i < results.Length; i++) {
 				if (results[i].Success) {
-					resultResponse += respondFunction(teachers[i], results[i].Value) + "\n";
+					response += respondFunction(teachers[i], results[i].Value) + "\n\n";
 				} else {
-					allSuccessful = false;
+					response += $"{teachers[i]}: Geen info.";
 				}
 			}
 
-			if (allSuccessful) {
-				response += resultResponse;
-				response += "Hou er rekening mee dat ik niet weet of een leraar ziek is, of om een andere reden weg is.";
-			} else if (string.IsNullOrEmpty(resultResponse)) {
-				response += "Echter heb ik info over geen enkele leraar kunnen vinden.\n";
-			} else {
-				response += "Echter heb ik alleen over sommige leraren info kunnen vinden.\n";
-				response += resultResponse;
-				response += "Hou er rekening mee dat ik niet weet of een leraar ziek is, of om een andere reden weg is.";
+			return response;
+		}
+
+		private async Task<string> RespondTeacherWeekdayMultiple(DayOfWeek day, string ambiguousPart, params string[] teacherAbbrs) {
+			string response = "";
+
+			string[] teachers = new string[teacherAbbrs.Length];
+			for (int i = 0; i < teacherAbbrs.Length; i++) {
+				teachers[i] = GetTeacherNameFromAbbr(teacherAbbrs[i]);
 			}
 
-			await ReplyAsync(response);
+			if (teachers.Contains(null)) {
+				await FatalError($"RespondTeacherWeekdayMultiple: GetTeacherName({string.Join(",", teacherAbbrs)}) == null");
+			}
+			
+			ReturnValue<ScheduleRecord>[] results = new ReturnValue<ScheduleRecord>[teachers.Length];
+			for (int i = 0; i < results.Length; i++) {
+				results[i] = await GetFirstRecord(day, "StaffMember", teacherAbbrs[i]);
+			}
+			
+			for (int i = 0; i < results.Length; i++) {
+				if (results[i].Success) {
+					response += RespondTeacherWeekday(teachers[i], day, results[i].Value) + "\n\n";
+				} else {
+					response += $"{teachers[i]}: Geen info.";
+				}
+			}
+
+			return response;
 		}
 	}
 }
