8ba0a8351058d385505ad056d1264e7ca0b7fd50
DirkPC <d.kok.2000@gmail.com>
2018-10-02 21:58:44 +0200

    SNSService no longer sends critical error messages when in running in debug

--- RoosterBot/SNSService.cs
+++ RoosterBot/SNSService.cs
@@ -13,13 +13,19 @@ namespace RoosterBot {
 		}
 
 		public async Task SendCriticalErrorNotificationAsync(string message) {
+#if !DEBUG
 			Logger.Log(Discord.LogSeverity.Info, "SNSService", "Sending error report to SNS (async)");
 			await m_SNSClient.PublishAsync(new PublishRequest(m_ConfigService.SNSCriticalFailureARN, message));
+#else
+			await Task.CompletedTask; // Just await it instead of returning it to suppress the warning
+#endif
 		}
 		
 		public void SendCriticalErrorNotification(string message) {
+#if !DEBUG
 			Logger.Log(Discord.LogSeverity.Info, "SNSService", "Sending error report to SNS");
 			m_SNSClient.Publish(new PublishRequest(m_ConfigService.SNSCriticalFailureARN, message));
+#endif
 		}
 	}
 }
