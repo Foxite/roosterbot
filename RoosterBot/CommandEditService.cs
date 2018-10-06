8b0a07375fbb9827e76e5f98f6ebffda1362f893
foxite <d.kok.2000@gmail.com>
2018-10-06 20:57:31 +0200

    the bot will now delete its response if a command is deleted. this only works for the lsst command by a user.

--- RoosterBot/CommandEditService.cs
+++ RoosterBot/CommandEditService.cs
@@ -3,9 +3,9 @@ using System.Threading.Tasks;
 using System.Linq;
 using Discord;
 using System.Collections.Generic;
-using System;
 using Discord.WebSocket;
 using Discord.Commands;
+using System;
 
 namespace RoosterBot {
 	/* The class lets us change our response when a user edits their command (especially after an error occured).
@@ -14,28 +14,37 @@ namespace RoosterBot {
 	public class EditedCommandService : CommandService {
 		private ConcurrentDictionary<ulong, CommandResponsePair> m_Messages;
 		private DiscordSocketClient m_Client;
-		private Func<IUserMessage, SocketUserMessage, Task> m_HandleCommandFunction;
 
-		public EditedCommandService(DiscordSocketClient client, Func<IUserMessage, SocketUserMessage, Task> handleCommandFunction) {
+		public delegate Task HandleCommand(IUserMessage ourResponse, SocketUserMessage command);
+		private HandleCommand m_HandleCommandFunction;
+
+		public EditedCommandService(DiscordSocketClient client, HandleCommand handleCommandFunction) {
 			m_Messages = new ConcurrentDictionary<ulong, CommandResponsePair>();
 			m_Client = client;
 			m_HandleCommandFunction = handleCommandFunction;
 
 			m_Client.MessageUpdated += OnMessageUpdated;
+			m_Client.MessageDeleted += OnMessageDeleted;
+		}
+
+		private async Task OnMessageDeleted(Cacheable<IMessage, ulong> message, ISocketMessageChannel channel) {
+			if (m_Messages.TryGetValue(message.Id, out CommandResponsePair crp)) {
+				await crp.Response.DeleteAsync();
+			}
 		}
 
 		// Warning: argument names are speculative because the documentation around the API is nearly nonexistent.
 		// The ulong in messageBefore is the id of the IMessage.
 		// However, I can only assume that the ISocketMessageChannel was included by mistake/convenience, because you can get the same information from the SocketMessage.
 		private async Task OnMessageUpdated(Cacheable<IMessage, ulong> messageBefore, SocketMessage messageAfter, ISocketMessageChannel channel) {
+			// This part is the point of this class. It checks if the new message is a command, executes it, and tells ReplyAsync in modules that use this
+			//  class to edit their response instead of making a new one.
+			// This class is designed to work with EditableCmdModuleBase in that aspect. It can simply be replaced as ModuleBase in existing modules and they will
+			//  automatically adopt this functionality without needing additional work.
 			if (!(messageAfter is SocketUserMessage socketMessageAfter))
 				return;
 			
 			if (m_Messages.TryGetValue(messageAfter.Id, out CommandResponsePair crp)) {
-				// This part is the point of this class. It checks if the new message is a command, executes it, and tells ReplyAsync in modules that use this
-				//  class to edit their response instead of making a new one.
-				// This class is designed to work with EditableCmdModuleBase in that aspect. It can simply be replaced as ModuleBase in existing modules and they will
-				//  automatically adopt this functionality without needing additional work.
 				await m_HandleCommandFunction(crp.Response, socketMessageAfter);
 			}
 		}
