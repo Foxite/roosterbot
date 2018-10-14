4adc8a751b316297796deae8f7d7ccccba9c0470
DirkPC <d.kok.2000@gmail.com>
2018-10-14 22:42:40 +0200

    forgot a piece

--- RoosterBot/Modules/StudentScheduleModule.cs
+++ RoosterBot/Modules/StudentScheduleModule.cs
@@ -68,7 +68,7 @@ namespace RoosterBot.Modules {
 
 					if (record.Activity != "stdag doc") {
 						if (record.Activity != "pauze") {
-							string teachers = Teachers.GetFullNameFromAbbr(record.StaffMember);
+							string teachers = GetTeacherFullNamesFromAbbrs(record.StaffMember);
 							if (!string.IsNullOrWhiteSpace(teachers)) {
 								if (record.StaffMember == "JWO" && Util.RNG.NextDouble() < 0.1) {
 									response += $"<:VRjoram:392762653367336960> {teachers}\n";
