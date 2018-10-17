4d471a1861b194b9d9c97c9fe48d5d7628ce769a
DirkPC <d.kok.2000@gmail.com>
2018-10-17 16:59:23 +0200

    ConfigService is no longer internal, but rather now has few public members and its former public members have been made internal, including the constructor.

--- RoosterBot/Services/ConfigService.cs
+++ RoosterBot/Services/ConfigService.cs
@@ -1,27 +1,25 @@
 ﻿using System;
 using System.Collections.Concurrent;
-using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json.Linq;
 
 namespace RoosterBot.Services {
-	internal class ConfigService {
+	public class ConfigService {
 		private ConcurrentDictionary<ulong, CooldownData> m_CooldownList;
+		
+		public	 ulong  BotOwnerId { get; private set; }
+		public	 bool   ErrorReactions { get; private set; }
+		public	 string CommandPrefix { get; private set; }
+		public	 string GameString { get; private set; }
+		internal string SNSCriticalFailureARN { get; private set; }
 
-		public float  Cooldown { get; private set; } // TODO allow cooldown to be set per command
-		public ulong  BotOwnerId { get; private set; }
-		public bool   ErrorReactions { get; private set; }
-		public string CommandPrefix { get; private set; }
-		public string GameString { get; private set; }
-		public string SNSCriticalFailureARN { get; private set; }
-
-		public ConfigService(string jsonPath, out string authToken) {
+		internal ConfigService(string jsonPath, out string authToken) {
 			m_CooldownList = new ConcurrentDictionary<ulong, CooldownData>();
 			LoadConfigInternal(jsonPath, out authToken);
 		}
 
 		// The auth token will not be returned, because to take effect after changing it you would need to restart the bot.
-		public void ReloadConfig(string jsonPath) {
+		internal void ReloadConfig(string jsonPath) {
 			LoadConfigInternal(jsonPath, out string unused);
 		}
 
@@ -29,7 +27,6 @@ namespace RoosterBot.Services {
 			string jsonFile = File.ReadAllText(jsonPath);
 			JObject jsonConfig = JObject.Parse(jsonFile);
 			
-			Cooldown = jsonConfig["cooldown"].ToObject<float>();
 			BotOwnerId = jsonConfig["botOwnerId"].ToObject<ulong>();
 			authToken = jsonConfig["token"].ToObject<string>();
 			ErrorReactions = jsonConfig["errorReactions"].ToObject<bool>();
@@ -42,13 +39,13 @@ namespace RoosterBot.Services {
 		/// Call this before executing a command to see if the user is still in their cooldown period.
 		/// </summary>
 		/// <returns>1: true if the user is not in cooldown (in this case, the cooldown will be reset). 2: if the user was already warned for their cooldown excession.</returns>
-		public Tuple<bool, bool> CheckCooldown(ulong userId) {
+		public Tuple<bool, bool> CheckCooldown(ulong userId, float cooldownSeconds) {
 #if DEBUG
 			return new Tuple<bool, bool>(true, false);
 #else
 			DateTime now = DateTime.UtcNow;
 			if (m_CooldownList.ContainsKey(userId)) {
-				if (now.AddSeconds(-Cooldown).Ticks > m_CooldownList[userId].TimeLastCommand) { // Note: DateTime.AddSeconds() returns a *new* DateTime object so this does not actually change {now}.
+				if (now.AddSeconds(-cooldownSeconds).Ticks > m_CooldownList[userId].TimeLastCommand) { // Note: DateTime.AddSeconds() returns a *new* DateTime object so this does not actually change {now}.
 					m_CooldownList[userId].TimeLastCommand = now.Ticks;
 					m_CooldownList[userId].Warned = false;
 					return new Tuple<bool, bool>(true, false);
