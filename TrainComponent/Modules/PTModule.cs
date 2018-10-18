076b2492ff8747d5b3fd2faf64fb7bc6ddac6ccf
foxite <d.kok.2000@gmail.com>
2018-10-18 17:04:33 +0200

    started working on a public transit component

---
+++ TrainComponent/Modules/PTModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord.Commands;
+using RoosterBot.Modules;
+using TrainComponent.Services;
+
+namespace TrainComponent.Modules {
+	public class PTModule : EditableCmdModuleBase {
+		public NSAPI NSAPI { get; set; }
+
+		[Command("ov")]
+		public async Task GetTrainRouteCommand([Remainder] string parameters) {
+			
+		}
+	}
+}
---
+++ TrainComponent/PublicTransitComponent.cs
@@ -0,0 +1,24 @@
+﻿using RoosterBot;
+using RoosterBot.Services;
+using Microsoft.Extensions.DependencyInjection;
+using TrainComponent.Services;
+using TrainComponent.Modules;
+using Newtonsoft.Json.Linq;
+using System.IO;
+
+namespace TrainComponent {
+	public class PublicTransitComponent : ComponentBase {
+		public override void Initialize(ref IServiceCollection services, EditedCommandService commandService, string configPath) {
+			#region Config
+			string jsonFile = File.ReadAllText(Path.Combine(configPath, "Config.json"));
+			JObject jsonConfig = JObject.Parse(jsonFile);
+			string username = jsonConfig["nsapi_Username"].ToObject<string>();
+			string password = jsonConfig["nsapi_Password"].ToObject<string>();
+			#endregion Config
+			
+			services.AddSingleton(new NSAPI("http://webservices.ns.nl/ns-api-", username, password));
+
+			commandService.AddModuleAsync<PTModule>();
+		}
+	}
+}
