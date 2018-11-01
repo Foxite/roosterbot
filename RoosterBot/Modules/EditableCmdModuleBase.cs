baf0ac7c013877fdb4d57c0d9cd6949f44853a02
DirkPC <d.kok.2000@gmail.com>
2018-11-01 20:14:43 +0100

    added RoosterModuleBase with an optional type argument, and moved some highly generic functionality from EditableCmdModuleBase to RMB. ECMB now only enables editable commands, all modules should inherit from RMB instead of ModuleBase

--- RoosterBot/Modules/EditableCmdModuleBase.cs
+++ RoosterBot/Modules/EditableCmdModuleBase.cs
@@ -1,19 +1,12 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using Discord;
-using Discord.Commands;
 using RoosterBot.Services;
 
 namespace RoosterBot.Modules {
-	public abstract class EditableCmdModuleBase : ModuleBase<EditedCommandContext> {
+	public abstract class EditableCmdModuleBase : RoosterModuleBase<EditedCommandContext> {
 		// These should actually be protected, but they're here because Discord.NET injects these services when a command is called.
 		// There's a few other ways to get the services with the injection system, but this is the easiest way.
 		public EditedCommandService CmdService { get; set; }
-
-		public ConfigService Config { get; set; }
-		public SNSService SNSService { get; set; }
-
-		public string LogTag { get; protected set; }
 
 		private bool m_ResponseWasModified;
 		
@@ -33,41 +26,6 @@ namespace RoosterBot.Modules {
 				});
 				return Context.OriginalResponse;
 			}
-		}
-
-		protected async virtual Task<bool> AddReaction(string unicode) {
-			return await Util.AddReaction(Context.Message, unicode);
-		}
-
-		protected async virtual Task<bool> CheckCooldown() {
-			Tuple<bool, bool> result = Config.CheckCooldown(Context.User.Id, 2f); // TODO set cooldowns per command
-			if (result.Item1) {
-				return true;
-			} else {
-				if (!result.Item2) {
-					if (Config.ErrorReactions) {
-						await AddReaction("⚠");
-					}
-					await ReplyAsync(Context.User.Mention + ", je gaat een beetje te snel.");
-				}
-				return false;
-			}
-		}
-
-		protected async virtual Task MinorError(string message) {
-			if (Config.ErrorReactions) {
-				await AddReaction("❌");
-			}
-			await ReplyAsync(message);
-		}
-
-		protected async virtual Task FatalError(string message) {
-			Logger.Log(LogSeverity.Error, LogTag, message);
-			await SNSService.SendCriticalErrorNotificationAsync("Critical error: " + message);
-			if (Config.ErrorReactions) {
-				await AddReaction("🚫");
-			}
-			await ReplyAsync("Ik weet niet wat, maar er is iets gloeiend misgegaan. Probeer het later nog eens? Dat moet ik zeggen van mijn maker, maar volgens mij gaat het niet werken totdat hij het fixt. Sorry.\n");
 		}
 	}
 }
---
+++ RoosterBot/Modules/RoosterModuleBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using RoosterBot.Services;
+
+namespace RoosterBot.Modules {
+	public class RoosterModuleBase<T> : ModuleBase<T> where T : class, ICommandContext {
+		public ConfigService Config { get; set; }
+		public SNSService SNSService { get; set; }
+
+		public string LogTag { get; protected set; }
+
+		protected async virtual Task<bool> AddReaction(string unicode) {
+			return await Util.AddReaction(Context.Message, unicode);
+		}
+
+		protected async virtual Task<bool> CheckCooldown() {
+			Tuple<bool, bool> result = Config.CheckCooldown(Context.User.Id, 2f); // TODO set cooldowns per command
+			if (result.Item1) {
+				return true;
+			} else {
+				if (!result.Item2) {
+					if (Config.ErrorReactions) {
+						await AddReaction("⚠");
+					}
+					await ReplyAsync(Context.User.Mention + ", je gaat een beetje te snel.");
+				}
+				return false;
+			}
+		}
+
+		protected async virtual Task MinorError(string message) {
+			if (Config.ErrorReactions) {
+				await AddReaction("❌");
+			}
+			await ReplyAsync(message);
+		}
+
+		protected async virtual Task FatalError(string message) {
+			Logger.Log(LogSeverity.Error, LogTag, message);
+			await SNSService.SendCriticalErrorNotificationAsync("Critical error: " + message);
+			if (Config.ErrorReactions) {
+				await AddReaction("🚫");
+			}
+			await ReplyAsync("Ik weet niet wat, maar er is iets gloeiend misgegaan. Probeer het later nog eens? Dat moet ik zeggen van mijn maker, maar volgens mij gaat het niet werken totdat hij het fixt. Sorry.\n");
+		}
+	}
+
+	public class RoosterModuleBase : RoosterModuleBase<CommandContext> { }
+}
