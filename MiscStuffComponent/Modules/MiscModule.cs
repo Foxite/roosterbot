0d48058885a4e264a35aa0884d23faa98da7d448
foxite <d.kok.2000@gmail.com>
2018-11-05 21:09:39 +0100

    seems useful to not let literally everyone send messages through the bot tho

--- MiscStuffComponent/Modules/MiscModule.cs
+++ MiscStuffComponent/Modules/MiscModule.cs
@@ -5,17 +5,17 @@ using RoosterBot.Modules;
 
 namespace MiscStuffComponent.Modules {
 	public class MiscModule : RoosterModuleBase {
-		[Command("send")]
+		[Command("send"), RequireOwner]
 		public async Task SendMessageCommand(ulong channel, [Remainder] string message) {
 			await (await Context.Client.GetChannelAsync(channel) as ITextChannel).SendMessageAsync(message);
 		}
 
-		[Command("delete")]
+		[Command("delete"), RequireOwner]
 		public async Task DeleteMessageCommand(ulong channel, ulong msg) {
 			await (await (await Context.Client.GetChannelAsync(channel) as ITextChannel).GetMessageAsync(msg)).DeleteAsync();
 		}
 
-		[Command("test")]
+		[Command("test"), RequireOwner]
 		public async Task TestCommand() {
 			await Context.Channel.SendMessageAsync("test ok");
 		}
