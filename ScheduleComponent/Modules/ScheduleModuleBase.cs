7d377012040b09d883a61dab323f60774b00eb98
foxite <d.kok.2000@gmail.com>
2019-02-13 10:49:49 +0100

    fixed #8

--- ScheduleComponent/Modules/ScheduleModuleBase.cs
+++ ScheduleComponent/Modules/ScheduleModuleBase.cs
@@ -156,7 +156,7 @@ namespace ScheduleComponent.Modules {
 		}
 
 		protected string TableItemDuration(ScheduleRecord record) {
-			string ret = $":stopwatch: {(int) record.Duration.TotalHours}:{record.Duration.Minutes}";
+			string ret = $":stopwatch: {(int) record.Duration.TotalHours}:{record.Duration.Minutes.ToString().PadLeft(2, '0')}";
 			if (record.Start < DateTime.Now && record.End > DateTime.Now) {
 				TimeSpan timeLeft = record.End - DateTime.Now;
 				ret += $" - nog {timeLeft.Hours}:{timeLeft.Minutes.ToString().PadLeft(2, '0')}";
