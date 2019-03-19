00fd088b87e596269697fbc768cd45d74e9ffad8
DirkPC <d.kok.2000@gmail.com>
2019-03-19 17:30:25 +0100

    fixed AppStart path also: CODE BUILD WORKS!

--- AppStart/AppStart.cs
+++ AppStart/AppStart.cs
@@ -5,7 +5,7 @@ namespace RoosterBot.Automation {
 	internal class AppStart {
 		private static void Main(string[] args) {
 			ProcessStartInfo psi = new ProcessStartInfo() {
-				FileName = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "../../../PipeServer/bin/Debug/PipeServer.exe"),
+				FileName = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "../../../RoosterBot/bin/Debug/RoosterBot.exe"),
 				CreateNoWindow = false,
 				UseShellExecute = true,
 
