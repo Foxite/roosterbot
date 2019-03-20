4f00e3cda0cd715499dd98577de77f6e0b29bcb7
foxite <d.kok.2000@gmail.com>
2019-03-20 10:52:25 +0100

    try to verify that AppStart is running because I'm not sure that it is

--- AppStart/AppStart.cs
+++ AppStart/AppStart.cs
@@ -1,9 +1,15 @@
-﻿using System.Diagnostics;
+﻿using System;
+using System.Diagnostics;
 using System.IO;
 
 namespace RoosterBot.Automation {
 	internal class AppStart {
 		private static void Main(string[] args) {
+			Directory.CreateDirectory("C:/temp");
+			using (StreamWriter append = File.AppendText("C:/temp/appstart.log")) {
+				append.WriteLine(DateTime.Now.ToLongTimeString());
+			}
+
 			ProcessStartInfo psi = new ProcessStartInfo() {
 				FileName = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().Location), "../../../PipeServer/bin/Debug/PipeServer.exe"),
 				CreateNoWindow = false,
