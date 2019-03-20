27f06ae00823fa4f0723dcf30bb39964f6716b35
foxite <d.kok.2000@gmail.com>
2019-03-20 11:47:06 +0100

    test 2

--- appstart.cmd
+++ appstart.cmd
@@ -1,4 +1,4 @@
 time /T > C:/temp/appstart.2.log
-start RoosterBot/bin/Debug/RoosterBot
+start RoosterBot/bin/Debug/RoosterBot.exe
 echo done > C:/temp/appstart.2.log
 time /T > C:/temp/appstart.2.log
