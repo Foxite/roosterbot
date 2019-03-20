d7ab9003d1aedb03ffa2f059888afc3dabdba225
foxite <d.kok.2000@gmail.com>
2019-03-20 12:13:15 +0100

    test 4: change committed line endings to windows, they are getting to the server in unix

--- appstart.cmd
+++ appstart.cmd
@@ -1,5 +1,6 @@
-time /T >> C:/temp/appstart.2.log
-start RoosterBot/bin/Debug/RoosterBot.exe
-echo done >> C:/temp/appstart.2.log
-time /T >> C:/temp/appstart.2.log
+@echo off
+time /T >> C:\temp\appstart.2.log
+start C:\temp\RoosterBot\RoosterBot.exe
+echo done >> C:\temp\appstart.2.log
+time /T >> C:\temp\appstart.2.log
 exit
