f24e08d7de54f52e987f0e6e07d527ada0727b2b
foxite <d.kok.2000@gmail.com>
2019-03-21 11:38:03 +0100

    determined that RoosterBot does get started but immediately stops again.

--- appstart.cmd
+++ appstart.cmd
@@ -1,6 +1,6 @@
 @echo off
 time /T >> C:\temp\appstart.2.log
-RoosterBot\bin\Debug\RoosterBot.exe
+start RoosterBot\bin\Debug\RoosterBot.exe
 echo done >> C:\temp\appstart.2.log
 time /T >> C:\temp\appstart.2.log
 exit
