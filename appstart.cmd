d143aa3281f3ae3c3d3083ef8ae4bc3d55283c34
foxite <d.kok.2000@gmail.com>
2019-03-21 11:09:17 +0100

    test 5

--- appstart.cmd
+++ appstart.cmd
@@ -1,6 +1,6 @@
 @echo off
 time /T >> C:\temp\appstart.2.log
-start C:\temp\RoosterBot\RoosterBot.exe
+C:\temp\RoosterBot\RoosterBot.exe
 echo done >> C:\temp\appstart.2.log
 time /T >> C:\temp\appstart.2.log
 exit
