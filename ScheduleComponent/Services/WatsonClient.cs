64cba38366fd87637aae0c69a501173320341e09
foxite <d.kok.2000@gmail.com>
2019-04-25 14:08:00 +0200

    more Watson work. it converts the natlang command into a classic command properly, but the AI is very weird

--- ScheduleComponent/Services/WatsonClient.cs
+++ ScheduleComponent/Services/WatsonClient.cs
@@ -41,28 +41,44 @@ namespace ScheduleComponent.Services {
 					List<RuntimeIntent> intents = result.Output.Intents;
 					if (intents.Count != 0) {
 						RuntimeIntent maxConfidence = intents[0];
+						Logger.Debug(LogTag, $"Selecting from {intents.Count} intents.");
 						for (int i = 1; i < intents.Count; i++) {
 							if (maxConfidence.Confidence < intents[i].Confidence) {
 								maxConfidence = intents[i];
 							}
 						}
-						string response = "Result: " + maxConfidence.Intent;
+						Logger.Debug(LogTag, $"Selected {maxConfidence.Intent}");
+						string response = "Result: " + maxConfidence.Intent + "\nEntities:\n";
+						string params_ = "";
 						foreach (RuntimeEntity entity in result.Output.Entities) {
-							response += $"\n{entity.Entity}: {entity.Value}";
-							if (entity.Groups != null) {
-								response += " (";
-								for (int i = 0; i < entity.Groups.Count; i++) {
-									response += (entity.Groups[i]?.Group ?? "null") + ", ";
+							response += $"- {entity.Entity}: {entity.Value}\n";
+
+							for (int i = 0; i < entity.Location.Count; i++) {
+								// The C# API says this is a nullable long, but the API documentation says its an "integer[]"
+								// Even though it is literally impossible to have a string longer than (2^31-1)/2 (about 1 billion) characters (https://stackoverflow.com/a/140749/3141917),
+								//  which is a length that easily fits into an Int32.
+								// And to top it off, they made it nullable as well.
+								// Who knows why? God, and maybe the programmers at IBM as well, but don't count on it.
+								int? start = (int?) entity.Location[i];
+								int? end = (int?) entity.Location[++i];
+								if (start.HasValue && end.HasValue) {
+									params_ += input.Substring(start.Value, end.Value - start.Value);
+								} else {
+									Logger.Error(LogTag, $"Entity {entity.Entity}: {entity.Value} was skipped: Start or end is null");
 								}
-								response += ")";
 							}
 						}
+						response += $"\nParams: {params_}\n";
+						response += $"Converted: `!{maxConfidence.Intent} {params_}`";
+						Logger.Debug(LogTag, $"Natlang command `{input}` was converted into `!{maxConfidence.Intent} {params_}`");
+
 						await message.Channel.SendMessageAsync(response);
 					} else {
+						Logger.Debug(LogTag, $"Natlang command `{input}` was not recognized.");
 						await Util.AddReaction(message, "❓");
 					}
 				} catch (Exception e) {
-					Logger.Log(LogSeverity.Error, LogTag, "That didn't work.", e);
+					Logger.Error(LogTag, "That didn't work.", e);
 				} finally {
 					if (sessionId != null) {
 						m_Assistant.DeleteSession(AssistantId, sessionId);
