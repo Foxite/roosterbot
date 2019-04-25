af8f10f399122cf13c8e2b821ef6e1fa42d2390f
foxite <d.kok.2000@gmail.com>
2019-04-25 14:08:47 +0200

    no longer always processes commands as natlang

--- ScheduleComponent/ScheduleComponent.cs
+++ ScheduleComponent/ScheduleComponent.cs
@@ -88,15 +88,20 @@ namespace ScheduleComponent {
 		private Task ProcessNaturalLanguageCommandsAsync(SocketMessage socketMsg) {
 			if (socketMsg is IUserMessage msg) {
 				int argPos = 0;
+				bool process = false;
 				if (msg.Channel is IDMChannel && !msg.Content.StartsWith(m_Config.CommandPrefix)) {
-					Logger.Log(LogSeverity.Info, "ScheduleComponent", $"Processing natural language command from {socketMsg.Author.Username}#{socketMsg.Author.Discriminator} in DM channel {socketMsg.Content}");
+					Logger.Log(LogSeverity.Info, "ScheduleComponent", $"Processing natlang command from {socketMsg.Author.Username}#{socketMsg.Author.Discriminator} in DM channel {socketMsg.Content}");
+					process = true;
 				} else if (msg.Channel is IGuildChannel gch && msg.HasMentionPrefix(m_Client.CurrentUser, ref argPos)) {
-					Logger.Log(LogSeverity.Info, "ScheduleComponent", $"Processing natural language command from {socketMsg.Author.Username}#{socketMsg.Author.Discriminator} in `{gch.Guild.Name}` channel `{gch.Name}`: {socketMsg.Content.Substring(argPos)}");
+					Logger.Log(LogSeverity.Info, "ScheduleComponent", $"Processing natlang command from {socketMsg.Author.Username}#{socketMsg.Author.Discriminator} in `{gch.Guild.Name}` channel `{gch.Name}`: {socketMsg.Content.Substring(argPos)}");
+					process = true;
 				}
 
+				if (process) {
 #pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-				m_Watson.ProcessCommandAsync(msg, msg.Content.Substring(argPos));
+					m_Watson.ProcessCommandAsync(msg, msg.Content.Substring(argPos));
 #pragma warning restore CS4014
+				}
 			}
 			return Task.CompletedTask;
 		}
