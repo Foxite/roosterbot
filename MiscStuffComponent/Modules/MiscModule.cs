51df0d236cbc345a9d7869a4546fa8d3f68cb2a1
foxite <d.kok.2000@gmail.com>
2019-05-09 13:56:48 +0200

    hide bot owner commands in MiscModule

--- MiscStuffComponent/Modules/MiscModule.cs
+++ MiscStuffComponent/Modules/MiscModule.cs
@@ -3,21 +3,22 @@ using Discord;
 using Discord.Commands;
 using RoosterBot.Attributes;
 using RoosterBot.Modules;
+using RoosterBot.Preconditions;
 
 namespace MiscStuffComponent.Modules {
 	[LogTag("MiscModule")]
 	public class MiscModule : RoosterModuleBase {
-		[Command("send"), RequireOwner]
+		[Command("send"), RequireBotManager, HiddenFromList]
 		public async Task SendMessageCommand(ulong channel, [Remainder] string message) {
 			await (await Context.Client.GetChannelAsync(channel) as ITextChannel).SendMessageAsync(message);
 		}
 
-		[Command("delete"), RequireOwner]
+		[Command("delete"), RequireBotManager, HiddenFromList]
 		public async Task DeleteMessageCommand(ulong channel, ulong msg) {
 			await (await (await Context.Client.GetChannelAsync(channel) as ITextChannel).GetMessageAsync(msg)).DeleteAsync();
 		}
 
-		[Command("test"), RequireOwner]
+		[Command("test"), RequireBotManager, HiddenFromList]
 		public async Task TestCommand() {
 			await Context.Channel.SendMessageAsync("test ok");
 		}
