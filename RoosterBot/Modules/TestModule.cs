de6451652bf718bbd21e6504c20cf048c3879326
foxite <d.kok.2000@gmail.com>
2019-05-09 13:59:35 +0200

    TestModule hidden from list

--- RoosterBot/Modules/TestModule.cs
+++ RoosterBot/Modules/TestModule.cs
@@ -1,15 +1,16 @@
 ﻿#if DEBUG
 using Discord.Commands;
+using RoosterBot.Attributes;
 using RoosterBot.Services;
-using RoosterBot.Modules.Preconditions;
+using RoosterBot.Preconditions;
 using System.Threading.Tasks;
 
 namespace RoosterBot.Modules {
-	[Attributes.LogTag("TestModule")]
+	[LogTag("TestModule"), HiddenFromList]
 	public class TestModule : EditableCmdModuleBase {
 		public SNSService SNS { get; set; }
 
-		[Command("sns"), RequireOwner]
+		[Command("sns"), RequireOwner, HiddenFromList]
 		public async Task TestErrorCommand() {
 			await SNS.SendCriticalErrorNotificationAsync("test message");
 		}
