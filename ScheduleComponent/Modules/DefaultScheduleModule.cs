db8827c586ef8c26433a782d952821b8afc1a6e5
foxite <d.kok.2000@gmail.com>
2019-05-29 11:11:06 +0200

    DefaultScheduleModule now makes it possible for parameterless commands to be defaulted to "ik"

--- ScheduleComponent/Modules/DefaultScheduleModule.cs
+++ ScheduleComponent/Modules/DefaultScheduleModule.cs
@@ -3,6 +3,8 @@ using Discord.Commands;
 using ScheduleComponent.Services;
 using RoosterBot.Modules;
 using RoosterBot.Attributes;
+using RoosterBot;
+using System;
 
 namespace ScheduleComponent.Modules {
 	// Provides several "virtual" commands that don't do anything useful, but serve as a single list item for 3 different "versions" of each command
@@ -15,32 +17,48 @@ namespace ScheduleComponent.Modules {
 
 		[Priority(-10), Command("nu", RunMode = RunMode.Sync), Alias("rooster"), Summary("Kijk wat er nu op het rooster staat.")]
 		public async Task DefaultCurrentCommand([Remainder] string wat = "") {
-			await ReplyAsync(ErrorMessage);
+			if (string.IsNullOrWhiteSpace(wat)) {
+				await Program.Instance.ExecuteSpecificCommand(Context.OriginalResponse, "nu ik", Context.Message);
+			} else {
+				await ReplyAsync(ErrorMessage);
+			}
 		}
 
 		[Priority(-10), Command("hierna", RunMode = RunMode.Sync), Alias("later", "straks", "zometeen"), Summary("Kijk wat er hierna op het rooster staat")]
 		public async Task DefaultNextCommand([Remainder] string wat = "") {
-			await ReplyAsync(ErrorMessage);
+			if (string.IsNullOrWhiteSpace(wat)) {
+				await Program.Instance.ExecuteSpecificCommand(Context.OriginalResponse, "hierna ik", Context.Message);
+			} else {
+				await ReplyAsync(ErrorMessage);
+			}
+		}
+
+		[Priority(-9), Command("dag", RunMode = RunMode.Sync), HiddenFromList]
+		public async Task DefaultWeekdayCommand(DayOfWeek day) {
+			await Program.Instance.ExecuteSpecificCommand(Context.OriginalResponse, "dag ik " + Util.GetStringFromDayOfWeek(day), Context.Message);
 		}
 
 		[Priority(-10), Command("dag", RunMode = RunMode.Sync), Summary("Het rooster voor een bepaalde dag. Als je de huidige dag gebruikt, pak ik volgende week. `!vandaag` doet dit niet.")]
-		public async Task DefaultWeekdayCommand([Remainder] string wat_en_weekdag) {
+		public async Task DefaultWeekdayCommand([Remainder] string wat_en_weekdag = "") {
 			await ReplyAsync("Ik weet niet of je het over een leraar, klas of lokaal hebt, en/of ik begrijp niet welke weekdag je bedoelt.");
 		}
 
 		[Priority(-10), Command("morgen", RunMode = RunMode.Sync), Summary("Wat er morgen op het rooster staat.")]
-		public async Task DefaultTomorrowCommand([Remainder] string wat) {
-			await ReplyAsync(ErrorMessage);
+		public async Task DefaultTomorrowCommand([Remainder] string wat = "") {
+			if (string.IsNullOrWhiteSpace(wat)) {
+				await Program.Instance.ExecuteSpecificCommand(Context.OriginalResponse, "morgen ik", Context.Message);
+			} else {
+				await ReplyAsync(ErrorMessage);
+			}
 		}
 
 		[Priority(-10), Command("vandaag", RunMode = RunMode.Sync), Summary("Wat er vandaag op het rooster staat.")]
-		public async Task DefaultTodayCommand([Remainder] string wat) {
-			await ReplyAsync(ErrorMessage);
-		}
-
-		[Priority(-10), Command("daarna", RunMode = RunMode.Sync), Summary("Kijk wat er gebeurt na het laatste wat je hebt bekeken.")]
-		public async Task DefaultAfterCommand() {
-			await ReplyAsync("Als je dit ziet is er een groot probleem");
+		public async Task DefaultTodayCommand([Remainder] string wat = "") {
+			if (string.IsNullOrWhiteSpace(wat)) {
+				await Program.Instance.ExecuteSpecificCommand(Context.OriginalResponse, "vandaag ik", Context.Message);
+			} else {
+				await ReplyAsync(ErrorMessage);
+			}
 		}
 	}
 }
