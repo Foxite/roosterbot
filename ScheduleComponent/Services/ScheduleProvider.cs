06a6add8dc4731a803a00dbcb71d9447e3aca1cf
foxite <d.kok.2000@gmail.com>
2019-06-03 13:42:19 +0200

    moved ScheduleProvider from ScheduleComponent.cs into a new file in the Services namespace

---
+++ ScheduleComponent/Services/ScheduleProvider.cs
@@ -0,0 +1,48 @@
+﻿using ScheduleComponent.DataTypes;
+using System;
+
+namespace ScheduleComponent.Services {
+	public class ScheduleProvider {
+		private ScheduleService m_Students;
+		private ScheduleService m_Teachers;
+		private ScheduleService m_Rooms;
+
+		public ScheduleProvider(ScheduleService students, ScheduleService teachers, ScheduleService rooms) {
+			m_Students = students;
+			m_Teachers = teachers;
+			m_Rooms = rooms;
+		}
+
+		public ScheduleRecord GetCurrentRecord(IdentifierInfo identifier) {
+			return GetScheduleType(identifier).GetCurrentRecord(identifier);
+		}
+
+		public ScheduleRecord GetNextRecord(IdentifierInfo identifier) {
+			return GetScheduleType(identifier).GetNextRecord(identifier);
+		}
+
+		public ScheduleRecord GetRecordAfter(IdentifierInfo identifier, ScheduleRecord givenRecord) {
+			return GetScheduleType(identifier).GetRecordAfter(identifier, givenRecord);
+		}
+
+		public ScheduleRecord[] GetSchedulesForDay(IdentifierInfo identifier, DayOfWeek day, bool includeToday) {
+			return GetScheduleType(identifier).GetSchedulesForDay(identifier, day, includeToday);
+		}
+
+		public AvailabilityInfo[] GetWeekAvailability(IdentifierInfo identifier, int weeksFromNow) {
+			return GetScheduleType(identifier).GetWeekAvailability(identifier, weeksFromNow);
+		}
+
+		private ScheduleService GetScheduleType(IdentifierInfo info) {
+			if (info is StudentSetInfo) {
+				return m_Students;
+			} else if (info is TeacherInfo) {
+				return m_Teachers;
+			} else if (info is RoomInfo) {
+				return m_Rooms;
+			} else {
+				throw new ArgumentException("Identifier type " + info.GetType().Name + " is not known to ScheduleProvider");
+			}
+		}
+	}
+}
