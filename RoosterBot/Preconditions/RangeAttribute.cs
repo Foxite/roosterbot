ca4861e879fad4d13074c6b5435c723cffb7b210
DirkPC <d.kok.2000@gmail.com>
2019-06-18 19:40:33 +0200

    RangeAttribute, a ParameterPrecondition that lets you set the range on anything that can be compared to a number using >= operator

---
+++ RoosterBot/Preconditions/RangeAttribute.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using Discord.Commands;
+
+namespace RoosterBot.Preconditions {
+	public class RangeAttribute : ParameterPreconditionAttribute {
+		public int Min { get; }
+		public int Max { get; }
+
+		public RangeAttribute(int min, int max) {
+			Min = min;
+			Max = max;
+		}
+
+		public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, ParameterInfo parameter, object value, IServiceProvider services) {
+			bool ret = (dynamic) value >= Min && (dynamic) value <= Max;
+			if (ret) {
+				return Task.FromResult(PreconditionResult.FromSuccess());
+			} else {
+				return Task.FromResult(PreconditionResult.FromError($"Dat is te hoog of te laag."));
+			}
+		}
+	}
+}
