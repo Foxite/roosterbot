20f96c892ad8e7f27f61655b7e8e66266e1bbf3e
DirkPC <d.kok.2000@gmail.com>
2019-06-18 15:56:20 +0200

    cleanup in ConfigService

--- RoosterBot/Services/ConfigService.cs
+++ RoosterBot/Services/ConfigService.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Concurrent;
 using System.IO;
 using System.Threading.Tasks;
 using Discord;
@@ -7,38 +6,40 @@ using Newtonsoft.Json.Linq;
 
 namespace RoosterBot.Services {
 	public class ConfigService {
-		public	 ulong        BotOwnerId { get; private set; }
+		public	 IUser        BotOwner { get; private set; }
 		public	 bool         ErrorReactions { get; private set; }
 		public	 string       CommandPrefix { get; private set; }
 		public	 string       GameString { get; private set; }
 		public   ITextChannel LogChannel { get; private set; }
 		internal string       SNSCriticalFailureARN { get; private set; }
 
-		internal ConfigService(string jsonPath, out string authToken) {  
-			LoadConfigInternal(jsonPath, out authToken);
-		}
+		[Obsolete("Use ConfigService.BotOwner")]
+		public	 ulong        BotOwnerId => BotOwner.Id;
 
-		// The auth token will not be returned, because to take effect after changing it you would need to restart the bot.
-		internal void ReloadConfig(string jsonPath) {
-			LoadConfigInternal(jsonPath, out string unused);
-		}
-
-		private void LoadConfigInternal(string jsonPath, out string authToken) {
+		internal ConfigService(string jsonPath, out string authToken) {
 			string jsonFile = File.ReadAllText(jsonPath);
 			JObject jsonConfig = JObject.Parse(jsonFile);
-			
-			BotOwnerId = jsonConfig["botOwnerId"].ToObject<ulong>();
+
 			authToken = jsonConfig["token"].ToObject<string>();
 			ErrorReactions = jsonConfig["errorReactions"].ToObject<bool>();
 			CommandPrefix = jsonConfig["commandPrefix"].ToObject<string>();
 			GameString = jsonConfig["gameString"].ToObject<string>();
 			SNSCriticalFailureARN = jsonConfig["snsCF_ARN"].ToObject<string>();
 		}
-
-		internal async Task SetLogChannelAsync(IDiscordClient client, string jsonPath) {
+		
+		/// <summary>
+		/// Load Discord.NET objects based on config data.
+		/// </summary>
+		internal async Task LoadDiscordInfo(IDiscordClient client, string jsonPath) {
 			string jsonFile = File.ReadAllText(Path.Combine(jsonPath, "Config.json"));
 			JObject jsonConfig = JObject.Parse(jsonFile);
-			LogChannel = await client.GetChannelAsync(jsonConfig["logChannelId"].ToObject<ulong>(), CacheMode.AllowDownload) as ITextChannel;
+
+			// Load IUser belonging to owner
+			ulong ownerId = jsonConfig["botOwnerId"].ToObject<ulong>();
+			BotOwner = await client.GetUserAsync(ownerId);
+
+			// Load LogChannel
+			LogChannel = await client.GetChannelAsync(jsonConfig["logChannelId"].ToObject<ulong>()) as ITextChannel;
 			if (LogChannel == null) {
 				Logger.Log(LogSeverity.Info, "Config", "LogChannel could not be found.");
 			}
