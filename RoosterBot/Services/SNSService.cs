b7e58dfc81aeee81f7c41503499b5855605c6eab
DirkPC <d.kok.2000@gmail.com>
2019-06-18 19:40:47 +0200

    moved preprocessor statements around in SNSService

--- RoosterBot/Services/SNSService.cs
+++ RoosterBot/Services/SNSService.cs
@@ -15,50 +15,49 @@ namespace RoosterBot.Services {
 			m_SNSClient = new AmazonSimpleNotificationServiceClient(Amazon.RegionEndpoint.EUWest3);
 
 			m_ConfigService = config;
-			Program.Instance.ProgramStopping += (o, e) => { Dispose(); };
 #endif
 		}
 
-#if !DEBUG
 		internal async Task SendCriticalErrorNotificationAsync(string message) {
+#if !DEBUG
 			Logger.Log(Discord.LogSeverity.Info, "SNSService", "Sending error report to SNS (async)");
 			try {
 				await m_SNSClient.PublishAsync(m_ConfigService.SNSCriticalFailureARN, message);
 			} catch (AmazonSimpleNotificationServiceException ex) {
 				Logger.Log(Discord.LogSeverity.Error, "SNSService", "Failed to send error report to SNS (async)", ex);
 			}
+#else
+			await Task.CompletedTask;
+#endif
 		}
 
 		internal void SendCriticalErrorNotification(string message) {
+#if !DEBUG
 			Logger.Log(Discord.LogSeverity.Info, "SNSService", "Sending error report to SNS (sync)");
 			try {
 				m_SNSClient.Publish(m_ConfigService.SNSCriticalFailureARN, message);
 			} catch (AmazonSimpleNotificationServiceException ex) {
 				Logger.Log(Discord.LogSeverity.Error, "SNSService", "Failed to send error report to SNS (sync)", ex);
 			}
-		}
-#else
-		internal Task SendCriticalErrorNotificationAsync(string message) {
-			return Task.CompletedTask;
-		}
-
-		internal void SendCriticalErrorNotification(string message) { }
 #endif
+		}
 
-		#region IDisposable Support
+#region IDisposable Support
 		// Everything in this region was added by Visual Studio during code analysis, I don't understand most of it.
 		// I mean, I do, but why would you have a second method for this? Why not just stick with a regular Dispose()? I don't see the reason.
 		private bool disposedValue = false; // To detect redundant calls
 
 		protected virtual void Dispose(bool disposing) {
 			if (!disposedValue) {
-#if !DEBUG
 				if (disposing) {
+#if !DEBUG
 					if (m_SNSClient != null) { // No idea why, but the code analyzer will complain if you use null coalescence for this.
 						m_SNSClient.Dispose(); // It won't if you do it like this, and it won't even suggest you use coalescence.
 					}
+#endif
 				}
 
+#if !DEBUG
 				m_ConfigService = null;
 #endif
 
@@ -71,6 +70,6 @@ namespace RoosterBot.Services {
 			// Do not change this code. Put cleanup code in Dispose(bool disposing) above.
 			Dispose(true);
 		}
-		#endregion
+#endregion
 	}
 }
