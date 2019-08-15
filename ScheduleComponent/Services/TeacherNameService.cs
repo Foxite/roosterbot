377d4261fd3f490348ce9b59f55418dd5e88a677
foxite <d.kok.2000@gmail.com>
2019-08-15 22:44:51 +0200

    TeacherNameService has been modified to keep track of which guilds are allowed to see what data.

--- ScheduleComponent/Services/TeacherNameService.cs
+++ ScheduleComponent/Services/TeacherNameService.cs
@@ -1,19 +1,21 @@
 ﻿using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 using CsvHelper;
+using Discord;
 using RoosterBot;
 using ScheduleComponent.DataTypes;
 
 namespace ScheduleComponent.Services {
 	public class TeacherNameService {
-		private List<TeacherInfo> m_Records = new List<TeacherInfo>();
+		private List<GuildTeacherList> m_Records = new List<GuildTeacherList>();
 		
 		/// <summary>
 		/// Loads a CSV with teacher abbreviations into memory.
 		/// </summary>
-		public async Task ReadAbbrCSV(string path) {
-			Logger.Log(Discord.LogSeverity.Info, "TeacherNameService", $"Loading abbreviation CSV file {Path.GetFileName(path)}");
+		public async Task ReadAbbrCSV(string path, ulong[] allowedGuilds) {
+			Logger.Log(LogSeverity.Info, "TeacherNameService", $"Loading abbreviation CSV file {Path.GetFileName(path)}");
 
 			using (StreamReader reader = File.OpenText(path)) {
 				using (CsvReader csv = new CsvReader(reader, new CsvHelper.Configuration.Configuration() { Delimiter = "," })) {
@@ -39,15 +41,15 @@ namespace ScheduleComponent.Services {
 					}
 
 					lock (m_Records) {
-						m_Records.AddRange(currentRecords);
+						m_Records.Add(new GuildTeacherList(allowedGuilds, currentRecords));
 					}
 				}
 			}
-			Logger.Log(Discord.LogSeverity.Info, "TeacherNameService", $"Successfully loaded abbreviation CSV file {Path.GetFileName(path)}");
+			Logger.Log(LogSeverity.Info, "TeacherNameService", $"Successfully loaded abbreviation CSV file {Path.GetFileName(path)}");
 		}
 
-		public TeacherInfo GetRecordFromAbbr(string abbr) {
-			return m_Records.Find(record => record.Abbreviation == abbr);
+		public TeacherInfo GetRecordFromAbbr(ulong guild, string abbr) {
+			return GetAllowedRecordsForGuild(guild).First(record => record.Abbreviation == abbr);
 		}
 
 		public TeacherInfo[] GetRecordsFromAbbrs(string[] abbrs) {
@@ -68,12 +70,12 @@ namespace ScheduleComponent.Services {
 			return records.ToArray();
 		}
 		
-		public TeacherInfo[] Lookup(string nameInput, bool skipNoLookup = true) {
+		public TeacherInfo[] Lookup(ulong guild, string nameInput, bool skipNoLookup = true) {
 			nameInput = nameInput.ToLower();
 
 			List<TeacherInfo> records = new List<TeacherInfo>();
 
-			foreach (TeacherInfo record in m_Records) {
+			foreach (TeacherInfo record in GetAllowedRecordsForGuild(guild)) {
 				if (skipNoLookup && record.NoLookup)
 					continue;
 
@@ -85,8 +87,27 @@ namespace ScheduleComponent.Services {
 			return records.ToArray();
 		}
 		
-		public IReadOnlyList<TeacherInfo> GetAllRecords() {
-			return m_Records;
+		public IEnumerable<TeacherInfo> GetAllRecords(ulong guild) {
+			return GetAllowedRecordsForGuild(guild);
+		}
+
+		private IEnumerable<TeacherInfo> GetAllowedRecordsForGuild(ulong guild) {
+			return m_Records.Where(gtl => gtl.IsGuildAllowed(guild)).SelectMany(gtl => gtl.Teachers);
+		}
+
+		private class GuildTeacherList {
+			private ulong[] m_AllowedGuilds;
+			private List<TeacherInfo> m_Teachers;
+
+			public IReadOnlyList<TeacherInfo> Teachers => m_Teachers;
+
+			public GuildTeacherList(ulong[] allowedGuilds, List<TeacherInfo> teachers) {
+				m_AllowedGuilds = allowedGuilds;
+				m_Teachers = teachers;
+			}
+
+			public bool IsGuildAllowed(ulong guildId) => m_AllowedGuilds.Contains(guildId);
+			public bool IsGuildAllowed(IGuild guild) => IsGuildAllowed(guild.Id);
 		}
 	}
 }
