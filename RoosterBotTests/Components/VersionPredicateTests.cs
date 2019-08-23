03c940e92b61e450460fbdef071043e4ebafcff4
DirkPC <d.kok.2000@gmail.com>
2019-08-23 23:37:51 +0200

    added a unit test project, currently contains tests for Version and VersionPredicate

---
+++ RoosterBotTests/Components/VersionPredicateTests.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace RoosterBot.Tests {
+	[TestClass]
+	public class VersionPredicateTests {
+		[TestMethod]
+		public void ConstructorTest() {
+			Assert.That.DoesNotThrowException(() => { new VersionPredicate(1, 0, 0); });
+			Assert.That.DoesNotThrowException(() => { new VersionPredicate(1, 0, null); });
+			Assert.That.DoesNotThrowException(() => { new VersionPredicate(1, null, null); });
+			Assert.ThrowsException<ArgumentException>(() => { new VersionPredicate(1, null, 0); });
+		}
+
+		[TestMethod]
+		public void MatchTest() {
+			VersionPredicate predicate = new VersionPredicate(1, 2, 3);
+
+			Assert.IsTrue (predicate.Matches(new Version(1, 2, 3)));
+
+			Assert.IsFalse(predicate.Matches(new Version(1, 2, 4)));
+			Assert.IsFalse(predicate.Matches(new Version(1, 3, 3)));
+			Assert.IsFalse(predicate.Matches(new Version(2, 2, 3)));
+			Assert.IsFalse(predicate.Matches(new Version(2, 3, 3)));
+			Assert.IsFalse(predicate.Matches(new Version(2, 2, 4)));
+			Assert.IsFalse(predicate.Matches(new Version(2, 4, 3)));
+			
+			predicate = new VersionPredicate(1, 2, null);
+			Assert.IsTrue (predicate.Matches(new Version(1, 2, 3)));
+			Assert.IsTrue (predicate.Matches(new Version(1, 2, 4)));
+			Assert.IsFalse(predicate.Matches(new Version(1, 3, 3)));
+			Assert.IsFalse(predicate.Matches(new Version(2, 2, 3)));
+			Assert.IsFalse(predicate.Matches(new Version(2, 3, 3)));
+			
+			predicate = new VersionPredicate(1, null, null);
+			Assert.IsTrue (predicate.Matches(new Version(1, 2, 3)));
+			Assert.IsTrue (predicate.Matches(new Version(1, 2, 4)));
+			Assert.IsTrue (predicate.Matches(new Version(1, 3, 3)));
+			Assert.IsFalse(predicate.Matches(new Version(2, 2, 3)));
+			Assert.IsFalse(predicate.Matches(new Version(2, 3, 3)));
+		}
+	}
+}
---
+++ RoosterBotTests/Components/VersionTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RoosterBot.Tests {
+	[TestClass]
+	public class VersionTests {
+		[TestMethod]
+		public void OperatorTest() {
+			// == operator
+			Assert.IsTrue(new Version(1, 0, 0) == new Version(1, 0, 0));
+			Assert.IsFalse(new Version(1, 1, 0) == new Version(1, 0, 0));
+			Assert.IsFalse(new Version(1, 1, 0) == new Version(1, 0, 0));
+			Assert.IsFalse(new Version(1, 0, 1) == new Version(1, 0, 0));
+
+			// > operator
+			Assert.IsTrue(new Version(1, 0, 1) > new Version(1, 0, 0));
+			Assert.IsTrue(new Version(1, 1, 0) > new Version(1, 0, 0));
+			Assert.IsTrue(new Version(2, 0, 0) > new Version(1, 2, 3));
+			Assert.IsFalse(new Version(1, 2, 3) > new Version(2, 0, 0));
+
+			// < operator
+			Assert.IsFalse(new Version(1, 0, 1) < new Version(1, 0, 0));
+			Assert.IsFalse(new Version(1, 1, 0) < new Version(1, 0, 0));
+			Assert.IsFalse(new Version(2, 0, 0) < new Version(1, 2, 3));
+			Assert.IsTrue(new Version(1, 2, 3) < new Version(2, 0, 0));
+			
+			// >=, <=, and != simply call the other operators, so we don't really need to test these.
+		}
+	}
+}
