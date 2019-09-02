5c1bb23b9c213d18856aac5efd4fb8811451205a
DirkPC <d.kok.2000@gmail.com>
2019-09-02 23:47:50 +0200

    RoosterBot now uses semantic versioning, split Constants.tt into a partial class, the old VersionString is now called DeployVersion

---
+++ RoosterBot/Constants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RoosterBot {
+	public static partial class Constants {
+		public static readonly DateTime FirstReleaseDate = new DateTime(2018, 10, 1);
+		public static readonly int DaysSinceV1 = (int) (CurrentReleaseDate - FirstReleaseDate).TotalDays;
+		public static readonly string DeployVersion = DaysSinceV1.ToString() + "." + ReleasesToday;
+
+		public static readonly Version RoosterBotVersion = new Version(2, 0, 0);
+		public static string VersionString => RoosterBotVersion.ToString();
+	}
+}
