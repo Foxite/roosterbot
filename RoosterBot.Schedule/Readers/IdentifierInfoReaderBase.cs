54869b0eefac1f01f227ab4ffbb85e4a5d764397
DirkPC <d.kok.2000@gmail.com>
2019-09-02 21:59:28 +0200

    this is hard because we need specific error messages, for example StudentSetInfo failing because the mentioned user is not known vs a direct class reference being invalid

--- RoosterBot.Schedule/Readers/IdentifierInfoReaderBase.cs
+++ RoosterBot.Schedule/Readers/IdentifierInfoReaderBase.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace RoosterBot.Schedule {
 	public class IdentifierInfoReaderBase<T> : TypeReader where T : IdentifierInfo {
-		// TODO We can probably move all the matching code out of the typereaders into IdentifierValidationService delegates
 		public override Task<TypeReaderResult> ReadAsync(ICommandContext context, string input, IServiceProvider services) {
 			T result = services.GetService<IdentifierValidationService>().Validate<T>(context, input);
 			if (result is null) {
