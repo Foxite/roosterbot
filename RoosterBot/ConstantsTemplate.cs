9db4342819d7d4181a6475c25acfcc80e4c8123e
foxite <d.kok.2000@gmail.com>
2019-09-03 19:27:37 +0200

    fixed some csproj problems that I probably forgot to push

--- RoosterBot/ConstantsTemplate.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is auto-generated.
-using System;
-
-namespace RoosterBot {
-	public static partial class Constants {
-		public static readonly DateTime CurrentReleaseDate = new DateTime(2019, 9, 2);
-		public static readonly int ReleasesToday = 12;
-	}
-}
