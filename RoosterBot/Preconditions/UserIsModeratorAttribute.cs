e42dff20b778760a92db20aa3f9da45391902352
foxite <d.kok.2000@gmail.com>
2019-09-19 22:11:25 +0200

    localized UserIsModeratorAttribute

--- RoosterBot/Preconditions/UserIsModeratorAttribute.cs
+++ RoosterBot/Preconditions/UserIsModeratorAttribute.cs
@@ -6,19 +6,18 @@ using Discord.Commands;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace RoosterBot {
-	// TODO localize
 	public sealed class UserIsModeratorAttribute : RoosterPreconditionAttribute {
-		public override string Summary => "Je moet moderator zijn om dit te doen.";
+		public override string Summary => "#UserIsModeratorAttribute_Summary";
 
 		public override Task<PreconditionResult> CheckPermissionsAsync(ICommandContext context, CommandInfo command, IServiceProvider services) {
 			if (context.User is IGuildUser user) {
 				if (services.GetService<ConfigService>().StaffRoles.Intersect(user.RoleIds).Any()) {
 					return Task.FromResult(PreconditionResult.FromSuccess());
 				} else {
-					return Task.FromResult(PreconditionResult.FromError(":no_entry: Je moet moderator privileges hebben om dat te doen."));
+					return Task.FromResult(PreconditionResult.FromError("#UserIsModeratorAttribute_CheckFailed"));
 				}
 			} else {
-				return Task.FromResult(PreconditionResult.FromError(":x: Dit werkt alleen in een server."));
+				return Task.FromResult(PreconditionResult.FromError("#UserIsModeratorAttribute_GuildsOnly"));
 			}
 		}
 	}
