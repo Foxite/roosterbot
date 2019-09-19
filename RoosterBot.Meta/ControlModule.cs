8328f0109011120d173cb5926cf9efcc82add308
DirkPC <d.kok.2000@gmail.com>
2019-09-19 15:10:37 +0200

    removed !send and !delete

--- RoosterBot.Meta/ControlModule.cs
+++ RoosterBot.Meta/ControlModule.cs
@@ -12,27 +12,6 @@ namespace RoosterBot.Meta {
 			return Task.CompletedTask;
 		}
 
-		[Command("send"), RequireBotManager]
-		public async Task SendMessageCommand(IMessageChannel channel, [Remainder] string message) {
-			try {
-				await channel.SendMessageAsync(message);
-			} catch {
-				await ReplyAsync("Error sending message");
-			}
-		}
-
-		[Command("delete"), RequireBotManager]
-		public async Task DeleteMessageCommand(IMessageChannel channel, ulong msg) {
-			IMessage message = await channel.GetMessageAsync(msg);
-			if (message != null) {
-				try {
-					await message.DeleteAsync();
-				} catch {
-					await ReplyAsync("Error deleting message");
-				}
-			} else {
-				await ReplyAsync("Message not found");
-			}
 		}
 	}
 }
