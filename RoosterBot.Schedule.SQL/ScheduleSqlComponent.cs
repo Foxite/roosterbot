098125659e4e3ab0504a46378895082bf83bd375
foxite <d.kok.2000@gmail.com>
2019-09-22 22:06:19 +0200

    it now has the necessary tag

--- RoosterBot.Schedule.SQL/ScheduleSqlComponent.cs
+++ RoosterBot.Schedule.SQL/ScheduleSqlComponent.cs
@@ -8,6 +8,8 @@ namespace RoosterBot.Schedule.SQL {
 
 		public override Version ComponentVersion => new Version(0, 1, 0);
 
+		public override string[] Tags => new[] { "IUserClassesService" };
+
 		public override Task AddServicesAsync(IServiceCollection services, string configPath) {
 			m_UCS = new SqlDatabaseUserClassesService(Path.Combine(configPath, "Config.json"));
 			return Task.CompletedTask;
