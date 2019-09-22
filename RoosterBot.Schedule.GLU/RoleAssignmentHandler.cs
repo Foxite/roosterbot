9ba471e22a4e9108e23e23c7e300e083421073aa
foxite <d.kok.2000@gmail.com>
2019-09-22 22:04:15 +0200

    IUserClassesService.UserChangedClass now takes an IUser instead of IGuildUser

--- RoosterBot.Schedule.GLU/RoleAssignmentHandler.cs
+++ RoosterBot.Schedule.GLU/RoleAssignmentHandler.cs
@@ -2,15 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using Discord;
+using Discord.WebSocket;
 
 namespace RoosterBot.Schedule.GLU {
 	public class RoleAssignmentHandler {
 		private IReadOnlyDictionary<string, ulong[]> m_Roles;
+		private readonly IDiscordClient m_Client;
 
 		// TODO actually start using this
 		// The problem is that it needs both a service and a config path, which are mutually incompatible given the component initialization process.
 		// We probably need to load the config during AddServices and then get the UCS during AddModules, but that's for Tomorrow-Me to program.
-		public RoleAssignmentHandler(IUserClassesService ucs) {
+		public RoleAssignmentHandler(IDiscordClient client, IUserClassesService ucs) {
 			// TODO replace with config file
 			ulong[] yearRoles = new ulong[] { 494531025473503252, 494531131606040586, 494531205966987285, 494531269796036618 };
 
@@ -32,26 +34,31 @@ namespace RoosterBot.Schedule.GLU {
 			m_Roles = roles;
 
 			ucs.UserChangedClass += OnUserChangedClass;
+			m_Client = client;
 		}
 
-		private async void OnUserChangedClass(IGuildUser user, StudentSetInfo oldSSI, StudentSetInfo newSSI) {
+		private async void OnUserChangedClass(IUser user, StudentSetInfo oldSSI, StudentSetInfo newSSI) {
 			// Assign roles
 			try {
-				IEnumerable<IRole> newRoles = GetRolesForStudentSet(user.Guild, newSSI);
-				if (oldSSI != null) {
-					IEnumerable<IRole> oldRoles = GetRolesForStudentSet(user.Guild, oldSSI);
-					IEnumerable<IRole> keptRoles = oldRoles.Intersect(newRoles);
+				SocketGuild guild = (user as SocketUser)?.MutualGuilds.Where(thisGuild => thisGuild.Id == 278586698877894657).Single();
+				if (guild != null) {
+					IGuildUser guildUser = guild.GetUser(user.Id);
+					IEnumerable<IRole> newRoles = GetRolesForStudentSet(guild, newSSI);
+					if (oldSSI != null) {
+						IEnumerable<IRole> oldRoles = GetRolesForStudentSet(guild, oldSSI);
+						IEnumerable<IRole> keptRoles = oldRoles.Intersect(newRoles);
 
-					oldRoles = oldRoles.Except(keptRoles);
-					newRoles = newRoles.Except(keptRoles);
+						oldRoles = oldRoles.Except(keptRoles);
+						newRoles = newRoles.Except(keptRoles);
 
-					if (oldRoles.Any()) {
-						await user.RemoveRolesAsync(oldRoles);
+						if (oldRoles.Any()) {
+							await guildUser.RemoveRolesAsync(oldRoles);
+						}
 					}
-				}
 
-				if (newRoles.Any()) {
-					await user.AddRolesAsync(newRoles);
+					if (newRoles.Any()) {
+						await guildUser.AddRolesAsync(newRoles);
+					}
 				}
 			} catch (Exception) {
 				// Ignore, either we did not have permission or the roles were not found. In either case, it doesn't matter.
