0413d602dada78773f14976c6cdbbb3b36a51f42
foxite <d.kok.2000@gmail.com>
2019-10-07 12:21:25 +0200

    fixed broken constants template

--- RoosterBot/Constants.cs
+++ RoosterBot/Constants.cs
@@ -1,11 +1,5 @@
-﻿using System;
-
-namespace RoosterBot {
-	public static partial class Constants {
-		public static readonly DateTime FirstReleaseDate = new DateTime(2018, 10, 1);
-		public static readonly int DaysSinceV1 = (int) (CurrentReleaseDate - FirstReleaseDate).TotalDays;
-		public static readonly string DeployVersion = DaysSinceV1.ToString() + "." + ReleasesToday;
-
+﻿namespace RoosterBot {
+	public static class Constants {
 		public static readonly Version RoosterBotVersion = new Version(2, 0, 0);
 		public static string VersionString => RoosterBotVersion.ToString();
 	}
