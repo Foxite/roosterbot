aea8102ec099526f6491f5bda7866cd31c2df77f
foxite <d.kok.2000@gmail.com>
2019-10-07 15:31:09 +0200

    moved WeatherInfo into its own file in DataTypes, it's probably going to get bigger

---
+++ RoosterBot.Weather/DataTypes/WeatherInfo.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace RoosterBot.Weather {
+	public class WeatherInfo {
+		public CityInfo City { get; }
+
+		/// <summary>
+		/// Celcius
+		/// </summary>
+		public float Temperature { get; }
+
+		// TODO properties with wind, pressure, humidity, precipation and whatnot
+		// TODO constructor, ideally it will take the API response and fill itself in, so we won't have to assign 20 parameters
+
+		public string Present() {
+			return $"{City.Name}, {City.Region}: {Math.Round(Temperature, 1)} °C";
+		}
+	}
+}
--- RoosterBot.Weather/Services/WeatherService.cs
+++ RoosterBot.Weather/Services/WeatherService.cs
@@ -12,20 +12,4 @@ namespace RoosterBot.Weather {
 		public Task<WeatherInfo> GetWeatherForecastAsync(CityInfo city, TimeSpan timeFromNow) => GetWeatherForecastAsync(city, DateTime.Now + timeFromNow);
 		public Task<WeatherInfo> GetWeatherForecastAsync(CityInfo city, DateTime dateTime) => throw new NotImplementedException();
 	}
-
-	public class WeatherInfo {
-		public CityInfo City { get; }
-
-		/// <summary>
-		/// Celcius
-		/// </summary>
-		public float Temperature { get; }
-
-		// TODO properties with wind, pressure, humidity, precipation and whatnot
-		// TODO constructor, ideally it will take the API response and fill itself in, so we won't have to assign 20 parameters
-
-		public string Present() {
-			return $"{City.Name}, {City.Region}: {Math.Round(Temperature, 1)} °C";
-		}
-	}
 }
