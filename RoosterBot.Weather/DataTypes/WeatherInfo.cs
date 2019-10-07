71cf3bc8f54f7dc72016f29ca77c1836771ebd85
DirkPC <d.kok.2000@gmail.com>
2019-10-07 17:26:33 +0200

    finished code conversion

--- RoosterBot.Weather/DataTypes/WeatherInfo.cs
+++ RoosterBot.Weather/DataTypes/WeatherInfo.cs
@@ -38,53 +38,46 @@ namespace RoosterBot.Weather {
 
 		private string GetDescription() {
 			switch (m_WeatherCode) {
-				case 200:
-					return "Onweer met lichte regen";
-				case 201:
-					return "Onweer met regen";
-				case 202:
-					return "Onweer met zware regen";
-				case 230:
-					return "Onweer met lichte motregen";
-				case 231:
-					return "Onweer met motregen";
-				case 232:
-					return "Onweer met hevige motregen";
-				case 233:
-					return "Onweer met hagel";
-				case 300:
-				case 301:
-				case 302:
-				case 500:
-				case 501:
-				case 502:
-				case 511:
-				case 520:
-				case 521:
-				case 522:
-				case 600:
-				case 601:
-				case 602:
-				case 611:
-				case 612:
-				case 621:
-				case 622:
-				case 623:
-				case 700:
-				case 711:
-				case 721:
-				case 731:
-				case 741:
-				case 751:
-				case 800:
-				case 801:
-				case 802:
-				case 803:
-				case 804:
-					return "TODO";
-				case 900:
-					return "Onbekend";
+				case 200: return ":cloud_lightning: Onweer met lichte regen";
+				case 201: return ":thunder_cloud_rain: Onweer met regen";
+				case 202: return ":thunder_cloud_rain: Onweer met zware regen";
+				case 230: return ":cloud_lightning: Onweer met lichte motregen";
+				case 231: return ":thunder_cloud_rain: Onweer met motregen";
+				case 232: return ":thunder_cloud_rain: Onweer met hevige motregen";
+				case 233: return ":thunder_cloud_rain: Onweer met hagel";
+				case 300: return "Lichte motregen";
+				case 301: return "Motregen";
+				case 302: return "Hevige motregen";
+				case 500: return "Lichte regen";
+				case 501: return "Regen";
+				case 502: return "Zware regen";
+				case 511: return "Vriezende regen";
+				case 520: return "Lichte buien";
+				case 521: return "Buien";
+				case 522: return "Hevige buien";
+				case 600: return "Lichte sneeuw";
+				case 601: return "Sneeuw";
+				case 602: return "Hevige sneeuw";
+				case 610: return "Sneeuw en regen";
+				case 611: return "Ijzel";
+				case 612: return "Hevige ijzel";
+				case 621: return "Sneeuwbuien";
+				case 622: return "Zware sneeuwbuien";
+				case 623: return "Sneeuwvlagen";
+				case 700: return "Mist";
+				case 711: return "Rook";
+				case 721: return "Nevel";
+				case 731: return "Zand/stof";
+				case 741: return "Mist";
+				case 751: return "Vriezende mist";
+				case 800: return "Klare lucht";
+				case 801: return "Enkele wolken";
+				case 802: return "Licht bewolkt";
+				case 803: return "Overwegend bewolkt";
+				case 804: return "Bewolkt";
+				case 900: return "Onbekend";
 				default:
+					Logger.Error("WeatherInfo", "Unknown code " + m_WeatherCode);
 					return "Gefeliciteerd, je krijg een gratis Mars-reep van de bot eigenaar. Foutcode D" + m_WeatherCode;
 			}
 		}
