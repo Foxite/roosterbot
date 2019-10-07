c668e5b5940b6ab27b106dd177d1d3a1e0abb7f7
foxite <d.kok.2000@gmail.com>
2019-10-07 15:30:43 +0200

    fleshed out module, assuming I did this right, we don't need to touch this anymore

--- RoosterBot.Weather/Modules/WeatherModule.cs
+++ RoosterBot.Weather/Modules/WeatherModule.cs
@@ -5,6 +5,9 @@ using System.Threading.Tasks;
 namespace RoosterBot.Weather {
 	// TODO localize? The API is not restricted to the Netherlands but we would have to provide the entire cities file, which takes considerable time to read
 	// Alternatively we could provide a cities file with only the cities in countries we are localized to
+
+	// The free license for Weatherbit allows 500 calls per month, so we shouldn't try to show too much data at once. If the user wants to know the weather 3 hours from now, they
+	// should request just that, instead of being shown a per-hour forecast of the entire day (24 calls), while they only care about one data point.
 	[Name("weer")]
 	public class WeatherModule : RoosterModuleBase {
 		public WeatherService Weather { get; set; }
@@ -17,16 +20,22 @@ namespace RoosterBot.Weather {
 
 		[Command(RunMode = RunMode.Async), Alias("dag")]
 		public async Task GetDayForecastCommand(DayOfWeek day, CityInfo city) {
-			// Get the forecast for today at {hours} o clock
+			// Get the forecast for the day at {hours} o clock
+			DateTime date = DateTime.Today.AddDays((int) (day - DateTime.Today.DayOfWeek));
 			Task<WeatherInfo> getHourForecast(int hours) {
-				return Weather.GetWeatherForecastAsync(city, DateTime.Today.AddHours(hours));
+				return Weather.GetWeatherForecastAsync(city, date.AddHours(hours));
 			}
 			
-			WeatherInfo morning   = await getHourForecast(8);
-			WeatherInfo afternoon = await getHourForecast(12);
-			WeatherInfo evening   = await getHourForecast(18);
+			WeatherInfo morning = await getHourForecast(8);
+			WeatherInfo noon    = await getHourForecast(12);
+			WeatherInfo evening = await getHourForecast(18);
 
-			string response = $"{city.Name}: Weer op {day}";
+			string response = $"{city.Name}: Weer op {day}\n";
+			response +=   "08:00: " + morning.Present();
+			response += "\n12:00: " + noon.Present();
+			response += "\n18:00: " + evening.Present();
+
+			ReplyDeferred(response);
 		}
 
 		// TODO Discord.NET features a builtin TimeSpan reader, however it probably won't work very well in Dutch (or any language besides English probably)
