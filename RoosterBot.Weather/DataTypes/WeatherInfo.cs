b1185c99f6f5693d7e2d2135d9cba29eacf776d9
foxite <d.kok.2000@gmail.com>
2019-10-07 17:08:59 +0200

    fleshed out fields (apparently not much is necessary), added a stub GetDescription function that converts the WeatherBit code to a string

--- RoosterBot.Weather/DataTypes/WeatherInfo.cs
+++ RoosterBot.Weather/DataTypes/WeatherInfo.cs
@@ -1,19 +1,92 @@
 ﻿using System;
+using Newtonsoft.Json.Linq;
 
 namespace RoosterBot.Weather {
 	public class WeatherInfo {
+		// Weather code from WeatherBit
+		private short m_WeatherCode;
+
 		public CityInfo City { get; }
 
 		/// <summary>
 		/// Celcius
 		/// </summary>
 		public float Temperature { get; }
+		public float ApparentTemperature { get; }
 
-		// TODO properties with wind, pressure, humidity, precipation and whatnot
-		// TODO constructor, ideally it will take the API response and fill itself in, so we won't have to assign 20 parameters
+		/// <summary>
+		/// m/s
+		/// </summary>
+		public float WindSpeed { get; }
+
+		public string WindDirectionAbbr { get; }
+
+		internal WeatherInfo(CityInfo city, JObject jObject) {
+			City = city;
+
+			JObject info = jObject["data"][0].ToObject<JObject>();
+
+			Temperature = info["temp"].ToObject<float>();
+			ApparentTemperature = info["app_temp"].ToObject<float>();
+
+			m_WeatherCode = info["weather"]["code"].ToObject<short>();
+		}
 
 		public string Present() {
-			return $"{City.Name}, {City.Region}: {Math.Round(Temperature, 1)} °C";
+			return $"{City.Name}, {City.Region}: {Math.Round(Temperature, 1)} °C\n{GetDescription()}";
+		}
+
+		private string GetDescription() {
+			switch (m_WeatherCode) {
+				case 200:
+					return "Onweer met lichte regen";
+				case 201:
+					return "Onweer met regen";
+				case 202:
+					return "Onweer met zware regen";
+				case 230:
+					return "Onweer met lichte motregen";
+				case 231:
+					return "Onweer met motregen";
+				case 232:
+					return "Onweer met hevige motregen";
+				case 233:
+					return "Onweer met hagel";
+				case 300:
+				case 301:
+				case 302:
+				case 500:
+				case 501:
+				case 502:
+				case 511:
+				case 520:
+				case 521:
+				case 522:
+				case 600:
+				case 601:
+				case 602:
+				case 611:
+				case 612:
+				case 621:
+				case 622:
+				case 623:
+				case 700:
+				case 711:
+				case 721:
+				case 731:
+				case 741:
+				case 751:
+				case 800:
+				case 801:
+				case 802:
+				case 803:
+				case 804:
+					return "TODO";
+				case 900:
+					return "Onbekend";
+				default:
+					return "Gefeliciteerd, je krijg een gratis Mars-reep van de bot eigenaar. Foutcode D" + m_WeatherCode;
+			}
 		}
 	}
 }
