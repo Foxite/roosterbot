31a34d6afb26d95984e9d3a173d4eebac9a8a44c
foxite <d.kok.2000@gmail.com>
2019-10-08 09:21:22 +0200

    fixed an additional error

--- RoosterBot.Weather/Modules/WeatherModule.cs
+++ RoosterBot.Weather/Modules/WeatherModule.cs
@@ -49,18 +49,19 @@ namespace RoosterBot.Weather {
 		}
 
 		private async Task RespondDayForecast(CityInfo city, DateTime date) {
+			string response;
 			using (IDisposable typingState = Context.Channel.EnterTypingState()) {
 				WeatherInfo[] dayForecast = await Weather.GetDayForecastAsync(city, date);
 
 				string relativeDateReference = DateTimeUtil.GetRelativeDateReference(date, Culture);
 
-				string response = $"{city.Name}: Weer {relativeDateReference}\n";
+				response = $"{city.Name}: Weer {relativeDateReference}\n";
 				response += "08:00: " + dayForecast[0].Present();
 				response += "\n\n12:00: " + dayForecast[1].Present();
 				response += "\n\n18:00: " + dayForecast[2].Present();
-
-				ReplyDeferred(response);
 			}
+
+			ReplyDeferred(response);
 		}
 	}
 }
