2d6ed5f95081f5c30e2a378dde5c191588d3f29f
foxite <d.kok.2000@gmail.com>
2019-10-09 12:18:30 +0200

    PublicTransit help text

--- RoosterBot.PublicTransit/PublicTransitComponent.cs
+++ RoosterBot.PublicTransit/PublicTransitComponent.cs
@@ -36,14 +36,14 @@ namespace RoosterBot.PublicTransit {
 
 			registerModules(new[] { await commandService.AddModuleAsync<PTModule>(services) });
 
-			string helpText = "Met `!ov` kan je informatie opzoeken via de NS reisplanner.\n";
+			string helpText = "Met `{0}ov` kan je informatie opzoeken via de NS reisplanner.\n";
 			helpText += "Dit ondersteunt alleen treinreizen, dus geen bussen. Ook kan je alleen treinstations in Nederland opzoeken, en geen steden, adressen, of andere plaatsen.\n";
-			helpText += "Dit is hoe je de command gebruikt: `!ov <naam van vertrekstation>, <naam van aankomststation>`\n";
-			helpText += "Als ik niet het goede station heb gevonden, kun een code invullen die je met `!stations` kan vinden. (Hieronder meer.) Gebruik dan `!ov $CODE`.\n";
+			helpText += "Dit is hoe je de command gebruikt: `{0}ov <naam van vertrekstation>, <naam van aankomststation>`\n";
+			helpText += "Als ik niet het goede station heb gevonden, kun een code invullen die je met `{0}stations` kan vinden. (Hieronder meer.) Gebruik dan `{0}ov $CODE`.\n";
 			helpText += "Je kunt het vertrekstation overslaan. In dit geval wordt Utrecht Vaartsche Rijn gebruikt, want dit is om de hoek bij de school.\n\n";
 
-			helpText += "Je kunt stations opzoeken met `!stations <naam van station>`";
-			help.AddHelpSection(this, "trein", helpText);
+			helpText += "Je kunt stations opzoeken met `{0}stations <naam van station>`";
+			help.AddHelpSection(this, "trein", string.Format(helpText, services.GetService<ConfigService>().CommandPrefix));
 		}
 
 		protected override void Dispose(bool disposing) {
