04c38a6e455b6041676a3d090908c494f0a7d629
DirkPC <d.kok.2000@gmail.com>
2019-10-13 17:00:40 +0200

    changed flow of class assignment

--- ScheduleComponent/Modules/UserClassModule.cs
+++ ScheduleComponent/Modules/UserClassModule.cs
@@ -27,37 +27,39 @@ namespace ScheduleComponent.Modules {
 			response += "Gebruik bijvoorbeeld `!ik <jouw klas>` om dit te veranderen.";
 			await ReplyAsync(response);
 		}
-		
+
 		[Command("ik"), Summary("Stel in in welke klas jij zit."), RequireContext(ContextType.Guild, ErrorMessage = "Deze command moet binnen een server worden gebruikt.")]
 		public async Task SetClassForUser([Name("jouw klas")] string studentSet) {
+			StudentSetInfo oldStudentSet = await Classes.GetClassForDiscordUser(Context.User);
+			string clazzName = studentSet.ToUpper();
+
 			try {
-				StudentSetInfo oldStudentSet = await Classes.GetClassForDiscordUser(Context.User);
-				string clazzName = studentSet.ToUpper();
-
 				await Classes.SetClassForDiscordUser(Context.User, clazzName);
-				await ReplyAsync("Genoteerd: jij zit in " + clazzName + ".");
-
-				StudentSetInfo newStudentSet = new StudentSetInfo() { ClassName = clazzName };
-
-				IGuildUser guildUser = Context.User as IGuildUser;
-
-				try {
-					IRole[] oldRoles;
-					if (oldStudentSet == null) {
-						oldRoles = new[] { Context.Guild.GetRole(278937741478330389) };
-					} else {
-						oldRoles = Roles.GetRolesForStudentSet(Context.Guild, oldStudentSet).ToArray();
-					}
-					IRole[] newRoles = Roles.GetRolesForStudentSet(Context.Guild, newStudentSet).ToArray();
-
-					await guildUser.RemoveRolesAsync(oldRoles);
-					await guildUser.AddRolesAsync(newRoles);
-				} catch (Exception e) {
-					Logger.Error("UserClassModule", "Failed to assign role", e);
-					await Config.BotOwner.SendMessageAsync("Failed to assign role: " + e.ToString());
-				}
 			} catch (ArgumentException) {
 				await ReplyAsync("Dat is geen klas.");
+				return;
+			}
+
+			await ReplyAsync("Genoteerd: jij zit in " + clazzName + ".");
+
+			StudentSetInfo newStudentSet = new StudentSetInfo() { ClassName = clazzName };
+
+			IGuildUser guildUser = Context.User as IGuildUser;
+
+			try {
+				IRole[] oldRoles;
+				if (oldStudentSet == null) {
+					oldRoles = new[] { Context.Guild.GetRole(278937741478330389u) };
+				} else {
+					oldRoles = Roles.GetRolesForStudentSet(Context.Guild, oldStudentSet).ToArray();
+				}
+				IRole[] newRoles = Roles.GetRolesForStudentSet(Context.Guild, newStudentSet).ToArray();
+
+				await guildUser.RemoveRolesAsync(oldRoles);
+				await guildUser.AddRolesAsync(newRoles);
+			} catch (Exception e) {
+				Logger.Error("UserClassModule", "Failed to assign role", e);
+				await Config.BotOwner.SendMessageAsync("Failed to assign role: " + e.ToString());
 			}
 		}
 	}
