9abdc1b98cbde87c7c32a2e3acc8562ed73a9ebb
foxite <d.kok.2000@gmail.com>
2019-10-16 16:36:50 +0200

    localized WeatherModule

--- RoosterBot.Weather/Modules/WeatherModule.cs
+++ RoosterBot.Weather/Modules/WeatherModule.cs
@@ -6,11 +6,11 @@ using RoosterBot.DateTimeUtils;
 namespace RoosterBot.Weather {
 	// The free license for Weatherbit allows 500 calls per month, so we shouldn't try to show too much data at once. If the user wants to know the weather 3 hours from now, they
 	// should request just that, instead of being shown a per-hour forecast of the entire day (24 calls), while they only care about one data point.
-	[Name("weer"), Group("weer")]
+	[Name("weer"), Group("weer"), LocalizedModule("nl-NL", "en-US"]
 	public class WeatherModule : RoosterModuleBase {
 		public WeatherService Weather { get; set; }
 
-		[Command(RunMode = RunMode.Async), Alias("in")]
+		[Command("#WeatherModule_CurrentWeather_Aliases", RunMode = RunMode.Async), Alias("#WeatherModule_CurrentWeather_Name")]
 		public async Task GetCurrentWeatherCommand([Remainder] CityInfo city) {
 			WeatherInfo weather;
 			using (IDisposable typingState = Context.Channel.EnterTypingState()) {
@@ -19,14 +19,14 @@ namespace RoosterBot.Weather {
 			ReplyDeferred(weather.Present(DateTime.Now, Culture));
 		}
 
-		[Command(RunMode = RunMode.Async), Alias("op")]
+		[Command("#WeatherModule_DayForecast_Name", RunMode = RunMode.Async), Alias("#WeatherModule_DayForecast_Aliases")]
 		public async Task GetDayForecastCommand(DayOfWeek day, [Remainder] CityInfo city) {
 			// Get the forecast for the day
 			DateTime date = DateTime.Today.AddDays(day - DateTime.Today.DayOfWeek);
 			await RespondDayForecast(city, date);
 		}
 
-		[Command(RunMode = RunMode.Async), Alias("op")]
+		[Command("#WeatherModule_TimeForecast_Aliases", RunMode = RunMode.Async), Alias("#WeatherModule_TimeForecast_Aliases")]
 		public async Task GetDayForecastCommand(DayOfWeek day, TimeSpan timeOffset, [Remainder] CityInfo city) {
 			DateTime datetime;
 			WeatherInfo weather;
@@ -38,7 +38,7 @@ namespace RoosterBot.Weather {
 			ReplyDeferred(weather.Present(datetime, Culture));
 		}
 
-		[Command("over", RunMode = RunMode.Async)]
+		[Command("#WeatherModule_UnitForecast_Name", RunMode = RunMode.Async), Alias("#WeatherModule_UnitForecast_Aliases")]
 		public async Task GetForecastCommand(int amount, string unit, [Remainder] CityInfo city) {
 			if (amount < 1) {
 				await MinorError("Ik kan niet terug kijken.");
