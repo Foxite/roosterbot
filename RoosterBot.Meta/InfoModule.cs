b8cac7ab3827278d1f5b866bcee7cfa9ab086c18
DirkPC <d.kok.2000@gmail.com>
2019-10-28 22:35:07 +0100

    added InfoModule which tells the user which RB version we're on, as well as components and a github link

---
+++ RoosterBot.Meta/InfoModule.cs
@@ -0,0 +1,19 @@
+﻿using Discord.Commands;
+using System.Threading.Tasks;
+
+namespace RoosterBot.Meta {
+	public class InfoModule : RoosterModuleBase {
+		[Command("info")]
+		public Task InfoCommand() {
+			string response = GetString("InfoModule_InfoCommand_Pretext", Constants.VersionString);
+			foreach (ComponentBase component in Program.Instance.Components.GetComponents()) {
+				response += "\n" + GetString("InfoModule_InfoCommand_ListItem", component.Name, component.ComponentVersion.ToString());
+			}
+			response += "\n" + GetString("InfoModule_InfoCommand_PostText", "https://github.com/Foxite/roosterbot"); // Hardcoded for now
+			//response += "\nAlle code is beschikbaar op Github: ";
+			ReplyDeferred(response);
+
+			return Task.CompletedTask;
+		}
+	}
+}
