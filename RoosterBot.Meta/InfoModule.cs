aa040090db4a17886ca48b7676c939b250f0e2fe
foxite <d.kok.2000@gmail.com>
2019-10-29 09:37:19 +0100

    added summary and module name to Info

--- RoosterBot.Meta/InfoModule.cs
+++ RoosterBot.Meta/InfoModule.cs
@@ -2,8 +2,9 @@
 using System.Threading.Tasks;
 
 namespace RoosterBot.Meta {
+	[Name("info")]
 	public class InfoModule : RoosterModuleBase {
-		[Command("info")]
+		[Command("info"), Summary("#InfoModule_InfoCommand_Summary")]
 		public Task InfoCommand() {
 			string response = GetString("InfoModule_InfoCommand_Pretext", Constants.VersionString);
 			foreach (ComponentBase component in Program.Instance.Components.GetComponents()) {
