205457120d494e5f8dcea68afc8de7fef88fcdf9
foxite <d.kok.2000@gmail.com>
2019-11-05 10:04:50 +0100

    made MockUCS null-aware

--- RoosterBot.Schedule.MockUCS/MockUserClassesService.cs
+++ RoosterBot.Schedule.MockUCS/MockUserClassesService.cs
@@ -15,15 +15,15 @@ namespace RoosterBot.Schedule.MockUCS {
 			m_UserClasses = new ConcurrentDictionary<IUser, StudentSetInfo>();
 		}
 
-		public event Action<IGuildUser, StudentSetInfo, StudentSetInfo> UserChangedClass;
+		public event Action<IGuildUser, StudentSetInfo?, StudentSetInfo>? UserChangedClass;
 
-		public Task<StudentSetInfo> GetClassForDiscordUserAsync(ICommandContext context, IUser user) {
-			m_UserClasses.TryGetValue(user, out StudentSetInfo ssi);
+		public Task<StudentSetInfo?> GetClassForDiscordUserAsync(ICommandContext context, IUser user) {
+			m_UserClasses.TryGetValue(user, out StudentSetInfo? ssi);
 			return Task.FromResult(ssi);
 		}
 
-		public Task<StudentSetInfo> SetClassForDiscordUserAsync(ICommandContext context, IGuildUser user, StudentSetInfo ssi) {
-			m_UserClasses.TryGetValue(user, out StudentSetInfo old);
+		public Task<StudentSetInfo?> SetClassForDiscordUserAsync(ICommandContext context, IGuildUser user, StudentSetInfo ssi) {
+			m_UserClasses.TryGetValue(user, out StudentSetInfo? old);
 			m_UserClasses[user] = ssi;
 			if (user is IGuildUser guildUser) {
 				UserChangedClass?.Invoke(guildUser, old, ssi);
