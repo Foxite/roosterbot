8f666a567311eccbba78dd8a2f622906f4642e20
foxite <d.kok.2000@gmail.com>
2019-11-10 22:23:47 +0100

    added UserConfigService which provides user-specific overrides to GuildConfigService

---
+++ RoosterBot.Meta/FileUserConfigService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Newtonsoft.Json.Linq;
+
+namespace RoosterBot.Meta {
+	public class FileUserConfigService : UserConfigService {
+		private readonly string m_ConfigPath;
+		private IDictionary<ulong, UserConfig> m_Configs;
+
+		public FileUserConfigService(string configPath) {
+			IDictionary<string, JToken> jsonConfig = JObject.Parse(File.ReadAllText(configPath));
+			m_Configs = jsonConfig.ToDictionary(
+				/* Key */ kvp => ulong.Parse(kvp.Key),
+				/* Val */ kvp => {
+					JObject userJO = kvp.Value.ToObject<JObject>();
+					string cultureString = userJO["culture"].ToObject<string>();
+					return new UserConfig(
+						this,
+						cultureString.Length == 0 ? CultureInfo.GetCultureInfo(cultureString) : null,
+						ulong.Parse(kvp.Key),
+						userJO["customData"].ToObject<JObject>()
+					);
+				}
+			);
+			m_ConfigPath = configPath;
+		}
+
+		public override Task<UserConfig> GetConfigAsync(IUser user) {
+			if (!m_Configs.TryGetValue(user.Id, out UserConfig? gc)) {
+				gc = GetDefaultConfig(user.Id);
+			}
+			
+			return Task.FromResult(gc);
+		}
+
+		public override Task UpdateUserAsync(UserConfig config) {
+			m_Configs[config.UserId] = config;
+			// TODO (fix) this doesn't work because it doesn't know how to serialize UserConfig
+			File.WriteAllText(m_ConfigPath, JObject.FromObject(m_Configs).ToString(Newtonsoft.Json.Formatting.None));
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ RoosterBot/Services/UserConfigService.cs
@@ -0,0 +1,50 @@
+﻿using Discord;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.Threading.Tasks;
+
+namespace RoosterBot {
+	/// <summary>
+	/// Provides user-specific overrides for the GuildConfigService.
+	/// </summary>
+	public abstract class UserConfigService {
+		public abstract Task<UserConfig> GetConfigAsync(IUser user);
+		public abstract Task UpdateUserAsync(UserConfig config);
+
+		protected UserConfig GetDefaultConfig(ulong id) {
+			return new UserConfig(this, null, id, new Dictionary<string, JToken>());
+		}
+	}
+
+	public class UserConfig {
+		private UserConfigService m_Service;
+		private IDictionary<string, JToken> m_CustomData;
+
+		public ulong UserId { get; }
+		public CultureInfo? Culture { get; set; }
+
+		public UserConfig(UserConfigService userConfigService, CultureInfo? culture, ulong userId, IDictionary<string, JToken> customData) {
+			m_Service = userConfigService;
+			Culture = culture;
+			UserId = userId;
+			m_CustomData = customData;
+		}
+
+		public bool TryGetData<T>(string key, [MaybeNullWhen(false)] out T data, T defaultValue = default) {
+			if (m_CustomData.TryGetValue(key, out JToken? value)) {
+				data = value.ToObject<T>();
+				return true;
+			} else {
+				data = defaultValue;
+				return false;
+			}
+		}
+
+		public async void SetDataAsync<T>(string key, T data) {
+			m_CustomData[key] = JToken.FromObject(data);
+			await m_Service.UpdateUserAsync(this);
+		}
+	}
+}
