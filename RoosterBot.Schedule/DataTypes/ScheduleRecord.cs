b8cc3a6bad6c96f607c71e64bca15b37deb09c03
DirkPC <d.kok.2000@gmail.com>
2019-11-12 22:35:17 +0100

    ScheduleRecord should totally serialize now

--- RoosterBot.Schedule/DataTypes/ScheduleRecord.cs
+++ RoosterBot.Schedule/DataTypes/ScheduleRecord.cs
@@ -1,16 +1,21 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace RoosterBot.Schedule {
+	[JsonObject(MemberSerialization.OptOut)]
 	public abstract class ScheduleRecord {
+		// TODO (review) Do all these properties need to be in Schedule component? It's better to move everything into GLUScheduleRecord, and all identifiers types into Schedule.GLU
+		// An abstract PresentRowAsync will be necessary for this
 		public ActivityInfo Activity { get; }
 		public DateTime Start { get; }
 		public DateTime End { get; set; }
 		public IReadOnlyList<StudentSetInfo> StudentSets { get; }
 		public IReadOnlyList<TeacherInfo> StaffMember { get; }
 		public IReadOnlyList<RoomInfo> Room { get; }
+		public abstract bool ShouldCallNextCommand { get; }
 		public BreakTime? Break { get; set; }
 
 		protected ScheduleRecord(ActivityInfo activity, DateTime start, DateTime end, IReadOnlyList<StudentSetInfo> studentSets, IReadOnlyList<TeacherInfo> staffMember, IReadOnlyList<RoomInfo> room) {
@@ -22,11 +27,10 @@ namespace RoosterBot.Schedule {
 			Room = room;
 		}
 
-		public TimeSpan Duration => End - Start;
-		public string StudentSetsString => string.Join(", ", StudentSets.Select(info => info.ClassName));
-		public string StaffMemberString => string.Join(", ", StaffMember.Select(info => info.DisplayText));
-		public string RoomString => string.Join(", ", Room.Select(info => info.Room));
-		public abstract bool ShouldCallNextCommand { get; }
+		[JsonIgnore] public TimeSpan Duration => End - Start;
+		[JsonIgnore] public string StudentSetsString => string.Join(", ", StudentSets.Select(info => info.ScheduleCode));
+		[JsonIgnore] public string StaffMemberString => string.Join(", ", StaffMember.Select(info => info.DisplayText));
+		[JsonIgnore] public string RoomString => string.Join(", ", Room.Select(info => info.ScheduleCode));
 
 		public override string ToString() {
 			return $"{StudentSetsString}: {Activity.ScheduleCode} in {RoomString} from {Start.ToString()} (for {(int) Duration.TotalHours}:{Duration.Minutes}) (with " +
@@ -36,6 +40,7 @@ namespace RoosterBot.Schedule {
 		/// <summary>
 		/// Convert this instance to a string that can be sent to Discord.
 		/// </summary>
+		// TODO (review) Should this really be async? The instance is supposed to have all the data it needs, and shouldn't have any service reference to get data from
 		public abstract Task<string> PresentAsync(IdentifierInfo relevantIdentifier);
 	}
 
