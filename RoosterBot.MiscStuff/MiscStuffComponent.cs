1c01e4afe37289b7f547b7557593cbc94402b7d1
foxite <d.kok.2000@gmail.com>
2019-11-13 14:25:57 +0100

    removed counters

--- RoosterBot.MiscStuff/MiscStuffComponent.cs
+++ RoosterBot.MiscStuff/MiscStuffComponent.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
-using System.IO;
 using System;
 using System.Threading.Tasks;
 using Discord.Commands;
@@ -9,25 +8,15 @@ namespace RoosterBot.MiscStuff {
 	// This component is half composed of GLU-requested features, and half actual miscellanea.
 	// The GLU stuff should go into Schedule.GLU, which should be renamed to GLU. It will contain all current and future GLU-specific stuff.
 	public class MiscStuffComponent : ComponentBase {
-#nullable disable
-		public string ConfigPath { get; private set; }
-#nullable restore
-
 		public override Version ComponentVersion => new Version(1, 0, 0);
 
-		public override Task AddServicesAsync(IServiceCollection services, string configPath) {
-			ConfigPath = configPath;
-
-			services.AddSingleton(new CounterService(Path.Combine(configPath, "counters")));
-			return Task.CompletedTask;
-		}
+		public override Task AddServicesAsync(IServiceCollection services, string configPath) => Task.CompletedTask;
 
 		public override async Task AddModulesAsync(IServiceProvider services, RoosterCommandService commandService, HelpService help, Action<ModuleInfo[]> registerModules) {
 			services.GetService<ResourceService>().RegisterResources("RoosterBot.MiscStuff.Resources");
 
 			registerModules(await Task.WhenAll(
 				commandService.AddModuleAsync<MiscModule>(services),
-				commandService.AddModuleAsync<CounterModule>(services),
 				commandService.AddModuleAsync<ModerationModule>(services)
 			));
 		}
