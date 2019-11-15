43ee59b7481a0cf0f95126ef56528a60d963e094
foxite <d.kok.2000@gmail.com>
2019-11-15 16:12:48 +0100

    updated the async event functions in Util to be null-aware

--- RoosterBot/Util.cs
+++ RoosterBot/Util.cs
@@ -173,14 +173,15 @@ namespace RoosterBot {
 		#endregion
 
 		#region Async delegate
-		private static void CheckAsyncDelegate(Delegate asyncEvent, object[] parameters) {
+		private static void CheckAsyncDelegate(Delegate asyncEvent, object?[] parameters) {
 			if (asyncEvent.Method.ReturnType != typeof(Task)) {
 				throw new ArgumentException($"{nameof(asyncEvent)} must return Task", nameof(asyncEvent));
 			}
 
 			System.Reflection.ParameterInfo[] delegateParams = asyncEvent.Method.GetParameters();
 			for (int i = 0; i < delegateParams.Length; i++) {
-				if (!delegateParams[i].ParameterType.IsAssignableFrom(parameters[i].GetType())) {
+				object? param = parameters[i];
+				if (param != null && !delegateParams[i].ParameterType.IsAssignableFrom(param.GetType())) {
 					throw new ArgumentException($"Given parameter {i} must be assignable to the equivalent delegate parameter.", nameof(parameters));
 				}
 			}
@@ -189,7 +190,7 @@ namespace RoosterBot {
 		/// <summary>
 		/// Invokes an async delegate in such a way that the invocations run at the same time.
 		/// </summary>
-		public static async Task InvokeAsyncEventConcurrent(Delegate asyncEvent, params object[] parameters) {
+		public static async Task InvokeAsyncEventConcurrent(Delegate asyncEvent, params object?[] parameters) {
 			CheckAsyncDelegate(asyncEvent, parameters);
 
 			Delegate[] invocationList = asyncEvent.GetInvocationList();
@@ -205,7 +206,7 @@ namespace RoosterBot {
 		/// <summary>
 		/// Invokes an async delegate in such a way that the invocations run one by one.
 		/// </summary>
-		public static async Task InvokeAsyncEventSequential(Delegate asyncEvent, params object[] parameters) {
+		public static async Task InvokeAsyncEventSequential(Delegate asyncEvent, params object?[] parameters) {
 			CheckAsyncDelegate(asyncEvent, parameters);
 
 			Delegate[] invocationList = asyncEvent.GetInvocationList();
