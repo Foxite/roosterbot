9bc3b01ee1c8ec12da083bbcde9254c49ea88fbb
foxite <d.kok.2000@gmail.com>
2019-11-15 15:31:26 +0100

    PostCommandHandler now only accepts RoosterCommandContext. The current command handlers should ensure that this never becomes a problem, but it's theoretically possible for it to happen.

--- RoosterBot/Handlers/PostCommandHandler.cs
+++ RoosterBot/Handlers/PostCommandHandler.cs
@@ -2,7 +2,6 @@
 using Discord.Commands;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace RoosterBot {
@@ -22,71 +21,82 @@ namespace RoosterBot {
 		}
 
 		public async Task OnCommandExecuted(Optional<CommandInfo> command, ICommandContext context, IResult result) {
-			if (!result.IsSuccess) {
-				string response = "Er is iets heel erg misgegaan, en er is iets heel erg misgegaan toen ik het aan je wou vertellen."; // TODO (localize) This error message
-				bool bad = true;
-				string badReport = $"\"{context.Message}\": ";
+			if (context is RoosterCommandContext rcc) {
+				if (!result.IsSuccess) {
+					string response = "";
+					bool bad = true;
+					string badReport = $"\"{context.Message}\": ";
 
-				GuildConfig guildConfig = await m_GCS.GetConfigAsync(context.Guild); // TODO (fix) this fucks up in DMs!
+					GuildConfig guildConfig = await m_GCS.GetConfigAsync(rcc.Guild ?? rcc.UserGuild);
 
-				if (result.Error.HasValue) {
-					switch (result.Error.Value) {
-						case CommandError.UnknownCommand:
-							response = string.Format(m_ResourcesService.GetString(guildConfig.Culture, "Program_OnCommandExecuted_UnknownCommand"), guildConfig.CommandPrefix);
-							bad = false;
-							break;
-						case CommandError.BadArgCount:
-							response = m_ResourcesService.GetString(guildConfig.Culture, "Program_OnCommandExecuted_BadArgCount");
-							bad = false;
-							break;
-						case CommandError.UnmetPrecondition:
-							response = m_ResourcesService.ResolveString(guildConfig.Culture, Program.Instance.Components.GetComponentForModule(command.Value.Module), result.ErrorReason);
-							bad = false;
-							break;
-						case CommandError.ParseFailed:
-							response = result.ErrorReason;
-							bad = false;
-							break;
-						case CommandError.ObjectNotFound:
-							badReport += "ObjectNotFound";
-							break;
-						case CommandError.MultipleMatches:
-							badReport += "MultipleMatches";
-							break;
-						case CommandError.Exception:
-							badReport += "Exception\n";
-							badReport += result.ErrorReason;
-							break;
-						case CommandError.Unsuccessful:
-							badReport += "Unsuccessful\n";
-							badReport += result.ErrorReason;
-							break;
-						default:
-							badReport += "Unknown error: " + result.Error.Value.ToString();
-							break;
-					}
-				} else {
-					badReport += "No error reason";
-					bad = true;
-				}
-
-				if (bad) {
-					Logger.Error("Program", "Error occurred while parsing command " + badReport);
-					if (m_Config.BotOwner != null) {
-						await m_Config.BotOwner.SendMessageAsync(badReport);
+					if (result.Error.HasValue) {
+						switch (result.Error.Value) {
+							case CommandError.UnknownCommand:
+								response = string.Format(m_ResourcesService.GetString(guildConfig.Culture, "Program_OnCommandExecuted_UnknownCommand"), guildConfig.CommandPrefix);
+								bad = false;
+								break;
+							case CommandError.BadArgCount:
+								response = m_ResourcesService.GetString(guildConfig.Culture, "Program_OnCommandExecuted_BadArgCount");
+								bad = false;
+								break;
+							case CommandError.UnmetPrecondition:
+								response = m_ResourcesService.ResolveString(guildConfig.Culture, Program.Instance.Components.GetComponentForModule(command.Value.Module), result.ErrorReason);
+								bad = false;
+								break;
+							case CommandError.ParseFailed:
+								response = result.ErrorReason;
+								bad = false;
+								break;
+							case CommandError.ObjectNotFound:
+								badReport += "ObjectNotFound";
+								break;
+							case CommandError.MultipleMatches:
+								badReport += "MultipleMatches";
+								break;
+							case CommandError.Exception:
+								badReport += "Exception\n";
+								badReport += result.ErrorReason;
+								break;
+							case CommandError.Unsuccessful:
+								badReport += "Unsuccessful\n";
+								badReport += result.ErrorReason;
+								break;
+							default:
+								badReport += "Unknown error: " + result.Error.Value.ToString();
+								break;
+						}
+					} else {
+						badReport += "No error reason";
+						bad = true;
 					}
 
-					response = Util.Error + m_ResourcesService.GetString(guildConfig.Culture, "RoosterBot_FatalError");
-				} else {
-					response = Util.Error + response;
-				}
+					if (bad) {
+						Logger.Error("Program", "Error occurred while parsing command " + badReport);
+						if (m_Config.BotOwner != null) {
+							await m_Config.BotOwner.SendMessageAsync(badReport);
+						}
 
-				IReadOnlyCollection<IUserMessage>? initialResponses = (context as RoosterCommandContext)?.Responses;
-				if (initialResponses != null) {
-					await Util.ModifyResponsesIntoSingle(response, initialResponses, false);
-				} else {
-					m_CRS.AddResponse(context.Message, await context.Channel.SendMessageAsync(response));
+						response = Util.Error + m_ResourcesService.GetString(guildConfig.Culture, "RoosterBot_FatalError");
+					} else {
+						response = Util.Error + response;
+					}
+
+					IReadOnlyCollection<IUserMessage>? initialResponses = (context as RoosterCommandContext)?.Responses;
+					if (initialResponses != null) {
+						await Util.ModifyResponsesIntoSingle(response, initialResponses, false);
+					} else {
+						m_CRS.AddResponse(context.Message, await context.Channel.SendMessageAsync(response));
+					}
 				}
+			} else {
+				var nse = new NotSupportedException($"A command has been executed that used a context of type {context.GetType().Name}. RoosterBot does not support this as of version 2.1. " +
+					"All command context objects must be derived from RoosterCommandContext. " +
+					$"Starting in RoosterBot 3.0, it will no longer be possible to add modules that are not derived from {nameof(RoosterModuleBase)}. " +
+					"This exception object contains useful information in its Data property; use a debugger to see where this error came from.");
+				nse.Data.Add("command", command);
+				nse.Data.Add("context", context);
+				nse.Data.Add("result", result);
+				throw nse;
 			}
 		}
 	}
