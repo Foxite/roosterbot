b8b1276e05083d6c42ff6186a9cdfeb6855ab5b5
foxite <d.kok.2000@gmail.com>
2019-11-16 22:52:43 +0100

    apparently nullable value types can't be used as attribute parameters

--- RoosterBot/Preconditions/CountAttribute.cs
+++ RoosterBot/Preconditions/CountAttribute.cs
@@ -4,14 +4,14 @@ using Discord.Commands;
 
 namespace RoosterBot {
 	/// <summary>
-	/// This precondition requires that the amount of item in an array is >= and/or <= a value. Both Min and Max are inclusive and optional.
+	/// This precondition requires that the amount of item in an array is >= and <= a value. Both Min and Max are inclusive.
 	/// Of course, it can only be used on parammeters that have an Array type; an exception will be thrown if the parameter is not an array.
 	/// </summary>
 	public sealed class CountAttribute : RoosterParameterPreconditionAttribute {
-		public int? Min { get; }
-		public int? Max { get; }
+		public int Min { get; }
+		public int Max { get; }
 
-		public CountAttribute(int? min, int? max) {
+		public CountAttribute(int min, int max) {
 			Min = min;
 			Max = max;
 		}
@@ -19,7 +19,7 @@ namespace RoosterBot {
 		protected override Task<RoosterPreconditionResult> CheckPermissionsAsync(RoosterCommandContext context, ParameterInfo parameter, object value, IServiceProvider services) {
 			if (parameter.Type.IsArray) {
 				int length = ((Array) value).Length;
-				if ((!Min.HasValue || length >= Min) && (!Max.HasValue || length <= Max)) {
+				if (length >= Min && length <= Max) {
 					return Task.FromResult(RoosterPreconditionResult.FromSuccess());
 				} else {
 					return Task.FromResult(RoosterPreconditionResult.FromErrorBuiltin("Program_OnCommandExecuted_BadArgCount"));
