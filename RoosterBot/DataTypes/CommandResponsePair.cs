b85b9f1d7877778ebe77d4115ee518e5a037ecec
foxite <d.kok.2000@gmail.com>
2019-11-16 18:46:45 +0100

    re-added CommandResponsePair which somehow got lost

---
+++ RoosterBot/DataTypes/CommandResponsePair.cs
@@ -0,0 +1,11 @@
+﻿namespace RoosterBot {
+	public class CommandResponsePair {
+		public ulong CommandId { get; internal set; }
+		public ulong ResponseId { get; internal set; }
+
+		public CommandResponsePair(ulong commandId, ulong responseId) {
+			CommandId = commandId;
+			ResponseId = responseId;
+		}
+	}
+}
