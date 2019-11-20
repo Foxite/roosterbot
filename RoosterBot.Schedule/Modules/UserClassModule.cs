c42f979cb932da9d5375d2c831c950f71ee1ddd9
DirkPC <d.kok.2000@gmail.com>
2019-11-20 23:49:05 +0100

    properly localize UserClassModule

--- RoosterBot.Schedule/Modules/UserClassModule.cs
+++ RoosterBot.Schedule/Modules/UserClassModule.cs
@@ -2,10 +2,8 @@
 using Discord.Commands;
 
 namespace RoosterBot.Schedule {
-	[Name("#UserClassModule_Name"), Summary("#UserClassModule_Summary"), Group("#UserClassModule_Group")]
+	[LocalizedModule("nl-NL", "en-US"), Name("#UserClassModule_Name"), Summary("#UserClassModule_Summary"), Group("#UserClassModule_Group")]
 	public class UserClassModule : RoosterModuleBase {
-		public IdentifierValidationService Validation { get; set; } = null!;
-
 		[Command("#UserClassModule_GetClassForUser_Name", RunMode = RunMode.Async), Summary("#UserClassModule_GetClassForUser_Summary")]
 		public Task GetClassForUser() {
 			StudentSetInfo? ssi = UserConfig.GetStudentSet();
@@ -19,21 +17,16 @@ namespace RoosterBot.Schedule {
 			ReplyDeferred(response);
 			return Task.CompletedTask;
 		}
-		
-		[Command("#UserClassModule_SetClassForUser_Name", RunMode = RunMode.Async), Summary("#UserClassModule_SetClassForUser_Summary"), RequireContext(ContextType.Guild, ErrorMessage = "Deze command moet binnen een server worden gebruikt.")]
-		public async Task SetClassForUser([Name("#UserClassModule_SetClassForUser_class_Name")] string clazzName) {
-			StudentSetInfo? newStudentSet = await Validation.ValidateAsync<StudentSetInfo>(Context, clazzName);
-			if (newStudentSet != null) {
-				StudentSetInfo? oldStudentSet = await UserConfig.SetStudentSetAsync(newStudentSet);
-				if (oldStudentSet == null) {
-					ReplyDeferred(GetString("UserClassModule_SetClassForUser_ConfirmUserIsInClass", newStudentSet.DisplayText));
-				} else if (oldStudentSet == newStudentSet) {
-					ReplyDeferred(GetString("UserClassModule_SetClassForUser_ConfirmNoChange", newStudentSet.DisplayText));
-				} else {
-					ReplyDeferred(GetString("UserClassModule_SetClassForUser_ConfirmUserIsInClassWithOld", newStudentSet.DisplayText, oldStudentSet.DisplayText));
-				}
+
+		[Command("#UserClassModule_SetClassForUser", RunMode = RunMode.Async), Summary("#UserClassModule_SetClassForUser_Summary"), RequireContext(ContextType.Guild, ErrorMessage = "Deze command moet binnen een server worden gebruikt.")]
+		public async Task SetClassForUser([Name("#UserClassModule_SetClassForUser_class_Name")] StudentSetInfo newStudentSet) {
+			StudentSetInfo? oldStudentSet = await UserConfig.SetStudentSetAsync(newStudentSet);
+			if (oldStudentSet == null) {
+				ReplyDeferred(GetString("UserClassModule_SetClassForUser_ConfirmUserIsInClass", newStudentSet.DisplayText));
+			} else if (oldStudentSet == newStudentSet) {
+				ReplyDeferred(GetString("UserClassModule_SetClassForUser_ConfirmNoChange", newStudentSet.DisplayText));
 			} else {
-				ReplyDeferred(GetString("UserClassModule_SetClassForUser_InvalidClass"));
+				ReplyDeferred(GetString("UserClassModule_SetClassForUser_ConfirmUserIsInClassWithOld", newStudentSet.DisplayText, oldStudentSet.DisplayText));
 			}
 		}
 	}
