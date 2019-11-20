d18ce324ea387147eb0161bcb59a92c7a3abddf6
DirkPC <d.kok.2000@gmail.com>
2019-11-20 21:29:37 +0100

    fixed an apparently long-standing bug where MissingResourceException was never used, somehow never noticed this

--- RoosterBot/Services/ResourceService.cs
+++ RoosterBot/Services/ResourceService.cs
@@ -18,13 +18,20 @@ namespace RoosterBot {
 		}
 		
 		public string GetString(CultureInfo culture, string name) {
-			return m_ResourceManagers[Assembly.GetCallingAssembly()].GetString(name, culture)
-				?? throw new MissingResourceException("Resource named " + name + " in culture " + culture.Name + " was not found in your assembly.");
+			var assembly = Assembly.GetCallingAssembly();
+			try {
+				return m_ResourceManagers[assembly].GetString(name, culture)!;
+			} catch (MissingManifestResourceException e) {
+				throw new MissingResourceException($"Resource named {name} in culture {culture.Name} was not found in assembly {assembly.FullName}", e);
+			}
 		}
 
 		public string GetString(Assembly assembly, CultureInfo culture, string name) {
-			return m_ResourceManagers[assembly].GetString(name, culture)
-				?? throw new MissingResourceException("Resource named " + name + " in culture " + culture.Name + " was not found in your assembly.");
+			try {
+				return m_ResourceManagers[assembly].GetString(name, culture)!;
+			} catch (MissingManifestResourceException e) {
+				throw new MissingResourceException($"Resource named {name} in culture {culture.Name} was not found in assembly {assembly.FullName}", e);
+			}
 		}
 
 		public string ResolveString(CultureInfo culture, ComponentBase? component, string str) {
@@ -35,8 +42,7 @@ namespace RoosterBot {
 				} else {
 					assembly = component.GetType().Assembly;
 				}
-				return GetString(assembly, culture, str.Substring(1))
-					?? throw new MissingResourceException("Resource named " + str.Substring(1) + " in culture " + culture.Name + " was not found in the given assembly.");
+				return GetString(assembly, culture, str.Substring(1));
 			} else if (str.StartsWith('\\')) {
 				return str.Substring(1); // If a string is not meant to be resolved but needs to start with a # then it can be escaped with \.
 			} else {
