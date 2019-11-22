5e34ae181267064b9ad2aa5c59de1b13880f01fd
foxite <d.kok.2000@gmail.com>
2019-11-22 15:52:46 +0100

    fixed compile errors in RoosterCommandService

--- RoosterBot/Services/RoosterCommandService.cs
+++ RoosterBot/Services/RoosterCommandService.cs
@@ -1,18 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Reflection;
-using System.Threading.Tasks;
 using Discord;
-using Discord.Commands;
-using Discord.Commands.Builders;
-using ParameterInfo = System.Reflection.ParameterInfo;
+using Qmmands;
 
 namespace RoosterBot {
-	/// <summary>
-	/// RoosterCommandService keeps track of executed commands and their replies.
-	/// </summary>
 	public sealed class RoosterCommandService : CommandService {
 		private readonly ResourceService m_ResourceService;
 
@@ -20,7 +12,7 @@ namespace RoosterBot {
 			m_ResourceService = resourceService;
 		}
 
-		internal RoosterCommandService(ResourceService resourceService, CommandServiceConfig config) : base(config) {
+		internal RoosterCommandService(ResourceService resourceService, CommandServiceConfiguration config) : base(config) {
 			m_ResourceService = resourceService;
 		}
 
@@ -30,7 +22,7 @@ namespace RoosterBot {
 				message.Source == MessageSource.User &&
 				message is IUserMessage userMessage &&
 				message.Content.Length > prefix.Length &&
-				userMessage.HasStringPrefix(prefix, ref argPos)) {
+				userMessage.Content.StartsWith(prefix)) {
 				// First char after prefix
 				char firstChar = message.Content.Substring(prefix.Length)[0];
 				if ((firstChar >= 'A' && firstChar <= 'Z') || (firstChar >= 'a' && firstChar <= 'z')) {
@@ -42,240 +34,30 @@ namespace RoosterBot {
 			return false;
 		}
 
-		// One of the task items for 3.0 was that we switch to Qmmands.
-		// Qmmands is a command framework similar to what we're already using, but it's not limited to Discord. It works with any text interface.
-		// Furthermore I asked the product owner if there's any way to localize commands, and it's extremely easy: AddModule<YourModule>(x => x.WithName(m_Resources.ResolveString(x.Name))
-		// I've inferred that this will construct a module as usual and allow you to make changes to it. Instead of doing everything ourselves, we only do what we want.
-		// So when I heard about this, I decided to move Qmmands to RoosterBot 2.2.
-		public Task<ModuleInfo[]> AddLocalizedModuleAsync<T>() => AddLocalizedModuleAsync(typeof(T));
-		public async Task<ModuleInfo[]> AddLocalizedModuleAsync(Type module) {
-			// For each culture supported by the module, create a ModuleInfo from the type with resolved strings
-			if (module.IsSubclassOf(typeof(RoosterModuleBase<>))) {
-				throw new ArgumentException(module.Name + " must derive from RoosterModuleBase to support localization.");
+		public Module[] AddLocalizedModuleAsync<T>() => AddLocalizedModuleAsync(typeof(T));
+		public Module[] AddLocalizedModuleAsync(Type module) {
+			object[] localizedAttributes = module.GetCustomAttributes(typeof(LocalizedModuleAttribute), true);
+			if (localizedAttributes.Length == 1) {
+				ComponentBase? component = Program.Instance.Components.GetComponentFromAssembly(module.Assembly);
+				IReadOnlyList<string> locales = ((LocalizedModuleAttribute) localizedAttributes[0]).Locales;
+
+				Module[] localizedModules = new Module[locales.Count];
+
+				for (int i = 0; i < locales.Count; i++) {
+					string locale = locales[i];
+					var culture = CultureInfo.GetCultureInfo(locale);
+
+					localizedModules[i] = AddModule(module, (builder) => {
+						foreach (string alias in m_ResourceService.ResolveString(culture, component, builder.Name + "_Aliases").Split('|')) {
+							builder.AddAlias(alias);
+						}
+						builder.Name = m_ResourceService.ResolveString(culture, component, builder.Name);
+					});
+				}
+				return localizedModules;
+			} else {
+				throw new ArgumentException("Module class " + module.FullName + " can not be localized because it does not have " + nameof(LocalizedModuleAttribute));
 			}
-
-			IReadOnlyList<string> locales = module.GetCustomAttributes().OfType<LocalizedModuleAttribute>().Single().Locales;
-			ComponentBase component = Program.Instance.Components.GetComponentFromAssembly(module.Assembly);
-			ModuleInfo[] localizedModules = new ModuleInfo[locales.Count];
-
-			for (int i = 0; i < locales.Count; i++) {
-				string locale = locales[i];
-				localizedModules[i] = await CreateModuleAsync("", GetModuleBuildFunction(module, component, locale));
-			}
-
-			return localizedModules;
-		}
-
-		private Action<ModuleBuilder> GetModuleBuildFunction(Type module, ComponentBase component, string locale) {
-			return (moduleBuilder) => {
-				IEnumerable<(MethodInfo method, CommandAttribute attribute)> commands = module.GetMethods()
-					.Where(method => method.ReturnType == typeof(Task) || method.ReturnType == typeof(Task<RuntimeResult>))
-					.Select(method => (method, attribute: method.GetCustomAttribute<CommandAttribute>(false)!))
-					.Where(commandTuple => commandTuple.attribute != null);
-
-				if (!commands.Any()) {
-					throw new ArgumentException(module.Name + " does not have any suitable command methods.");
-				}
-
-				CultureInfo culture = CultureInfo.GetCultureInfo(locale);
-				moduleBuilder.AddPrecondition(new RequireCultureAttribute(locale, true));
-
-				foreach (Attribute attr in module.GetCustomAttributes()) {
-					switch (attr) {
-						case NameAttribute name:
-							moduleBuilder.WithName(name.Text); // Don't resolve it here because we want to automatically add localized ones if there's no AliasAttribute
-							break;                             // So we do it after the foreach loop
-						case AliasAttribute alias:
-							if (alias.Aliases.Length == 1 && alias.Aliases[0].StartsWith("#")) {
-								moduleBuilder.AddAliases(m_ResourceService.ResolveString(culture, component, alias.Aliases[0]).Split('|'));
-							} else {
-								moduleBuilder.AddAliases(alias.Aliases);
-							}
-							break;
-						case RemarksAttribute remarks:
-							moduleBuilder.WithRemarks(m_ResourceService.ResolveString(culture, component, remarks.Text));
-							break;
-						case SummaryAttribute summary:
-							moduleBuilder.WithSummary(m_ResourceService.ResolveString(culture, component, summary.Text));
-							break;
-						case GroupAttribute group:
-							moduleBuilder.Group = m_ResourceService.ResolveString(culture, component, group.Prefix);
-							break;
-						case PreconditionAttribute precondition:
-							moduleBuilder.AddPrecondition(precondition);
-							break;
-						default:
-							moduleBuilder.AddAttributes(attr);
-							break;
-					}
-				}
-
-				if (!moduleBuilder.Aliases.Any() && moduleBuilder.Name.StartsWith("#")) {
-					moduleBuilder.AddAliases(m_ResourceService.ResolveString(culture, component, moduleBuilder.Name + "_Aliases").Split('|'));
-				}
-				if (string.IsNullOrWhiteSpace(moduleBuilder.Name)) {
-					moduleBuilder.Name = module.Name;
-				}
-				moduleBuilder.Name = m_ResourceService.ResolveString(culture, component, moduleBuilder.Name);
-
-				foreach ((MethodInfo method, CommandAttribute commandAttribute) in commands) {
-					string primaryAlias = m_ResourceService.ResolveString(culture, component, commandAttribute.Text);
-					if (!string.IsNullOrWhiteSpace(moduleBuilder.Group)) {
-						primaryAlias = (moduleBuilder.Group + " " + primaryAlias).Trim();
-					}
-					if (string.IsNullOrWhiteSpace(primaryAlias)) {
-						primaryAlias = m_ResourceService.ResolveString(culture, component, method.Name);
-					}
-					moduleBuilder.AddCommand(
-						primaryAlias,
-						async (context, parameters, commandServices, command) => {
-							// Command execution
-							PropertyInfo[] properties = module.GetProperties();
-							RoosterModuleBase moduleInstance;
-							ParameterInfo[] ctorParameters = module.GetConstructors().First().GetParameters();
-							if (ctorParameters.Length == 0) {
-								// Activator.CreateInstance returns an `object?`. This does not match the documentation (where it says it returns `object`) and also does not make any kind of sense
-								//  as constructors can never return null.
-								moduleInstance = (RoosterModuleBase) (Activator.CreateInstance(module) ?? throw new InvalidOperationException("Activator.CreateInstance returned null"));
-							} else {
-								object[] services = ctorParameters.Select(param => commandServices.GetService(param.ParameterType)).ToArray();
-								moduleInstance = (RoosterModuleBase) (Activator.CreateInstance(module, services) ?? throw new InvalidOperationException("Activator.CreateInstance returned null"));
-							}
-
-							foreach (PropertyInfo prop in properties.Where(prop => prop.SetMethod != null && prop.SetMethod.IsPublic && !prop.SetMethod.IsAbstract)) {
-								object service = commandServices.GetService(prop.PropertyType);
-								prop.SetValue(moduleInstance, service);
-							}
-
-							//module.GetProperty("Context").SetValue(moduleInstance, context);
-							moduleInstance.Context = (RoosterCommandContext) context;
-
-							try {
-								((IRoosterModuleBase) moduleInstance).BeforeExecuteInternal(command);
-
-								Task task = method.Invoke(moduleInstance, parameters) as Task ?? Task.CompletedTask;
-								await task;
-							} finally {
-								((IRoosterModuleBase) moduleInstance).AfterExecuteInternal(command);
-								if (moduleInstance is IDisposable disposableModuleInstance) {
-									disposableModuleInstance.Dispose();
-								}
-							}
-						},
-						GetCommandBuildFunction(method, commandAttribute, moduleBuilder, culture, component)
-					);
-				}
-
-				// Submodule creation
-				IEnumerable<Type> submodules = module.GetNestedTypes().Where(nestedClass => nestedClass.IsSubclassOf(typeof(ModuleBase<>)));
-
-				foreach (Type submodule in submodules) {
-					if (submodule.GetCustomAttribute<LocalizedModuleAttribute>() != null) {
-						throw new ArgumentException("Submodules of localized modules can not be localized. They are always localized in the same culture as their parent.");
-					}
-
-					moduleBuilder.AddModule(submodule.GetCustomAttribute<NameAttribute>()?.Text ?? submodule.Name, GetModuleBuildFunction(submodule, component, locale));
-				}
-			};
-		}
-
-		private Action<CommandBuilder> GetCommandBuildFunction(MethodInfo method, CommandAttribute commandAttribute, ModuleBuilder moduleBuilder, CultureInfo culture, ComponentBase component) {
-			return (commandBuilder) => {
-				if (!string.IsNullOrWhiteSpace(commandAttribute.Text)) {
-					commandBuilder.WithName(commandAttribute.Text);
-				}
-
-				foreach (Attribute attr in method.GetCustomAttributes()) {
-					switch (attr) {
-						case NameAttribute name:
-							Logger.Warning("RoosterCommandService", "Attempted to add a NameAttribute to a localized command in module " + moduleBuilder.Name +
-								". NameAttributes are not used in localized modules.");
-							break;
-						case AliasAttribute alias:
-							Logger.Warning("RoosterCommandService", "Attempted to manually add aliases to localized command in module " + moduleBuilder.Name +
-								". These are automatically added according to the resource key of the command name, followed by `_Aliases`, and split by '|'.");
-							break;
-						case PriorityAttribute priority:
-							commandBuilder.WithPriority(priority.Priority);
-							break;
-						case PreconditionAttribute precondition:
-							commandBuilder.AddPrecondition(precondition);
-							break;
-						case SummaryAttribute summary:
-							commandBuilder.WithSummary(m_ResourceService.ResolveString(culture, component, summary.Text));
-							break;
-						case RemarksAttribute remarks:
-							commandBuilder.WithRemarks(m_ResourceService.ResolveString(culture, component, remarks.Text));
-							break;
-						default:
-							commandBuilder.AddAttributes(attr);
-							break;
-					}
-				}
-
-				if (commandBuilder.Aliases.Count == 1 && commandBuilder.Name.StartsWith("#")) {
-					string localizedAliases = m_ResourceService.ResolveString(culture, component, commandBuilder.Name + "_Aliases");
-					if (localizedAliases.Length != 0) {
-						commandBuilder.AddAliases(localizedAliases.Split('|'));
-					}
-				}
-				if (string.IsNullOrWhiteSpace(commandBuilder.Name)) {
-					commandBuilder.Name = method.Name;
-				}
-				if (commandAttribute.IgnoreExtraArgs.HasValue) {
-					commandBuilder.IgnoreExtraArgs = commandAttribute.IgnoreExtraArgs.Value;
-				}
-
-				commandBuilder.Name = m_ResourceService.ResolveString(culture, component, commandBuilder.Name);
-				commandBuilder.RunMode = commandAttribute.RunMode;
-
-				ParameterInfo[] parameters = method.GetParameters();
-				foreach (ParameterInfo parameter in parameters) {
-					string? paramName = parameter.GetCustomAttribute<NameAttribute>()?.Text;
-					if (paramName == null) {
-						paramName = parameter.Name;
-					} else {
-						paramName = m_ResourceService.ResolveString(culture, component, paramName);
-					}
-
-					commandBuilder.AddParameter(
-						m_ResourceService.ResolveString(culture, component, paramName!), // Second example of a pointless, undocumented nullable property in reflection classes
-						parameter.ParameterType,
-						GetParamBuildFunction(parameter, culture, component)
-					);
-				}
-			};
-		}
-
-		private Action<ParameterBuilder> GetParamBuildFunction(ParameterInfo parameter, CultureInfo culture, ComponentBase component) {
-			return (paramBuilder) => {
-				foreach (Attribute attr in parameter.GetCustomAttributes()) {
-					switch (attr) {
-						case SummaryAttribute summary:
-							paramBuilder.Summary = m_ResourceService.ResolveString(culture, component, summary.Text);
-							break;
-						case ParameterPreconditionAttribute precondition:
-							paramBuilder.AddPrecondition(precondition);
-							break;
-						case RemainderAttribute remainder:
-							paramBuilder.IsRemainder = true;
-							break;
-						case ParamArrayAttribute _:
-							paramBuilder.WithIsMultiple(true);
-							break;
-						default:
-							paramBuilder.AddAttributes(attr);
-							break;
-					}
-				}
-				paramBuilder
-					.WithDefault(parameter.DefaultValue)
-					.WithIsOptional(parameter.HasDefaultValue);
-
-				if (paramBuilder.TypeReader == null) {
-					paramBuilder.TypeReader = TypeReaders[paramBuilder.ParameterType].FirstOrDefault();
-				}
-			};
 		}
 	}
 }
