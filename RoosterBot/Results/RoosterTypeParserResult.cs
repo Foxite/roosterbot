97922bd13875f06de675f165cdd4d4aad6001661
foxite <d.kok.2000@gmail.com>
2019-11-24 14:33:16 +0100

    go back to preview Qmmands because we actually do need the unsealed TypeParserResult

---
+++ RoosterBot/Results/RoosterTypeParserResult.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Qmmands;
+
+namespace RoosterBot {
+	public class RoosterTypeParserResult<T> : TypeParserResult<T>, IRoosterTypeParserResult {
+		public IReadOnlyList<object> ErrorReasonObjects { get; }
+		public ComponentBase? ErrorReasonComponent { get; }
+
+		private RoosterTypeParserResult(T value) : base(value) {
+			ErrorReasonObjects = Array.Empty<object>();
+		}
+
+		private RoosterTypeParserResult(string reason, ComponentBase? errorReasonComponent, params object[] errorReasonObjects) : base(reason) {
+			ErrorReasonObjects = errorReasonObjects;
+			ErrorReasonComponent = errorReasonComponent;
+		}
+
+		public static new RoosterTypeParserResult<T> Successful(T result) => new RoosterTypeParserResult<T>(result);
+
+		public static RoosterTypeParserResult<T> Unsuccessful(string reason, ComponentBase? errorReasonComponent, params object[] errorReasonObjects) {
+			return new RoosterTypeParserResult<T>(reason, errorReasonComponent, errorReasonObjects);
+		}
+
+		internal static RoosterTypeParserResult<T> UnsuccessfulBuiltIn(string reason, params object[] errorReasonObjects) {
+			return new RoosterTypeParserResult<T>(reason, null, errorReasonObjects);
+		}
+	}
+
+	internal interface IRoosterTypeParserResult : IRoosterResult {
+		public string Reason { get; }
+		public bool HasValue { get; }
+	}
+}
