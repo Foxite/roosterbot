bffa1629f3d2d83c726e152a6a5c906c3b11c9dc
foxite <d.kok.2000@gmail.com>
2019-11-26 22:32:04 +0100

    moved RoosterCommandResult to own file

---
+++ RoosterBot/Modules/RoosterCommandContext.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Globalization;
+using Discord;
+using Qmmands;
+
+namespace RoosterBot {
+	public class RoosterCommandContext : CommandContext {
+		public IDiscordClient Client { get; }
+		public IUserMessage Message { get; }
+		public IUser User { get; }
+		public IMessageChannel Channel { get; }
+		public IGuild? Guild { get; }
+		public bool IsPrivate { get; }
+
+		public UserConfig UserConfig { get; }
+		public GuildConfig GuildConfig { get; }
+		public CultureInfo Culture => UserConfig.Culture ?? GuildConfig.Culture;
+
+		public RoosterCommandContext(IDiscordClient client, IUserMessage message, UserConfig userConfig, GuildConfig guildConfig, IServiceProvider isp) : base(isp) {
+			Client = client;
+			Message = message;
+			User = message.Author;
+			Channel = message.Channel;
+			IsPrivate = Channel is IPrivateChannel;
+			Guild = (Channel as IGuildChannel)?.Guild;
+
+			UserConfig = userConfig;
+			GuildConfig = guildConfig;
+		}
+
+		public override string ToString() {
+			if (Guild != null) {
+				return $"{User.Username}#{User.Discriminator} in `{Guild.Name}` channel `{Channel.Name}`: {Message.Content}";
+			} else {
+				return $"{User.Username}#{User.Discriminator} in private channel `{Channel.Name}`: {Message.Content}";
+			}
+		}
+	}
+}
--- RoosterBot/Modules/RoosterModuleBase.cs
+++ RoosterBot/Modules/RoosterModuleBase.cs
@@ -146,37 +146,4 @@ namespace RoosterBot {
 	}
 
 	public abstract class RoosterModuleBase : RoosterModuleBase<RoosterCommandContext> { }
-
-	public class RoosterCommandContext : CommandContext {
-		public IDiscordClient Client { get; }
-		public IUserMessage Message { get; }
-		public IUser User { get; }
-		public IMessageChannel Channel { get; }
-		public IGuild? Guild { get; }
-		public bool IsPrivate { get; }
-
-		public UserConfig UserConfig { get; }
-		public GuildConfig GuildConfig { get; }
-		public CultureInfo Culture => UserConfig.Culture ?? GuildConfig.Culture;
-
-		public RoosterCommandContext(IDiscordClient client, IUserMessage message, UserConfig userConfig, GuildConfig guildConfig, IServiceProvider isp) : base(isp) {
-			Client = client;
-			Message = message;
-			User = message.Author;
-			Channel = message.Channel;
-			IsPrivate = Channel is IPrivateChannel;
-			Guild = (Channel as IGuildChannel)?.Guild;
-
-			UserConfig = userConfig;
-			GuildConfig = guildConfig;
-		}
-
-		public override string ToString() {
-			if (Guild != null) {
-				return $"{User.Username}#{User.Discriminator} in `{Guild.Name}` channel `{Channel.Name}`: {Message.Content}";
-			} else {
-				return $"{User.Username}#{User.Discriminator} in private channel `{Channel.Name}`: {Message.Content}";
-			}
-		}
-	}
 }
