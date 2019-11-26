ad1054767e94a9a54a8dfa60c437aaa257d306b2
foxite <d.kok.2000@gmail.com>
2019-11-26 17:58:41 +0100

    CommandSuccessHandler now sends a message with RoosterCommandResult.PresentAsync(), if a RoosterCommandResult is included

--- RoosterBot/Handlers/CommandSuccessHandler.cs
+++ RoosterBot/Handlers/CommandSuccessHandler.cs
@@ -9,6 +9,13 @@ namespace RoosterBot {
 
 		public async Task OnCommandExecuted(CommandExecutedEventArgs args) {
 			if (args.Context is RoosterCommandContext rcc) {
+				if (args.Result != null) {
+					if (args.Result is RoosterCommandResult result) {
+						await rcc.RespondAsync(result.Present());
+					} else {
+						Logger.Warning("CommandHandler", "A successful command has returned an unknown Result type: " + args.Result.GetType().FullName + ". It will not be used.");
+					}
+				}
 				await rcc.UserConfig.UpdateAsync();
 			} else {
 				ForeignContext(args.Result.Command, args.Context, args.Result);
