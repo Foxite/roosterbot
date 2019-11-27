c51335022011365eb63f40f4fb8bf2ce3172d5d2
foxite <d.kok.2000@gmail.com>
2019-11-27 12:29:40 +0100

    TextResult now takes an IEmote instead of Emote, uses Constants emotes

--- RoosterBot/Results/TextResult.cs
+++ RoosterBot/Results/TextResult.cs
@@ -7,7 +7,7 @@ namespace RoosterBot {
 	/// A TextResult consists of an optional emote followed by a string.
 	/// </summary>
 	public class TextResult : RoosterCommandResult {
-		public Emote? PrefixEmote { get; }
+		public IEmote? PrefixEmote { get; }
 		public string Response { get; }
 
 		public override IReadOnlyList<object> ErrorReasonObjects => throw new NotImplementedException();
@@ -16,19 +16,17 @@ namespace RoosterBot {
 
 		public override bool IsSuccessful => throw new NotImplementedException();
 
-		public TextResult(Emote? prefixEmote, string response) {
+		public TextResult(IEmote? prefixEmote, string response) {
 			PrefixEmote = prefixEmote;
 			Response = response;
 		}
 
-		public override string Present() => (PrefixEmote != null ? (PrefixEmote + " ") : "") + Response;
+		public override string Present() => (PrefixEmote != null ? (PrefixEmote.ToString() + " ") : "") + Response;
 
-		private static TextResult GetResult(string emote, string response) => new TextResult(Emote.Parse(emote.Trim()), response);
-
-		public static TextResult Success(string response) => GetResult(Util.Success, response);
-		public static TextResult Info   (string response) => GetResult(Util.Info,    response);
-		public static TextResult Warning(string response) => GetResult(Util.Warning, response);
-		public static TextResult Unknown(string response) => GetResult(Util.Unknown, response);
-		public static TextResult Error  (string response) => GetResult(Util.Error,   response);
+		public static TextResult Success(string response) => new TextResult(Constants.Success, response);
+		public static TextResult Info   (string response) => new TextResult(Constants.Info,    response);
+		public static TextResult Warning(string response) => new TextResult(Constants.Warning, response);
+		public static TextResult Unknown(string response) => new TextResult(Constants.Unknown, response);
+		public static TextResult Error  (string response) => new TextResult(Constants.Error,   response);
 	}
 }
