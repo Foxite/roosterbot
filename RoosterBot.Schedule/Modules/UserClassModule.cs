7f5eea5e2b42be2d1d7a854a46fc33168feaad9b
foxite <d.kok.2000@gmail.com>
2019-11-27 12:29:02 +0100

    UserClassModule

--- RoosterBot.Schedule/Modules/UserClassModule.cs
+++ RoosterBot.Schedule/Modules/UserClassModule.cs
@@ -5,7 +5,7 @@ namespace RoosterBot.Schedule {
 	[LocalizedModule("nl-NL", "en-US"), Name("#UserClassModule_ModuleName"), Description("#UserClassModule_ModuleSummary"), Group("#UserClassModule_Group")]
 	public class UserClassModule : RoosterModuleBase {
 		[Command("#UserClassModule_CommandName"), RunMode(RunMode.Parallel), Description("#UserClassModule_CommandSummary"), RequireContext(ContextType.Guild)]
-		public async Task SetClassForUser([Name("#UserClassModule_class_Name")] StudentSetInfo? newStudentSet = null) {
+		public async Task<CommandResult> UserClass([Name("#UserClassModule_class_Name")] StudentSetInfo? newStudentSet = null) {
 			if (newStudentSet == null) {
 				StudentSetInfo? ssi = UserConfig.GetStudentSet();
 				string response;
@@ -15,15 +15,15 @@ namespace RoosterBot.Schedule {
 					response = GetString("UserClassModule_UserIsInClass", ssi.DisplayText);
 				}
 				response += GetString("UserClassModule_ChangeHint", GuildConfig.CommandPrefix);
-				ReplyDeferred(response);
+				return TextResult.Info(response);
 			} else {
 				StudentSetInfo? oldStudentSet = await UserConfig.SetStudentSetAsync(newStudentSet);
 				if (oldStudentSet == null) {
-					ReplyDeferred(GetString("UserClassModule_ConfirmUserIsInClass", newStudentSet.DisplayText));
+					return TextResult.Success(GetString("UserClassModule_ConfirmUserIsInClass", newStudentSet.DisplayText));
 				} else if (oldStudentSet == newStudentSet) {
-					ReplyDeferred(GetString("UserClassModule_ConfirmNoChange", newStudentSet.DisplayText));
+					return TextResult.Info(GetString("UserClassModule_ConfirmNoChange", newStudentSet.DisplayText));
 				} else {
-					ReplyDeferred(GetString("UserClassModule_ConfirmUserIsInClassWithOld", newStudentSet.DisplayText, oldStudentSet.DisplayText));
+					return TextResult.Success(GetString("UserClassModule_ConfirmUserIsInClassWithOld", newStudentSet.DisplayText, oldStudentSet.DisplayText));
 				}
 			}
 		}
