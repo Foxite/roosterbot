c1cbbaeed32c95875a37d7ab9859e92a293fa61d
foxite <d.kok.2000@gmail.com>
2019-11-29 13:30:53 +0100

    added AudioResult

---
+++ RoosterBot/Results/AudioResult.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace RoosterBot {
+	public class AudioResult : RoosterCommandResult {
+		public string Caption { get; }
+		public string FilePath { get; }
+
+		public AudioResult(string caption, string filePath) {
+			Caption = caption;
+			FilePath = filePath;
+		}
+
+		public override Task PresentAsync(RoosterCommandContext context) => context.RespondAsync(Caption, FilePath);
+	}
+}
