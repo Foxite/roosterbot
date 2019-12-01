1e84bdcd0724b767f3b51f1c754a7c2d2d95b9c9
foxite <d.kok.2000@gmail.com>
2019-12-01 20:28:46 +0100

    fixed compile error in LocalizedModuleAttribute

--- RoosterBot/Attributes/LocalizedModuleAttribute.cs
+++ RoosterBot/Attributes/LocalizedModuleAttribute.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 
 namespace RoosterBot {
-	[Obsolete("Localize your component by overriding " + nameof(Component.SupportedCultures)))]
+	[Obsolete("Localize your component by overriding " + nameof(Component.SupportedCultures))]
 	public sealed class LocalizedModuleAttribute : Attribute {
 		public IReadOnlyList<string> Locales { get; }
 
