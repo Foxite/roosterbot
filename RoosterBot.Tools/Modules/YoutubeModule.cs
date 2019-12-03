8f455a5836f33a2be504ba8ecfe4ee93f158b1fa
foxite <d.kok.2000@gmail.com>
2019-12-03 14:10:29 +0100

    updated YoutubeModule with stuff from preview branch

--- RoosterBot.Tools/Modules/YoutubeModule.cs
+++ RoosterBot.Tools/Modules/YoutubeModule.cs
@@ -4,40 +4,42 @@ using System.Linq;
 using System.Threading.Tasks;
 using Qmmands;
 using YoutubeExplode;
+using YoutubeExplode.Converter;
 using YoutubeExplode.Models;
 using YoutubeExplode.Models.MediaStreams;
 
 namespace RoosterBot.Tools {
-	[Group("youtube"), Name("Youtube")]
+	[Name("Youtube")]
 	public class YoutubeModule : RoosterModule {
-		[Command("mp3")]
-		public async Task<CommandResult> DownloadYoutubeAudioCommand(string url) {
-			using IDisposable typingState = Context.Channel.EnterTypingState();
+		[Command("youtube")]
+		public async Task<CommandResult> DownloadYoutubeAudioCommand(string format, string url) {
+			string[] formats = new[] { "mp3", "m4a", "wav", "wma", "ogg", "aac", "opus" };
+			if (formats.Contains(format)) {
+				using IDisposable typingState = Context.Channel.EnterTypingState();
 
-			string id = YoutubeClient.ParseVideoId(url);
-			var client = new YoutubeClient();
-			Video video = await client.GetVideoAsync(id);
+				string id = YoutubeClient.ParseVideoId(url);
+				var client = new YoutubeClient();
+				Video video = await client.GetVideoAsync(id);
 
-			MediaStreamInfoSet streams = await client.GetVideoMediaStreamInfosAsync(id);
-			if (streams.Audio.Any()) {
-				AudioStreamInfo selectedStream = streams.Audio.First();
-				foreach (AudioStreamInfo stream in streams.Audio.Skip(1)) {
-					if (stream.Bitrate > selectedStream.Bitrate) {
-						selectedStream = stream;
+				MediaStreamInfoSet streams = await client.GetVideoMediaStreamInfosAsync(id);
+				if (streams.Audio.Any()) {
+					DirectoryInfo directory = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()));
+					string filePath = Path.Combine(Path.GetTempPath(), directory.FullName, video.Title + ".mp3");
+					// TODO (feature) Get this hardcoded exe path from config
+					await new YoutubeConverter(client, "C:/RoosterBot/ffmpeg.exe").DownloadAndProcessMediaStreamsAsync(streams.Audio, filePath, "mp3");
+
+					if (new FileInfo(filePath).Length > 8e6) {
+						return TextResult.Error("Unable to upload audio: file size exceeds 8 MB");
+					} else {
+						var ret = TextResult.Success($"{video.Title} by {video.Author} ({video.Duration.ToString("c", Culture)})");
+						ret.UploadFilePath = filePath;
+						return ret;
 					}
-				}
-				string filePath = Path.Combine(Path.GetTempPath(), video.Title + "." + selectedStream.Container.GetFileExtension());
-				await client.DownloadMediaStreamAsync(selectedStream, filePath);
-
-				if (new FileInfo(filePath).Length > 8e6) {
-					return TextResult.Error("Unable to upload audio: file size exceeds 8 MB");
 				} else {
-					var ret = TextResult.Success($"{video.Title} by {video.Author} ({video.Duration.ToString("c", Culture)})");
-					ret.UploadFilePath = filePath;
-					return ret;
+					return TextResult.Error("Unable to download audio: no audio streams");
 				}
 			} else {
-				return TextResult.Error("Unable to download audio: no audio streams");
+				return TextResult.Error("Unable to download audio: unrecognized format\nValid formats: " + string.Join(", ", formats));
 			}
 		}
 	}
