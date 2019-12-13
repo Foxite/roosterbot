a16fd5c8d79bbede80c0c58c66225e9d96af1ced
foxite <d.kok.2000@gmail.com>
2019-12-13 16:14:02 +0100

    added todo items for version 2.3

--- RoosterBot/Program.cs
+++ RoosterBot/Program.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// TODO 2.3 (refactor) The program currently throws a ton of boneheaded exceptions: https://blogs.msdn.microsoft.com/ericlippert/2008/09/10/vexing-exceptions/
+// We really should avoid that wherever possible. Instead of trying to index an array and letting the caller deal with a mysterious "IndexOutOfRangeException",
+//  we should catch that before doing anything else and throw an ArgumentException with an actual explanation.
+
+// TODO 2.3 (refactor) Add docstrings to all public things. There's a way to have VS raise a warning for missing docstrings; Qmmands has this enabled, so go steal it from them.
+
+using System;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Pipes;
