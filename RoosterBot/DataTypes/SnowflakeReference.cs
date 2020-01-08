d2f8ff77f3d62d892b47eb7d8349090103a481bf
foxite <d.kok.2000@gmail.com>
2020-01-08 15:24:15 +0100

    added SnowflakeReference, which can identify a snowflake without getting it from a platform, and some helper functions

---
+++ RoosterBot/DataTypes/SnowflakeReference.cs
@@ -0,0 +1,14 @@
+﻿namespace RoosterBot {
+	/// <summary>
+	/// Contains data needed to obtain an <see cref="ISnowflake"/> object.
+	/// </summary>
+	public class SnowflakeReference {
+		public PlatformComponent Platform { get; }
+		public object Id { get; }
+
+		public SnowflakeReference(PlatformComponent platform, object id) {
+			Platform = platform;
+			Id = id;
+		}
+	}
+}
---
+++ RoosterBot/Util/PlatformUtil.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Linq;
+
+namespace RoosterBot {
+	public static class PlatformUtil {
+		/// <summary>
+		/// This allows any component to convert strings (such as stored in config files) into objects that can be compared to <see cref="ISnowflake.Id"/> values,
+		/// without having to directly interact with any PlatformComponent.
+		/// </summary>
+		public static PlatformComponent? GetPlatformComponent(string platformName) {
+			return Program.Instance.Components.GetComponents().OfType<PlatformComponent>().Where(pc => pc.PlatformName == platformName).SingleOrDefault();
+		}
+
+		public static SnowflakeReference GetSnowflakeReference(string platformName, string snowflakeString) {
+			PlatformComponent? platform = GetPlatformComponent(platformName);
+			if (platform == null) {
+				throw new InvalidOperationException("Cannot find a PlatformComponent for " + platformName);
+			}
+			return new SnowflakeReference(platform, platform.GetSnowflakeIdFromString(snowflakeString));
+		}
+
+		public static SnowflakeReference GetReference(this ISnowflake snowflake) => new SnowflakeReference(snowflake.Platform, snowflake.Id);
+	}
+}
