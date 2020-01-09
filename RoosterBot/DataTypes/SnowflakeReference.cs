8ae583705c0ed91df579ae875e3c7dad3b898698
DirkPC <d.kok.2000@gmail.com>
2020-01-09 17:01:01 +0100

    added equality and hashcode methods/operators to SnowflakeReference

--- RoosterBot/DataTypes/SnowflakeReference.cs
+++ RoosterBot/DataTypes/SnowflakeReference.cs
@@ -1,14 +1,42 @@
-﻿namespace RoosterBot {
+﻿using System;
+
+namespace RoosterBot {
 	/// <summary>
 	/// Contains data needed to obtain an <see cref="ISnowflake"/> object.
+	/// This class can be instantiated before a platform connection is available, allowing it to be deserialized from config data at startup.
 	/// </summary>
-	public class SnowflakeReference {
+	public class SnowflakeReference : IEquatable<SnowflakeReference> {
 		public PlatformComponent Platform { get; }
 		public object Id { get; }
 
 		public SnowflakeReference(PlatformComponent platform, object id) {
 			Platform = platform;
 			Id = id;
+		}
+
+		public override bool Equals(object? obj) => Equals(obj as SnowflakeReference);
+
+		public bool Equals(SnowflakeReference? other) =>
+			!(other is null)
+			&& Platform.PlatformName == other.Platform.PlatformName
+			&& Id.Equals(other.Id);
+
+		public override int GetHashCode() => HashCode.Combine(Platform, Id);
+
+		public static bool operator ==(SnowflakeReference? left, SnowflakeReference? right) {
+			if (left is null && right is null) {
+				return true;
+			}
+
+			if ((left is null) != (right is null)) {
+				return false;
+			}
+
+			return left!.Equals(right);
+		}
+
+		public static bool operator !=(SnowflakeReference? left, SnowflakeReference? right) {
+			return !(left == right);
 		}
 	}
 }
