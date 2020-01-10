bd4a8209528edb0a19797c691b182458e8e1edd3
foxite <d.kok.2000@gmail.com>
2020-01-10 23:47:58 +0100

    I've been stuck on this bug all day, mostly because the debugger misled me into thinking the problem was something else. But I fixed it now, though it's not ideal and likely to pop up somewhere else. See comment for more details.

--- RoosterBot/DataTypes/SnowflakeReference.cs
+++ RoosterBot/DataTypes/SnowflakeReference.cs
@@ -7,6 +7,7 @@ namespace RoosterBot {
 	/// </summary>
 	public class SnowflakeReference : IEquatable<SnowflakeReference> {
 		public PlatformComponent Platform { get; }
+
 		public object Id { get; }
 
 		public SnowflakeReference(PlatformComponent platform, object id) {
@@ -16,10 +17,23 @@ namespace RoosterBot {
 
 		public override bool Equals(object? obj) => Equals(obj as SnowflakeReference);
 
-		public bool Equals(SnowflakeReference? other) =>
-			!(other is null)
-			&& Platform.PlatformName == other.Platform.PlatformName
-			&& Id.Equals(other.Id);
+		public bool Equals(SnowflakeReference? other) {
+			if (!(other is null)) {
+				return Platform.PlatformName == other.Platform.PlatformName &&
+					   // Here's the problem:
+					   // When you serialize a ulong, it gets stored as a number.
+					   // When you serialize an sbyte, it gets stored as a number.
+					   // When you deserialize a number into a variable typed object, it will pick the smallest type that fits it.
+					   // This means you can't serialize a ulong and expect to get back a ulong. That won't happen unless it's bigger than 2^63.
+					   // When you compare a deserialized SnowflakeReference, to an SR obtained from a "live" snowflake, it may not work.
+					   // As a solution, we compare the actual numeric value of the IDs (if it's a numeric type), which sidesteps the problem.
+					   // But this is not ideal. An ideal solution allows us to store the type of the ID along with it, but it doesn't seem that Newtonsoft.Json
+					   //  supports this, at least not for primitive types.
+					   Util.CompareNumeric(Id, other.Id);
+			} else {
+				return false;
+			}
+		}
 
 		public override int GetHashCode() => HashCode.Combine(Platform, Id);
 
--- RoosterBot/Util/Util.cs
+++ RoosterBot/Util/Util.cs
@@ -1,7 +1,29 @@
 ﻿using System;
+using System.Linq;
 
 namespace RoosterBot {
 	public static class Util {
 		public static readonly Random RNG = new Random();
+
+		public static bool CompareNumeric(object left, object right) {
+			Type[] numerics = new[] {
+				typeof(byte),
+				typeof(short),
+				typeof(int),
+				typeof(long),
+				typeof(sbyte),
+				typeof(ushort),
+				typeof(uint),
+				typeof(ulong),
+				typeof(float),
+				typeof(double),
+				typeof(decimal),
+			};
+			if (numerics.Contains(left.GetType()) && numerics.Contains(right.GetType())) {
+				return ((IConvertible) left).ToDecimal(null).Equals(((IConvertible) left).ToDecimal(null));
+			} else {
+				return left.Equals(right);
+			}
+		}
 	}
 }
