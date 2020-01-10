295caff7a0aac2312009c4a2fe4365c5d17819a2
foxite <d.kok.2000@gmail.com>
2020-01-10 13:14:05 +0100

    added DiscordCommandContext which extends RoosterCommandContext and provides type-safe access to discord entities, and the BaseSocketClient, so discord-specific modules require minimal effort to update.

---
+++ RoosterBot.DiscordNet/DiscordCommandContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Discord.WebSocket;
+
+namespace RoosterBot.DiscordNet {
+	public class DiscordCommandContext : RoosterCommandContext {
+		public BaseSocketClient Client { get; }
+		public new Discord.IUserMessage Message { get; }
+		public new Discord.IUser User { get; }
+		public new Discord.IMessageChannel Channel { get; }
+		public Discord.IGuild? Guild { get; }
+
+		public DiscordCommandContext(DiscordMessage message, UserConfig userConfig, ChannelConfig guildConfig, IServiceProvider isp) : base(message, userConfig, guildConfig, isp) {
+			Client = DiscordNetComponent.Instance.Client;
+			Message = message.DiscordEntity;
+			User = Message.Author;
+			Channel = Message.Channel;
+			Guild = Channel is SocketGuildChannel sgc ? sgc.Guild : null;
+		}
+	}
+}
