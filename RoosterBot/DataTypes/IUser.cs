6596d2a3e5d1392845cd1c69ee724f999700d3f1
foxite <d.kok.2000@gmail.com>
2020-01-13 10:54:52 +0100

    docstrings for IUser.IsBotAdmin and IsChannelAdmin

--- RoosterBot/DataTypes/IUser.cs
+++ RoosterBot/DataTypes/IUser.cs
@@ -22,7 +22,15 @@
 		/// This user represents us.
 		/// </summary>
 		bool IsRoosterBot { get; }
+
+		/// <summary>
+		/// This user has administrative permissions over the bot, and can do things like shut it down.
+		/// </summary>
 		bool IsBotAdmin { get; }
+
+		/// <summary>
+		/// This user has administrative permissions within a given <see cref="IChannel"/>, and within the channel, can do things like delete other user's messages and remove users.
+		/// </summary>
 		bool IsChannelAdmin(IChannel channel);
 	}
 }
