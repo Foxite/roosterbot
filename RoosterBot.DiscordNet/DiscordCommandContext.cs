3db9ff4a2c5bc5da5c4dcb11745e0a99f1fa4bd8
foxite <d.kok.2000@gmail.com>
2020-01-13 12:36:53 +0100

    as a test, DiscordCommandContext will now display a single AspectListResult using an embed.

--- RoosterBot.DiscordNet/DiscordCommandContext.cs
+++ RoosterBot.DiscordNet/DiscordCommandContext.cs
@@ -1,12 +1,15 @@
-﻿using Discord.WebSocket;
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.WebSocket;
 
 namespace RoosterBot.DiscordNet {
 	public class DiscordCommandContext : RoosterCommandContext {
 		public BaseSocketClient Client { get; }
-		public new Discord.IUserMessage Message { get; }
+		public new IUserMessage Message { get; }
 		public new Discord.IUser User { get; }
-		public new Discord.IMessageChannel Channel { get; }
-		public Discord.IGuild? Guild { get; }
+		public new IMessageChannel Channel { get; }
+		public IGuild? Guild { get; }
 
 		public DiscordCommandContext(DiscordMessage message, UserConfig userConfig, ChannelConfig guildConfig) : base(DiscordNetComponent.Instance, message, userConfig, guildConfig) {
 			Client = DiscordNetComponent.Instance.Client;
@@ -14,6 +17,28 @@ namespace RoosterBot.DiscordNet {
 			User = Message.Author;
 			Channel = Message.Channel;
 			Guild = Channel is SocketGuildChannel sgc ? sgc.Guild : null;
+		}
+
+		protected async override Task<IMessage> SendResultAsync(RoosterCommandResult result) {
+			var alr = result as AspectListResult ?? ((result is CompoundResult cr && cr.IndividualResults.CountEquals(1)) ? cr.IndividualResults.First() as AspectListResult : null);
+			if (alr != null) {
+				var embed = new EmbedBuilder()
+					.WithTitle(alr.Caption)
+					.WithFields(
+						from aspect in alr
+						select new EmbedFieldBuilder()
+							.WithName(aspect.PrefixEmote.ToString() + aspect.Name)
+							.WithValue(aspect.Value)
+							.WithIsInline(true))
+					.Build();
+				if (result.UploadFilePath == null) {
+					return new DiscordMessage(await Channel.SendMessageAsync(embed: embed));
+				} else {
+					return new DiscordMessage(await Channel.SendFileAsync(result.UploadFilePath, embed: embed));
+				}
+			} else {
+				return await base.SendResultAsync(result);
+			}
 		}
 	}
 }
