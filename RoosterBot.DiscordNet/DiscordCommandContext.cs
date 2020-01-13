e557f5f8196ef0dd814b6f7761d22e1b64c2b22c
foxite <d.kok.2000@gmail.com>
2020-01-13 17:16:29 +0100

    a todo item

--- RoosterBot.DiscordNet/DiscordCommandContext.cs
+++ RoosterBot.DiscordNet/DiscordCommandContext.cs
@@ -61,6 +61,7 @@ namespace RoosterBot.DiscordNet {
 		}
 
 		private async Task<IMessage> SendPaginatedResult(PaginatedResult pr) {
+			// TODO some of this could be done in RoosterBot, only problem is that it can't add the buttons. Platform would have to take care of that.
 			if (!pr.MoveNext()) {
 				return new DiscordMessage(await Channel.SendMessageAsync("Empty result!")); // TODO
 			}
