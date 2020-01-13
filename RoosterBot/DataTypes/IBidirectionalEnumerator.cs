40d7af122ec2cb821292962739cb94bcc6f68c26
foxite <d.kok.2000@gmail.com>
2020-01-13 13:11:51 +0100

    WIP: paginated results. I added PaginatedResult for this, which implements IBidirectionalEnumerator (which I also added), which is an IEnumerator with an added MovePrevious function.

---
+++ RoosterBot/DataTypes/IBidirectionalEnumerator.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace RoosterBot {
+	public interface IBidirectionalEnumerator<T> : IEnumerator<T> {
+		/// <summary>
+		/// Moves the enumerator to the previous element of the collection.
+		/// </summary>
+		/// <returns>
+		/// <see langword="true"/> if the enumerator was successfully moved to the previous element; <see langword="false"/> if the enumerator has passed the beginning of the enumeration.
+		/// </returns>
+		/// <exception cref="System.InvalidOperationException">
+		/// The collection was modified after the enumerator was created.
+		/// </exception>
+		/// <remarks>
+		/// It may move the enumerator past its initial value. Use <see cref="System.Collections.IEnumerator.Reset"/> to set the enumerator to its initial value.
+		/// </remarks>
+		bool MovePrevious();
+	}
+}
---
+++ RoosterBot/Results/PaginatedResult.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+
+namespace RoosterBot {
+	public sealed class PaginatedResult : RoosterCommandResult, IBidirectionalEnumerator<RoosterCommandResult> {
+		private readonly IBidirectionalEnumerator<RoosterCommandResult> m_Enumerator;
+
+		public RoosterCommandResult Current => m_Enumerator.Current;
+
+		object? IEnumerator.Current => m_Enumerator.Current;
+
+		public PaginatedResult(IBidirectionalEnumerator<RoosterCommandResult> pageEnumerator) {
+			m_Enumerator = pageEnumerator;
+		}
+
+		public void Dispose() => m_Enumerator.Dispose();
+		public bool MoveNext() => m_Enumerator.MoveNext();
+		public bool MovePrevious() => m_Enumerator.MovePrevious();
+		public void Reset() => m_Enumerator.Reset();
+
+		public override string ToString(RoosterCommandContext rcc) => Current.ToString(rcc);
+	}
+}
