337205552739d71fea8e54b3ed9ef8328271cfaf
DirkPC <d.kok.2000@gmail.com>
2020-01-14 20:30:37 +0100

    AspectListResults in PaginatedResults should now be embedded, can't test it because I discovered a problem with the new MemoryScheduleProvider implementation.

--- RoosterBot.DiscordNet/DiscordCommandContext.cs
+++ RoosterBot.DiscordNet/DiscordCommandContext.cs
@@ -24,8 +24,8 @@ namespace RoosterBot.DiscordNet {
 
 		// I'd make this a local, but then I couldn't have the null-awareness attributes on the parameters.
 		private bool IsResult<T>(RoosterCommandResult input, [MaybeNullWhen(false), NotNullWhen(true)] out T? result) where T : RoosterCommandResult {
-			                                            // Hard-to-read expression - I've laid it out here:
-			result = input as T ??                      // Simple, if result is T then return result as T.
+			result =                                    // Hard-to-read expression - I've laid it out here:
+				input as T ??                           // Simple, if result is T then return result as T.
 				((input is CompoundResult cr            // If it's not T: Is it a compound result...
 				&& cr.IndividualResults.CountEquals(1)) //  with only one item?
 				? cr.IndividualResults.First() as T     //   Then return the first (and only) item as T, returning null if it's not T.
@@ -44,20 +44,40 @@ namespace RoosterBot.DiscordNet {
 		}
 
 		private async Task<IMessage> SendAspectList(AspectListResult alr) {
-			var embed = new EmbedBuilder()
-				.WithTitle(alr.Caption)
-				.WithFields(
-					from aspect in alr
-					select new EmbedFieldBuilder()
-					.WithName(aspect.PrefixEmote.ToString() + aspect.Name)
-					.WithValue(aspect.Value)
-					.WithIsInline(true))
-				.Build();
+			Embed embed = AspectListToEmbedBuilder(alr).Build();
 			if (alr.UploadFilePath == null) {
 				return new DiscordMessage(await Channel.SendMessageAsync(embed: embed));
 			} else {
 				return new DiscordMessage(await Channel.SendFileAsync(alr.UploadFilePath, embed: embed));
 			}
+		}
+
+		private EmbedBuilder AspectListToEmbedBuilder(AspectListResult alr) {
+			string title = alr.Caption;
+			string? description = null;
+			int colonIndex = title.IndexOf(':');
+			if (colonIndex != -1) {
+				title = title.Substring(0, colonIndex);
+				description = title.Substring(colonIndex + 1).Trim();
+			}
+
+			return new EmbedBuilder() {
+				Title = alr.Caption,
+				Description = description,
+				Fields = (
+					from aspect in alr
+					select new EmbedFieldBuilder() {
+						Name = aspect.PrefixEmote.ToString() + aspect.Name,
+						Value = aspect.Value,
+						IsInline = true
+					}
+				).ToList(),
+				Author = new EmbedAuthorBuilder() {
+					IconUrl = User.GetAvatarUrl(),
+					Name = (User as IGuildUser)?.Nickname ?? (User.Username + "#" + User.Discriminator)
+				},
+				Timestamp = DateTimeOffset.UtcNow
+			};
 		}
 
 		private async Task<IMessage> SendPaginatedResult(PaginatedResult pr) {
@@ -68,27 +88,35 @@ namespace RoosterBot.DiscordNet {
 
 			IUserMessage message = await Channel.SendMessageAsync(pr.Current.ToString(this));
 
-			Task goPrevious() {
-				if (pr.MovePrevious()) {
+			Task goTo(Func<bool> moveAction) {
+				RoosterCommandResult current = pr.Current;
+				if (moveAction()) {
 					return message.ModifyAsync(props => {
-						props.Content = pr.Current.ToString(this);
+						if (pr.Current is AspectListResult alr) {
+							props.Content = null;
+							props.Embed = AspectListToEmbedBuilder(alr).Build();
+						} else {
+							props.Content = current.ToString(this);
+							props.Embed = null;
+						}
 					});
-				}
-				return Task.CompletedTask;
-			}
-
-			Task goNext() {
-				if (pr.MoveNext()) {
+				} else {
 					return message.ModifyAsync(props => {
-						props.Content = pr.Current.ToString(this);
+						if (current is AspectListResult alr) {
+							props.Embed = props.Embed.Value.ToEmbedBuilder()
+								.WithFooter(props.Embed.Value.Footer + "\nNo more results")
+								.Build();
+						} else {
+							props.Content += "\nNo more results.";
+						}
 					});
 				}
 				return Task.CompletedTask;
 			}
 
 			new InteractiveMessageHandler(message, User, new Dictionary<Discord.IEmote, Func<Task>>() {
-				{ new Discord.Emoji("⬅️"), goPrevious },
-				{ new Discord.Emoji("➡️"), goNext },
+				{ new Discord.Emoji("⬅️"), () => goTo(pr.MoveNext) },
+				{ new Discord.Emoji("➡️"), () => goTo(pr.MovePrevious) },
 				//{ new Discord.Emoji("⏪"), reset }
 			});
 			return new DiscordMessage(message);
