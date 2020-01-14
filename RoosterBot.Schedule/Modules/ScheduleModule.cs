d4616569ec96d942ab6b1c7617b8ffcd23b9badc
foxite <d.kok.2000@gmail.com>
2020-01-14 17:17:32 +0100

    Since ScheduleModule now contains not nearly as much code as it used to, it's acceptable to have everything in one file. RoosterBot.Schedule/Modules is clean once more.

--- RoosterBot.Schedule/Modules/ScheduleModule.cs
+++ RoosterBot.Schedule/Modules/ScheduleModule.cs
@@ -7,7 +7,7 @@ using RoosterBot.DateTimeUtils;
 namespace RoosterBot.Schedule {
 	[Name("#ScheduleModule_Name")]
 	[Description("#ScheduleModule_Summary")]
-	public partial class ScheduleModule : RoosterModule {
+	public class ScheduleModule : RoosterModule {
 		public ScheduleService Schedules { get; set; } = null!;
 		
 		[Command("#ScheduleModule_NowCommand"), Description("#ScheduleModule_DefaultCurrentCommand_Summary")]
@@ -107,6 +107,66 @@ namespace RoosterBot.Schedule {
 				}
 			} else {
 				return resolve.ErrorResult;
+			}
+		}
+
+		private CommandResult RespondDay(IdentifierInfo? info, DateTime date) {
+			ReturnValue<IdentifierInfo> resolve = ResolveNullInfo(info);
+			if (resolve.Success) {
+				info = resolve.Value;
+				return new PaginatedResult(new DayScheduleEnumerator(Context, info, date, new string[] {
+					GetString("ScheduleModule_RespondDay_ColumnActivity"),
+					GetString("ScheduleModule_RespondDay_ColumnTime"),
+					GetString("ScheduleModule_RespondDay_ColumnStudentSets"),
+					GetString("ScheduleModule_RespondDay_ColumnTeacher"),
+					GetString("ScheduleModule_RespondDay_ColumnRoom")
+				}));
+			} else {
+				return resolve.ErrorResult;
+			}
+		}
+		
+		private CommandResult RespondWeek(IdentifierInfo? info, int weeksFromNow) {
+			ReturnValue<IdentifierInfo> resolve = ResolveNullInfo(info);
+			return resolve.Success
+				? new PaginatedResult(new WeekScheduleEnumerator(Context, resolve.Value, weeksFromNow))
+				: resolve.ErrorResult;
+		}
+
+		private PaginatedResult GetSingleResult(ScheduleRecord record, IdentifierInfo identifier, string caption) =>
+			new PaginatedResult(new SingleScheduleEnumerator(Context, record, identifier, caption));
+
+		private ReturnValue<IdentifierInfo> ResolveNullInfo(IdentifierInfo? info) {
+			if (info == null) {
+				StudentSetInfo? ssi = Context.UserConfig.GetStudentSet();
+				if (ssi != null) {
+					return ReturnValue<IdentifierInfo>.Successful(ssi);
+				} else {
+					return ReturnValue<IdentifierInfo>.Unsuccessful(TextResult.Error(GetString("StudentSetInfoReader_CheckFailed_MentionSelf", GuildConfig.CommandPrefix)));
+				}
+			} else {
+				return ReturnValue<IdentifierInfo>.Successful(info);
+			}
+		}
+
+		private Task<ReturnValue<ScheduleRecord?>> GetRecordAtDateTime(IdentifierInfo identifier, DateTime datetime) {
+			return HandleErrorAsync(() => Schedules.GetRecordAtDateTime(identifier, datetime, Context));
+		}
+
+		private Task<ReturnValue<ScheduleRecord>> GetRecordAfterDateTime(IdentifierInfo identifier, DateTime datetime) {
+			return HandleErrorAsync(() => Schedules.GetRecordAfterDateTime(identifier, datetime, Context));
+		}
+
+		private async Task<ReturnValue<T>> HandleErrorAsync<T>(Func<Task<T>> action) {
+			try {
+				return ReturnValue<T>.Successful(await action());
+			} catch (Exception e) {
+				return ReturnValue<T>.Unsuccessful(TextResult.Error(GetString(e switch {
+					IdentifierNotFoundException _ => "ScheduleModule_HandleError_NotFound",
+					RecordsOutdatedException    _ => "ScheduleModule_HandleError_RecordsOutdated",
+					NoAllowedChannelsException  _ => "ScheduleModule_HandleError_NoSchedulesAvailableForServer",
+					_ => throw e
+				})));
 			}
 		}
 	}
