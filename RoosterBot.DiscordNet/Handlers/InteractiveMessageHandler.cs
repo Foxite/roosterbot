44a0481d14ed760541e44f6da373bd07af57a81d
DirkPC <d.kok.2000@gmail.com>
2020-01-14 20:29:26 +0100

    fixed InteractiveMessageHandler callbacks running on gateway thread

--- RoosterBot.DiscordNet/Handlers/InteractiveMessageHandler.cs
+++ RoosterBot.DiscordNet/Handlers/InteractiveMessageHandler.cs
@@ -38,24 +38,30 @@ namespace RoosterBot.DiscordNet {
 
 		private void OnExpired(object sender, ElapsedEventArgs e) => Dispose();
 
-		private Task OnMessageDeleted(Cacheable<Discord.IMessage, ulong> message, IMessageChannel channel) => _ = Task.Run(() => {
-			if (message.Id == m_Message.Id) {
-				Dispose();
-			}
-		});
+		private Task OnMessageDeleted(Cacheable<Discord.IMessage, ulong> message, IMessageChannel channel) {
+			_ = Task.Run(() => {
+				if (message.Id == m_Message.Id) {
+					Dispose();
+				}
+			});
+			return Task.CompletedTask;
+		}
 
-		private Task OnReactionAdded(Cacheable<IUserMessage, ulong> message, IMessageChannel channel, IReaction reaction) => _ = Task.Run(async () => {
-			if (message.Id == m_Message.Id) {
-				await foreach (IReadOnlyCollection<Discord.IUser> item in message.Value.GetReactionUsersAsync(reaction.Emote, message.Value.Reactions[reaction.Emote].ReactionCount + 1)) {
-					if (item.Any(user => user.Id == m_User.Id) && m_Callbacks.TryGetValue(reaction.Emote, out Func<Task>? callback)) {
-						ResetTimer();
-						await message.Value.RemoveReactionAsync(reaction.Emote, m_User.Id);
-						await callback();
-						break;
+		private Task OnReactionAdded(Cacheable<IUserMessage, ulong> message, IMessageChannel channel, IReaction reaction) {
+			_ = Task.Run(async () => {
+				if (message.Id == m_Message.Id) {
+					await foreach (IReadOnlyCollection<Discord.IUser> item in message.Value.GetReactionUsersAsync(reaction.Emote, message.Value.Reactions[reaction.Emote].ReactionCount + 1)) {
+						if (item.Any(user => user.Id == m_User.Id) && m_Callbacks.TryGetValue(reaction.Emote, out Func<Task>? callback)) {
+							ResetTimer();
+							await message.Value.RemoveReactionAsync(reaction.Emote, m_User.Id);
+							await callback();
+							break;
+						}
 					}
 				}
-			}
-		});
+			});
+			return Task.CompletedTask;
+		}
 
 		// There seems no way to actually reset a timer so we just create a new one.
 		private void ResetTimer() {
