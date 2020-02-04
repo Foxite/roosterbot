2b8ee1ad705a56f85e779ef11fb3ba197b59b300
DirkPC <d.kok.2000@gmail.com>
2020-02-04 17:09:57 +0100

    updated DiscordNet log handler

--- RoosterBot.DiscordNet/Handlers/LogHandler.cs
+++ RoosterBot.DiscordNet/Handlers/LogHandler.cs
@@ -1,26 +1,16 @@
 ﻿using System;
 using System.Threading.Tasks;
-using Discord;
 using Discord.WebSocket;
 
 namespace RoosterBot.DiscordNet {
 	internal sealed class LogHandler {
-
 		public LogHandler(BaseSocketClient client) {
 			client.Log += Log;
 		}
 
-		private Task Log(LogMessage msg) {
-			Action<string, string, Exception?> logFunc = msg.Severity switch {
-				LogSeverity.Verbose  => Logger.Verbose,
-				LogSeverity.Debug    => Logger.Debug,
-				LogSeverity.Info     => Logger.Info,
-				LogSeverity.Warning  => Logger.Warning,
-				LogSeverity.Error    => Logger.Error,
-				LogSeverity.Critical => Logger.Critical,
-				_                    => Logger.Info,
-			};
-			logFunc("Discord-" + msg.Source, msg.Message, msg.Exception);
+		private Task Log(Discord.LogMessage msg) {
+			var level = (LogLevel) Enum.Parse(typeof(LogLevel), msg.Severity.ToString());
+			Logger.Log(level, (msg.Source == "Discord" ? "" : "Discord-") + msg.Source, msg.Message, msg.Exception);
 
 			return Task.CompletedTask;
 		}
