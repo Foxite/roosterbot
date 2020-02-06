72b33c9f41fcf89b69a2aea7c7dc4dcaa3b29fff
foxite <d.kok.2000@gmail.com>
2020-02-06 17:07:26 +0100

    added a debugging module with a command that displays the raw userconfig for a user

---
+++ RoosterBot.Meta/Modules/DebuggingModule.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Qmmands;
+
+namespace RoosterBot.Meta {
+	[HiddenFromList, RequireBotManager, Group("debug")]
+	public class DebuggingModule : RoosterModule {
+		public UserConfigService UserConfigService { get; set; } = null!;
+
+		[Command("userconfig")]
+		public async Task<CommandResult> ShowUserConfig(IUser user) {
+			UserConfig config;
+			if (user.Platform.Name == Context.Platform.Name && user.Id == Context.User.Id) {
+				config = Context.UserConfig;
+			} else {
+				config = await UserConfigService.GetConfigAsync(user.GetReference());
+			}
+
+			return TextResult.Info(config.GetRawData().ToString());
+		}
+	}
+}
