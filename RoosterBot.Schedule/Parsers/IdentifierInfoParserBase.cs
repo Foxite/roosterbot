97a5869965010ff7afa1bbd34a7de5dcdef348c4
foxite <d.kok.2000@gmail.com>
2020-02-06 15:54:37 +0100

    fixed an issue, though it is not a nice fix

--- RoosterBot.Schedule/Parsers/IdentifierInfoParserBase.cs
+++ RoosterBot.Schedule/Parsers/IdentifierInfoParserBase.cs
@@ -3,6 +3,14 @@ using Microsoft.Extensions.DependencyInjection;
 using Qmmands;
 
 namespace RoosterBot.Schedule {
+	/// <summary>
+	/// Note to implementers:
+	/// 
+	/// To use a TypeParser deriving from this class you must have a resource with this key in all your locales:
+	/// <code>IdentifierInfoReaderBase_ErrorMessage</code>
+	/// This string will be used as the error reason key when the validator can't parse the input.
+	/// </summary>
+	// TODO fix problem described above
 	public abstract class IdentifierInfoParserBase<T> : RoosterTypeParser<T> where T : IdentifierInfo {
 		public async override ValueTask<RoosterTypeParserResult<T>> ParseAsync(Parameter parameter, string input, RoosterCommandContext context) {
 			T? result = await context.ServiceProvider.GetRequiredService<IdentifierValidationService>().ValidateAsync<T>(context, input);
