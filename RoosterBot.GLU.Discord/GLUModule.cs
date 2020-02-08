de1cb64252c77e00d54497ae0a59384c56c0c7a4
foxite <d.kok.2000@gmail.com>
2020-02-08 23:11:40 +0100

    added a bunch more !thanks responses

--- RoosterBot.GLU.Discord/GLUModule.cs
+++ RoosterBot.GLU.Discord/GLUModule.cs
@@ -3,10 +3,10 @@
 namespace RoosterBot.GLU {
 	[HiddenFromList]
 	public class GLUModule : RoosterModule {
-		[Priority(-1),
-		 Command("danku", "dankje", "dankjewel", "bedankt", "dank",
-				 "goed", "goedzo", "goodbot", "good",
-				 "thanks", "thnx", "thx", "ty", "thank")]
+		[Priority(-1), Command(
+			"danku", "dankje", "dankjewel", "bedankt", "dank",
+			"goed", "goedzo", "goodbot", "good",
+			"thanks", "thnx", "thx", "ty", "thank")]
 		public CommandResult ThankYouCommand([Remainder] string post = "") {
 			string response;
 			if (post.ToLower() == "joram" || (UserConfig.TryGetData("misc.alwaysjoram", out bool alwaysJoram, false) && alwaysJoram)) {
@@ -14,12 +14,34 @@ namespace RoosterBot.GLU {
 			} else {
 				string[] responses = new[] {
 					":smile:",
-					":thumbsup:",
-					"<:wsjoram:570601561072467969>",
+					":smiley",
+					":grin:",
 					":blush:",
-					":heart:"
+					":smiling_face_with_3_hearts:",
+					":hugging:",
+					":smiley_cat:",
+					":thumbsup:",
+					":love_you_gesture:",
+					"<:wsjoram:570601561072467969>"
 				};
-				response = responses[Util.RNG.Next(0, responses.Length)];
+				int selection = Util.RNG.Next(0, responses.Length + 1);
+				if (selection == 0) {
+					string[] hearts = new[] {
+						":heart:",
+						":orange_heart:",
+						":yellow_heart:",
+						":green_heart:",
+						":blue_heart:",
+						":purple_heart:",
+						":brown_heart:",
+						":white_heart:",
+						":two_hearts:",
+						":heartpulse:"
+					};
+					response = hearts[Util.RNG.Next(0, hearts.Length)];
+				} else {
+					response = responses[selection - 1];
+				}
 			}
 
 			return new TextResult(null, response);
