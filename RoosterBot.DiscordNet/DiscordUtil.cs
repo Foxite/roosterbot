17bd4b490c9e53890da7daf0a4c58df99dd9835a
foxite <d.kok.2000@gmail.com>
2020-02-21 17:59:36 +0100

    DiscordNet now uses its mention string as an alternative prefix, and ignores all whitespace following the prefix

--- RoosterBot.DiscordNet/DiscordUtil.cs
+++ RoosterBot.DiscordNet/DiscordUtil.cs
@@ -6,14 +6,31 @@ namespace RoosterBot.DiscordNet {
 			argPos = 0;
 			if (message.Content != null && // Message objects created for MessageUpdated events only contain what was modified. Content may be null in certain cases. https://github.com/discord-net/Discord.Net/issues/1409
 				message.Source == MessageSource.User &&
-				message is IUserMessage userMessage &&
-				message.Content.Length > prefix.Length &&
-				userMessage.Content.StartsWith(prefix)) {
+				message is IUserMessage userMessage) {
+				string mention = DiscordNetComponent.Instance.Client.CurrentUser.Mention;
+				if (userMessage.Content.StartsWith(mention)) {
+					prefix = mention;
+				} else if (!userMessage.Content.StartsWith(prefix)) {
+					return false;
+				}
+				if (userMessage.Content.Length == prefix.Length) {
+					return false;
+				}
+
+				argPos = prefix.Length;
+
+				while (userMessage.Content.Length > argPos && userMessage.Content[argPos] == ' ') {
+					// Skip whitespace
+					argPos++;
+				}
+
 				// First char after prefix
-				char firstChar = message.Content[prefix.Length];
+				char firstChar = message.Content[argPos];
 				if ((firstChar >= 'A' && firstChar <= 'Z') || (firstChar >= 'a' && firstChar <= 'z')) {
 					// Otherwise probably not meant as a command, but an expression (for example !!! or ?!, depending on the prefix used)
-					argPos = prefix.Length;
+					// TODO this means commands that start with a non alphabetic character don't work on Discord.
+					// I don't want to ignore unknown commands like Dyno does, but I also don't want to respond to !!! etc.
+
 					return true;
 				}
 			}
