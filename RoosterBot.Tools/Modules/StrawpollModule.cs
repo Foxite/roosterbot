f4e9021781144f5a42dffd34a66dc573653a1e74
DirkPC <d.kok.2000@gmail.com>
2020-03-05 20:43:23 +0100

    change strawpoll library because the one I had seems to be an older fork of another one

--- RoosterBot.Tools/Modules/StrawpollModule.cs
+++ RoosterBot.Tools/Modules/StrawpollModule.cs
@@ -1,14 +1,13 @@
 ﻿using System.Linq;
 using System.Threading.Tasks;
 using Qmmands;
-using StrawpollNET;
+using StrawPollNET;
 
 namespace RoosterBot.Tools {
 	public class StrawpollModule : RoosterModule {
 		[Command("strawpoll")]
 		public async Task<CommandResult> CreateStrawpoll(string title, params string[] answers) {
-			var api = new StrawPoll();
-			var poll = await api.CreatePollAsync(title, answers.ToList(), false, StrawpollNET.Data.DupCheck.DISABLED, false);
+			var poll = await API.Create.CreatePollAsync(title, answers.ToList(), false, StrawPollNET.Enums.DupCheck.Disabled, false);
 			return TextResult.Success(poll.PollUrl);
 		}
 	}
