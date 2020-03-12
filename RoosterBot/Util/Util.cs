81a496eae98d8972a7f03bbfa55c45e609bacbb8
DirkPC <d.kok.2000@gmail.com>
2020-03-12 23:57:35 +0100

    send the warning about template config files being created multiple times, once for each created config file.
    why did I decide to send it once in the first place?

--- RoosterBot/Util/Util.cs
+++ RoosterBot/Util/Util.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.IO;
@@ -11,8 +12,6 @@ namespace RoosterBot {
 	/// A static class containing several helper functions.
 	/// </summary>
 	public static class Util {
-		private static bool s_FileCreationWarningNotSent = true;
-
 		/// <summary>
 		/// The program-wide instance of <see cref="Random"/> for use in easter eggs or games.
 		/// </summary>
@@ -25,11 +24,8 @@ namespace RoosterBot {
 			if (File.Exists(filePath)) {
 				return JsonConvert.DeserializeObject<T>(File.ReadAllText(filePath), jss);
 			} else {
-				if (s_FileCreationWarningNotSent) {
-					s_FileCreationWarningNotSent = false;
-					Logger.Warning("Main", "A config file was not found and a blank template has been created: " + filePath);
-					Logger.Warning("Main", "You should read the file and change it where necessary. The template config will likely not function properly.");
-				}
+				Logger.Warning("Main", "A config file was not found and a blank template has been created: " + filePath);
+				Logger.Warning("Main", "You should read the file and change it where necessary. The template config will likely not function properly.");
 
 				jss ??= new JsonSerializerSettings();
 				jss.Converters.Add(new StringEnumConverter());
