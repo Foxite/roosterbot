e5ba385e10ebf967d29e43c0ebbee581527875fc
DirkPC <d.kok.2000@gmail.com>
2020-03-31 20:33:02 +0200

    added a tool for teachers in online classes to quickly copy/paste the present student names

---
+++ RoosterBot.GLU.Discord/Checks/RequireTeacherAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using RoosterBot.DiscordNet;
+
+namespace RoosterBot.GLU.Discord {
+	public class RequireTeacherAttribute : RoosterPreconditionAttribute {
+		public override string Summary => "Alleen leraren mogen deze command gebruiken.";
+		
+		protected override ValueTask<RoosterCheckResult> CheckAsync(RoosterCommandContext ctx) {
+			if (ctx.User is DiscordUser user && user.DiscordEntity is IGuildUser igu && igu.RoleIds.Contains(688839445981560883ul)) {
+				return ValueTaskUtil.FromResult(RoosterCheckResult.Successful);
+			} else {
+				return ValueTaskUtil.FromResult(RoosterCheckResult.Unsuccessful(Summary, GLUDiscordComponent.Instance));
+			}
+		}
+	}
+}
