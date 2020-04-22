78853bce5b67ddb93e01364ccf4e5c62892e7fc0
foxite <d.kok.2000@gmail.com>
2020-04-22 19:26:34 +0200

    added abstract DiscordNetCheckAttribute which enforces the use of DiscordCommandContext

---
+++ RoosterBot.DiscordNet/Checks/DiscordNetCheckAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace RoosterBot.DiscordNet {
+	/// <summary>
+	/// The parent to all checks defined in RoosterBot.DiscordNet. This class enforces the usage of <see cref="DiscordCommandContext"/>.
+	/// </summary>
+	public abstract class DiscordNetCheckAttribute : RoosterCheckAttribute {
+		protected override ValueTask<RoosterCheckResult> CheckAsync(RoosterCommandContext context) {
+			if (context is DiscordCommandContext dcc) {
+				return CheckAsync(dcc);
+			} else if (ThrowOnInvalidContext) {
+				throw new InvalidOperationException($"{nameof(RoosterCheckAttribute)} requires a ICommandContext instance that derives from {nameof(RoosterCommandContext)}.");
+			} else {
+				return RoosterCheckResult.Unsuccessful("If you see this, then you may slap the programmer.", DiscordNetComponent.Instance);
+			}
+		}
+
+		protected abstract ValueTask<RoosterCheckResult> CheckAsync(DiscordCommandContext context);
+	}
+}
