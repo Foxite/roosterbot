3d67deed886cf83e43a28688ed1f5627eb039a59
foxite <d.kok.2000@gmail.com>
2020-04-22 19:27:00 +0200

    added a check that requires that the command has an attachment

---
+++ RoosterBot.DiscordNet/Checks/MessageHasAttachmentAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoosterBot.DiscordNet {
+	public sealed class MessageHasAttachmentAttribute : DiscordNetCheckAttribute {
+		public override string Summary => "The command must have an attachment" + (RequiredTypes.Count > 0 ? (" of type " + string.Join(", ", RequiredTypes)) : "") + ".";
+
+		public IReadOnlyList<string> RequiredTypes { get; }
+
+		public MessageHasAttachmentAttribute(params string[] requiredTypes) {
+			RequiredTypes = requiredTypes;
+		}
+
+		protected override ValueTask<RoosterCheckResult> CheckAsync(DiscordCommandContext context) {
+			if (context.Message.Attachments.Any()) {
+				return ValueTaskUtil.FromResult(RoosterCheckResult.Successful);
+			} else {
+				return ValueTaskUtil.FromResult(RoosterCheckResult.Unsuccessful(Summary, DiscordNetComponent.Instance));
+			}
+		}
+	}
+}
