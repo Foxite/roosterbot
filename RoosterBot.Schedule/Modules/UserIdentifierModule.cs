e407d73ade55bf1c533108bd5b81e71a97023787
foxite <d.kok.2000@gmail.com>
2020-04-24 17:41:48 +0200

    updated resource names for UserIdentifierModule

--- RoosterBot.Schedule/Modules/UserIdentifierModule.cs
+++ RoosterBot.Schedule/Modules/UserIdentifierModule.cs
@@ -2,31 +2,30 @@
 using Qmmands;
 
 namespace RoosterBot.Schedule {
-	// TODO update resource names
 	// TODO update ui strings, still sounds like it's restricted to student sets
-	[Name("#UserClassModule_ModuleName"), Description("#UserClassModule_ModuleSummary"), Group("#UserClassModule_Group")]
+	[Name("#UserIdentifierModule_ModuleName"), Description("#UserIdentifierModule_ModuleSummary"), Group("#UserIdentifierModule_Group")]
 	public class UserIdentifierModule : RoosterModule {
-		[Command("#UserClassModule_CommandName"), Description("#UserClassModule_CommandSummary"), RequirePrivate(false)]
-		public async Task<CommandResult> UserClass([Name("#UserClassModule_class_Name")] IdentifierInfo? newIdentifier = null) {
+		[Command("#UserIdentifierModule_CommandName"), Description("#UserIdentifierModule_CommandSummary"), RequirePrivate(false)]
+		public async Task<CommandResult> UserClass([Name("#UserIdentifierModule_class_Name")] IdentifierInfo? newIdentifier = null) {
 			if (newIdentifier == null) {
 				IdentifierInfo? identifier = UserConfig.GetIdentifier();
 				string response;
 				if (identifier == null) {
-					response = GetString("UserClassModule_UserNotKnown");
+					response = GetString("UserIdentifierModule_UserNotKnown");
 				} else {
-					response = GetString("UserClassModule_UserIsInClass", identifier.DisplayText);
+					response = GetString("UserIdentifierModule_UserIsInClass", identifier.DisplayText);
 				}
-				response += GetString("UserClassModule_ChangeHint", ChannelConfig.CommandPrefix);
+				response += GetString("UserIdentifierModule_ChangeHint", ChannelConfig.CommandPrefix);
 				return TextResult.Info(response);
 			} else {
 				if (newIdentifier.AssignableToUser) {
 					IdentifierInfo? oldIdentifier = await UserConfig.SetIdentifierAsync(newIdentifier);
 					if (oldIdentifier == null) {
-						return TextResult.Success(GetString("UserClassModule_ConfirmUserIsInClass", newIdentifier.DisplayText));
+						return TextResult.Success(GetString("UserIdentifierModule_ConfirmUserIsInClass", newIdentifier.DisplayText));
 					} else if (oldIdentifier == newIdentifier) {
-						return TextResult.Info(GetString("UserClassModule_ConfirmNoChange", newIdentifier.DisplayText));
+						return TextResult.Info(GetString("UserIdentifierModule_ConfirmNoChange", newIdentifier.DisplayText));
 					} else {
-						return TextResult.Success(GetString("UserClassModule_ConfirmUserIsInClassWithOld", newIdentifier.DisplayText, oldIdentifier.DisplayText));
+						return TextResult.Success(GetString("UserIdentifierModule_ConfirmUserIsInClassWithOld", newIdentifier.DisplayText, oldIdentifier.DisplayText));
 					}
 				} else {
 					return TextResult.Error("You can't assign that to yourself."); // TODO localize
