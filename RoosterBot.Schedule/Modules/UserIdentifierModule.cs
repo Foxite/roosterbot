3186b0146ecddb34fe8a7e3651c3d10df5b6c994
foxite <d.kok.2000@gmail.com>
2020-04-28 12:50:24 +0200

    split UserIdentifierModule command

--- RoosterBot.Schedule/Modules/UserIdentifierModule.cs
+++ RoosterBot.Schedule/Modules/UserIdentifierModule.cs
@@ -4,31 +4,32 @@ using Qmmands;
 namespace RoosterBot.Schedule {
 	[Name("#UserIdentifierModule_ModuleName"), Description("#UserIdentifierModule_ModuleSummary"), Group("#UserIdentifierModule_Group")]
 	public class UserIdentifierModule : RoosterModule {
-		[Command("#UserIdentifierModule_CommandName"), Description("#UserIdentifierModule_CommandSummary"), RequirePrivate(false)]
-		public async Task<CommandResult> UserClass([Name("#UserIdentifierModule_class_Name")] IdentifierInfo? newIdentifier = null) {
-			if (newIdentifier == null) {
-				IdentifierInfo? identifier = UserConfig.GetIdentifier();
-				string response;
-				if (identifier == null) {
-					response = GetString("UserIdentifierModule_UserNotKnown");
-				} else {
-					response = GetString("UserIdentifierModule_UserIsIdentifier", identifier.DisplayText);
-				}
-				response += GetString("UserIdentifierModule_ChangeHint", ChannelConfig.CommandPrefix);
-				return TextResult.Info(response);
+		[Command("#UserIdentifierModule_GetCommandName"), Description("#UserIdentifierModule_GetCommandSummary"), RequirePrivate(false)]
+		public CommandResult GetIdentifier() {
+			IdentifierInfo? identifier = UserConfig.GetIdentifier();
+			string response;
+			if (identifier == null) {
+				response = GetString("UserIdentifierModule_UserNotKnown");
 			} else {
-				if (newIdentifier.AssignableToUser) {
+				response = GetString("UserIdentifierModule_UserIsIdentifier", identifier.DisplayText);
+			}
+			response += GetString("UserIdentifierModule_ChangeHint", ChannelConfig.CommandPrefix);
+			return TextResult.Info(response);
+		}
+
+		[Command("#UserIdentifierModule_SetCommandName"), Description("#UserIdentifierModule_SetCommandSummary"), RequirePrivate(false)]
+		public CommandResult SetIdentifier([Name("#UserIdentifierModule_class_Name")] IdentifierInfo newIdentifier) {
+			if (newIdentifier.AssignableToUser) {
 				IdentifierInfo? oldIdentifier = UserConfig.SetIdentifier(newIdentifier);
-					if (oldIdentifier == null) {
-						return TextResult.Success(GetString("UserIdentifierModule_ConfirmUserIsIdentifier", newIdentifier.DisplayText));
-					} else if (oldIdentifier == newIdentifier) {
-						return TextResult.Info(GetString("UserIdentifierModule_ConfirmNoChange", newIdentifier.DisplayText));
-					} else {
-						return TextResult.Success(GetString("UserIdentifierModule_ConfirmUserIsIdentifierWithOld", newIdentifier.DisplayText, oldIdentifier.DisplayText));
-					}
+				if (oldIdentifier == null) {
+					return TextResult.Success(GetString("UserIdentifierModule_ConfirmUserIsIdentifier", newIdentifier.DisplayText));
+				} else if (oldIdentifier == newIdentifier) {
+					return TextResult.Info(GetString("UserIdentifierModule_ConfirmNoChange", newIdentifier.DisplayText));
 				} else {
-					return TextResult.Error("#UserIdentifierModule_NotAssignableToUser");
+					return TextResult.Success(GetString("UserIdentifierModule_ConfirmUserIsIdentifierWithOld", newIdentifier.DisplayText, oldIdentifier.DisplayText));
 				}
+			} else {
+				return TextResult.Error("#UserIdentifierModule_NotAssignableToUser");
 			}
 		}
 	}
