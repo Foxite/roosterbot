0d3edd183ad831b83c3d5cc42d6f4eca87b65f37
DirkPC <d.kok.2000@gmail.com>
2020-08-20 18:59:04 +0200

    userconfig filtering in UserListModule

--- RoosterBot.DiscordNet/Modules/UserListModule.cs
+++ RoosterBot.DiscordNet/Modules/UserListModule.cs
@@ -11,12 +11,12 @@ namespace RoosterBot.DiscordNet {
 	public class UserListModule : RoosterModule<DiscordCommandContext> {
 		private const string UserConfigListKey = "discord.tools.userList";
 
-		private async Task<IEnumerable<IGuildUser>> GetList() {
-			IEnumerable<IGuildUser> userList = await Context.Guild!.GetUsersAsync();
+		private async Task<IEnumerable<DiscordUser>> GetList() {
+			IEnumerable<DiscordUser> userList = (await Context.Guild!.GetUsersAsync()).Select(igu => new DiscordUser(igu));
 
 			return UserConfig.TryGetData<IEnumerable<ulong>>(UserConfigListKey, out IEnumerable<ulong>? ret)
 				? userList.Join(ret, user => user.Id, id => id, (igu, id) => igu)
-				: userList.Where(user => !user.IsBot);
+				: userList.Where(user => !user.DiscordEntity.IsBot);
 		}
 
 		[Command("clear")]
@@ -27,7 +27,7 @@ namespace RoosterBot.DiscordNet {
 
 		[Command("with no nickname")]
 		public async Task<CommandResult> GetUnnamedUsers() {
-			return ReplyList((await GetList()).Where(user => user.Nickname == null));
+			return ReplyList((await GetList()).Where(user => ((IGuildUser) user.DiscordEntity).Nickname == null));
 		}
 
 		[Command("with role count")]
@@ -40,8 +40,8 @@ namespace RoosterBot.DiscordNet {
 				from user in await GetList()
 				let roleCount =
 					roles.Length == 0
-						? (user.RoleIds.Count - 1) // Everyone has a role called "@everyone" so ignore that one
-						: user.RoleIds.Intersect(roles.Select(role => role.Id)).Count()
+						? (((IGuildUser) user.DiscordEntity).RoleIds.Count - 1) // Everyone has a role called "@everyone" so ignore that one
+						: ((IGuildUser) user).RoleIds.Intersect(roles.Select(role => role.Id)).Count()
 				where compare(roleCount, count)
 				select user
 			);
@@ -49,7 +49,23 @@ namespace RoosterBot.DiscordNet {
 
 		[Command("with status")]
 		public async Task<CommandResult> UsersWithStatus(UserStatus status) {
-			return ReplyList((await GetList()).Where(user => user.Status == status));
+			return ReplyList((await GetList()).Where(user => user.DiscordEntity.Status == status));
+		}
+
+		[Command("with config value")]
+		public async Task<CommandResult> UsersWithConfigValue(string key, string value) {
+			var ucs = (UserConfigService) Context.ServiceProvider.GetService(typeof(UserConfigService));
+
+			return ReplyList((await GetList()).Where(user => {
+				Newtonsoft.Json.Linq.JObject? data = ucs.GetConfigAsync(user.GetReference()).Result?.GetRawData();
+				if (data != null) {
+					Newtonsoft.Json.Linq.JToken? jt = data.SelectToken(key);
+					string? v = jt?.ToObject<string>();
+					return v == value;
+				} else {
+					return false;
+				}
+			}));
 		}
 
 		private bool TryGetCompareFunc(string name, [NotNullWhen(true)] out Func<int, int, bool>? func) {
@@ -64,10 +80,11 @@ namespace RoosterBot.DiscordNet {
 			}
 		}
 
-		private RoosterCommandResult ReplyList(IEnumerable<IGuildUser> users) {
+		private RoosterCommandResult ReplyList(IEnumerable<DiscordUser> users) {
 			if (users.Any()) {
 				IEnumerable<string[]> userRows =
-					from user in users
+					from rbUser in users
+					let user = (IGuildUser) rbUser.DiscordEntity
 					orderby user.JoinedAt?.Date
 					select new[] {
 						$"@{user.Username}#{user.Discriminator}",
