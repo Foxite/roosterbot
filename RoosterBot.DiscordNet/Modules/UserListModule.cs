da1b3772e5a5555959c4c9b593ffa31bdc798805
DirkPC <d.kok.2000@gmail.com>
2020-08-20 20:08:04 +0200

    made ReplyList and GetList public static so they can be extended

--- RoosterBot.DiscordNet/Modules/UserListModule.cs
+++ RoosterBot.DiscordNet/Modules/UserListModule.cs
@@ -11,10 +11,10 @@ namespace RoosterBot.DiscordNet {
 	public class UserListModule : RoosterModule<DiscordCommandContext> {
 		private const string UserConfigListKey = "discord.tools.userList";
 
-		private async Task<IEnumerable<DiscordUser>> GetList() {
-			IEnumerable<DiscordUser> userList = (await Context.Guild!.GetUsersAsync()).Select(igu => new DiscordUser(igu));
+		public static async Task<IEnumerable<DiscordUser>> GetList(DiscordCommandContext context) {
+			IEnumerable<DiscordUser> userList = (await context.Guild!.GetUsersAsync()).Select(igu => new DiscordUser(igu));
 
-			return UserConfig.TryGetData<IEnumerable<ulong>>(UserConfigListKey, out IEnumerable<ulong>? ret)
+			return context.UserConfig.TryGetData<IEnumerable<ulong>>(UserConfigListKey, out IEnumerable<ulong>? ret)
 				? userList.Join(ret, user => user.Id, id => id, (igu, id) => igu)
 				: userList.Where(user => !user.DiscordEntity.IsBot);
 		}
@@ -27,7 +27,7 @@ namespace RoosterBot.DiscordNet {
 
 		[Command("with no nickname")]
 		public async Task<CommandResult> GetUnnamedUsers() {
-			return ReplyList((await GetList()).Where(user => ((IGuildUser) user.DiscordEntity).Nickname == null));
+			return ReplyList(Context, (await GetList(Context)).Where(user => ((IGuildUser) user.DiscordEntity).Nickname == null));
 		}
 
 		[Command("with role count")]
@@ -36,8 +36,8 @@ namespace RoosterBot.DiscordNet {
 				return TextResult.Error("Invalid comparison.");
 			}
 
-			return ReplyList(
-				from user in await GetList()
+			return ReplyList(Context,
+				from user in await GetList(Context)
 				let roleCount =
 					roles.Length == 0
 						? (((IGuildUser) user.DiscordEntity).RoleIds.Count - 1) // Everyone has a role called "@everyone" so ignore that one
@@ -49,14 +49,14 @@ namespace RoosterBot.DiscordNet {
 
 		[Command("with status")]
 		public async Task<CommandResult> UsersWithStatus(UserStatus status) {
-			return ReplyList((await GetList()).Where(user => user.DiscordEntity.Status == status));
+			return ReplyList(Context, (await GetList(Context)).Where(user => user.DiscordEntity.Status == status));
 		}
 
 		[Command("with config value")]
 		public async Task<CommandResult> UsersWithConfigValue(string key, string value) {
 			var ucs = (UserConfigService) Context.ServiceProvider.GetService(typeof(UserConfigService));
 
-			return ReplyList((await GetList()).Where(user => {
+			return ReplyList(Context, (await GetList(Context)).Where(user => {
 				Newtonsoft.Json.Linq.JObject? data = ucs.GetConfigAsync(user.GetReference()).Result?.GetRawData();
 				if (data != null) {
 					Newtonsoft.Json.Linq.JToken? jt = data.SelectToken(key);
@@ -80,7 +80,7 @@ namespace RoosterBot.DiscordNet {
 			}
 		}
 
-		private RoosterCommandResult ReplyList(IEnumerable<DiscordUser> users) {
+		public static RoosterCommandResult ReplyList(DiscordCommandContext context, IEnumerable<DiscordUser> users) {
 			if (users.Any()) {
 				IEnumerable<string[]> userRows =
 					from rbUser in users
@@ -89,18 +89,18 @@ namespace RoosterBot.DiscordNet {
 					select new[] {
 						$"@{user.Username}#{user.Discriminator}",
 						user.JoinedAt?.ToString("yyyy-MM-dd") ?? "Unknown",
-						string.Join(", ", user.RoleIds.Select(roleId => Context.Guild!.GetRole(roleId).Name).Where(roleName => roleName != "@everyone"))
+						string.Join(", ", user.RoleIds.Select(roleId => context.Guild!.GetRole(roleId).Name).Where(roleName => roleName != "@everyone"))
 					};
 
 				string[][] table = new string[userRows.Count() + 1][];
 				table[0] = new[] { "Username", "Joined", "Roles" };
 				userRows.CopyTo(table, 1);
 
-				UserConfig.SetData(UserConfigListKey, users.Select(user => user.Id));
+				context.UserConfig.SetData(UserConfigListKey, users.Select(user => user.Id));
 
 				return new TableResult("", table);
 			} else {
-				UserConfig.RemoveData(UserConfigListKey);
+				context.UserConfig.RemoveData(UserConfigListKey);
 
 				return TextResult.Info("No results.");
 			}
