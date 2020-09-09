e5606cb9509c8d681c34a9289ac843ef5972c970
Foxite <the@dirkkok.nl>
2020-09-09 23:23:07 +0200

    restructured CommandHandler. seems less spaghetti-ish now (despite not being functionally different).
    OverloadsFailedResult will now recurse (partially) into the other failure handlers, and if all error messages are the same, it will display just one.

--- RoosterBot/Handlers/CommandHandler.cs
+++ RoosterBot/Handlers/CommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
@@ -33,74 +34,28 @@ namespace RoosterBot {
 
 				switch (result) {
 					case CommandDisabledResult _:
-						response = Resources.GetString(context.Culture, "CommandHandling_Disabled");
+						response = HandleDisabled(context);
 						break;
 					case CommandNotFoundResult _:
-						response = string.Format(Resources.GetString(context.Culture, "CommandHandling_NotFound"), context.ChannelConfig.CommandPrefix);
+						response = HandleNotFound(context);
 						break;
 					case CommandOnCooldownResult cooldown:
-						response = string.Format(Resources.GetString(context.Culture, "CommandHandling_Cooldown"), cooldown.Cooldowns.First().RetryAfter.ToString("c", context.Culture));
+						response = HandleCooldown(context, cooldown);
 						break;
 					case OverloadsFailedResult overloads:
-						response = Resources.GetString(context.Culture, "CommandHandling_OverloadsFailed") + "\n";
-						response += string.Join('\n', overloads.FailedOverloads.Select(kvp => "`" + context.ChannelConfig.CommandPrefix + kvp.Key.GetSignature() + "`: " +
-							Resources.ResolveString(context.Culture, Program.Instance.Components.GetComponentForModule(kvp.Key.Module), kvp.Value.Reason)));
+						response = HandleOverloadsFailed(context, overloads);
 						break;
 					case ArgumentParseFailedResult argument:
-						if (argument.ParserResult is DefaultArgumentParserResult parseResult) {
-							response = Resources.GetString(context.Culture, "CommandHandling_Arguments_" + parseResult.Failure.ToString()) + "\n";
-							if (parseResult.FailurePosition != null) {
-								response += "`" + context.Message.Content + "`\n`" + new string(' ', context.Message.Content.Length - context.RawArguments.Length + parseResult.FailurePosition.Value) + "^`";
-							}
-						} else {
-							response = $"PostCommandHandler got ArgumentParseFailedResult but it has an unknown ParserResult: {argument.ParserResult.GetType().FullName}. This is the ToString: {argument.ParserResult}";
-							Logger.Warning("CommandHandler", response);
-							if (response.Length > 2000) {
-								const string TooLong = "The error message was longer than 2000 characters. This is the first section:\n";
-								response = TooLong + response.Substring(0, 1999 - TooLong.Length);
-							}
-							await Notifications.AddNotificationAsync(response);
-							response = Resources.GetString(context.Culture, "CommandHandling_FatalError");
-						}
+						response = await HandleArgumentParseFailed(context, argument);
 						break;
 					case TypeParseFailedResult type:
-						response = type.Reason;
-						if (type.TypeParserResult is IRoosterTypeParserResult rtpr) {
-							if (!rtpr.Parser.GetType().Assembly.Equals(Assembly.GetExecutingAssembly())) {
-								Component? component;
-								if (rtpr.Parser is IExternalResultStringParser ersp) {
-									component = ersp.ErrorReasonComponent;
-								} else {
-									component = Program.Instance.Components.GetComponentFromAssembly(rtpr.Parser.GetType().Assembly);
-								}
-								response = Resources.ResolveString(context.Culture, component, response);
-							}
-						}
+						response = HandleTypeParseFailed(context, type);
 						break;
 					case ChecksFailedResult check:
-						response = "";
-						foreach ((CheckAttribute Check, CheckResult Result) in check.FailedChecks) {
-							if (Result is RoosterCheckResult rcr) {
-								Component? component = Program.Instance.Components.GetComponentFromAssembly(Check.GetType().Assembly);
-								response += string.Format(Resources.ResolveString(context.Culture, component, Result.Reason), rcr.ErrorReasonObjects);
-							} else {
-								response += Result.Reason;
-							}
-							response += "\n";
-						}
+						response = HandleCheckFailed(context, check);
 						break;
 					case ParameterChecksFailedResult paramCheck:
-						response = "";
-						foreach ((ParameterCheckAttribute Check, CheckResult Result) in paramCheck.FailedChecks) {
-							if (Result is RoosterCheckResult rcr) {
-								Component? component = Program.Instance.Components.GetComponentFromAssembly(Check.GetType().Assembly);
-								response += string.Format(Resources.GetString(context.Culture, "CommandHandling_ParamCheckFailed"), paramCheck.Parameter.Name,
-									Resources.ResolveString(context.Culture, component, Result.Reason));
-							} else {
-								response += Result.Reason;
-							}
-							response += "\n";
-						}
+						response = HandleParameterCheckFailed(context, paramCheck);
 						break;
 					default:
 						response = $"PostCommandHandler got an unknown result: {result.GetType().FullName}. This is the ToString: {result}";
@@ -119,6 +74,112 @@ namespace RoosterBot {
 				}
 				await context.RespondAsync(TextResult.Error(response));
 			}
+		}
+
+		private string HandleDisabled(RoosterCommandContext context) => Resources.GetString(context.Culture, "CommandHandling_Disabled");
+		private string HandleNotFound(RoosterCommandContext context) => string.Format(Resources.GetString(context.Culture, "CommandHandling_NotFound"), context.ChannelConfig.CommandPrefix);
+		private string HandleCooldown(RoosterCommandContext context, CommandOnCooldownResult cooldown) => string.Format(Resources.GetString(context.Culture, "CommandHandling_Cooldown"), cooldown.Cooldowns.First().RetryAfter.ToString("c", context.Culture));
+
+		private string HandleOverloadsFailed(RoosterCommandContext context, OverloadsFailedResult overloads) {
+
+			var rows = overloads.FailedOverloads.Select(kvp => {
+				string reason = kvp.Value switch
+				{
+					ChecksFailedResult          check      => HandleCheckFailed(context, check),
+					CommandDisabledResult       _          => HandleDisabled(context),
+					TypeParseFailedResult       type       => HandleTypeParseFailed(context, type),
+					CommandOnCooldownResult     cooldown   => HandleCooldown(context, cooldown),
+					ParameterChecksFailedResult paramCheck => HandleParameterCheckFailed(context, paramCheck),
+					_ => kvp.Value.Reason,
+				};
+				return new KeyValuePair<string, string>("`" + context.ChannelConfig.CommandPrefix + kvp.Key.GetSignature() + "`: ", Resources.ResolveString(context.Culture, Program.Instance.Components.GetComponentForModule(kvp.Key.Module), reason));
+			}).ToDictionary(kvp => kvp.Key, kvp => kvp.Value);
+
+			if (rows.Values.Distinct().CountEquals(1)) {
+				return rows.Values.First();
+			} else {
+				return Resources.GetString(context.Culture, "CommandHandling_OverloadsFailed") + "\n" + string.Join('\n', rows.Select(kvp => kvp.Key + kvp.Value));
+			}
+		}
+
+		private async Task<string> HandleArgumentParseFailed(RoosterCommandContext context, ArgumentParseFailedResult argument) {
+			string response;
+			if (argument.ParserResult is DefaultArgumentParserResult parseResult) {
+				response = Resources.GetString(context.Culture, "CommandHandling_Arguments_" + parseResult.Failure.ToString()) + "\n";
+				if (parseResult.FailurePosition != null) {
+					response += "`" + context.Message.Content + "`\n`" + new string(' ', context.Message.Content.Length - context.RawArguments.Length + parseResult.FailurePosition.Value) + "^`";
+				}
+			} else {
+				response = $"PostCommandHandler got ArgumentParseFailedResult but it has an unknown ParserResult: {argument.ParserResult.GetType().FullName}. This is the ToString: {argument.ParserResult}";
+				Logger.Warning("CommandHandler", response);
+				if (response.Length > 2000) {
+					const string TooLong = "The error message was longer than 2000 characters. This is the first section:\n";
+					response = TooLong + response.Substring(0, 1999 - TooLong.Length);
+				}
+				await Notifications.AddNotificationAsync(response);
+				response = Resources.GetString(context.Culture, "CommandHandling_FatalError");
+			}
+
+			return response;
+		}
+
+		private string HandleParameterCheckFailed(RoosterCommandContext context, ParameterChecksFailedResult paramCheck) {
+			string response = "";
+			bool first = true;
+			foreach ((ParameterCheckAttribute Check, CheckResult Result) in paramCheck.FailedChecks) {
+				if (first) {
+					first = false;
+				} else {
+					response += "\n";
+				}
+				if (Result is RoosterCheckResult rcr) {
+					Component? component = Program.Instance.Components.GetComponentFromAssembly(Check.GetType().Assembly);
+					response += string.Format(Resources.GetString(context.Culture, "CommandHandling_ParamCheckFailed"), paramCheck.Parameter.Name,
+						Resources.ResolveString(context.Culture, component, Result.Reason));
+				} else {
+					response += Result.Reason;
+				}
+			}
+
+			return response;
+		}
+
+		private string HandleCheckFailed(RoosterCommandContext context, ChecksFailedResult check) {
+			string response = "";
+			bool first = true;
+			foreach ((CheckAttribute Check, CheckResult Result) in check.FailedChecks) {
+				if (first) {
+					first = false;
+				} else {
+					response += "\n";
+				}
+
+				if (Result is RoosterCheckResult rcr) {
+					Component? component = Program.Instance.Components.GetComponentFromAssembly(Check.GetType().Assembly);
+					response += string.Format(Resources.ResolveString(context.Culture, component, rcr.Reason), rcr.ErrorReasonObjects);
+				} else {
+					response += Result.Reason;
+				}
+			}
+
+			return response;
+		}
+
+		private string HandleTypeParseFailed(RoosterCommandContext context, TypeParseFailedResult type) {
+			string response = type.Reason;
+			if (type.TypeParserResult is IRoosterTypeParserResult rtpr) {
+				if (!rtpr.Parser.GetType().Assembly.Equals(Assembly.GetExecutingAssembly())) {
+					Component? component;
+					if (rtpr.Parser is IExternalResultStringParser ersp) {
+						component = ersp.ErrorReasonComponent;
+					} else {
+						component = Program.Instance.Components.GetComponentFromAssembly(rtpr.Parser.GetType().Assembly);
+					}
+					response = Resources.ResolveString(context.Culture, component, response);
+				}
+			}
+
+			return response;
 		}
 	}
 }
