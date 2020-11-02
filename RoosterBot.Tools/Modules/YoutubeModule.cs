2281dcb306d643352d8491c3f047e92cf3ed4d88
foxite <the@dirkkok.nl>
2020-11-02 15:57:03 +0100

    updated youtubeexplode

--- RoosterBot.Tools/Modules/YoutubeModule.cs
+++ RoosterBot.Tools/Modules/YoutubeModule.cs
@@ -11,7 +11,6 @@ namespace RoosterBot.Tools {
 	[Name("#YoutubeModule_Name")]
 	public class YoutubeModule : RoosterModule {
 		public YoutubeClient Client { get; set; } = null!;
-		public YoutubeConverter Converter { get; set; } = null!;
 
 		[Command("#YoutubeModule_Convert"), Description("#YoutubeModule_Convert_Description")]
 		public async Task<CommandResult> DownloadYoutubeAudioCommand([Name("#YoutubeModule_Convert_Format")] string format, [Name("#YoutubeModule_Convert_Url")] string url) {
@@ -21,12 +20,11 @@ namespace RoosterBot.Tools {
 				var id = new VideoId(url);
 				Video video = await Client.Videos.GetAsync(id);
 
-				//MediaStreamInfoSet streams = await Client.GetVideoMediaStreamInfosAsync(id);
 				var streams = await Client.Videos.Streams.GetManifestAsync(id);
 				if (streams.GetAudioOnly().Any()) {
 					DirectoryInfo directory = Directory.CreateDirectory(Path.Combine(Path.GetTempPath(), Path.GetRandomFileName()));
 					string filePath = Path.Combine(Path.GetTempPath(), directory.FullName, video.Title + "." + format);
-					await Converter.DownloadAndProcessMediaStreamsAsync(streams.GetAudioOnly().ToList(), filePath, format, ConversionPreset.Medium);
+					await Client.Videos.DownloadAsync(id, new ConversionRequest(ToolsComponent.Instance.PathToFFMPEG, filePath, new ConversionFormat(format), ConversionPreset.Slow));
 
 					if (new FileInfo(filePath).Length > 8e6) {
 						return TextResult.Error(GetString("YoutubeModule_Convert_Fail_Filesize"));
