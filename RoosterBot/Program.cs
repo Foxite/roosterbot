92112da0fb66d400a049bdb7586173ea6916b085
foxite <the@dirkkok.nl>
2021-01-14 21:48:21 +0100

    updated qmmands fork

--- RoosterBot/Program.cs
+++ RoosterBot/Program.cs
@@ -23,7 +23,7 @@ namespace RoosterBot {
 		/// <summary>
 		/// The version of RoosterBot.
 		/// </summary>
-		public static readonly Version Version = new Version(3, 2, 3);
+		public static readonly Version Version = new Version(3, 2, 4);
 
 		/// <summary>
 		/// The instance of the Program class.
